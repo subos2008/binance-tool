592692714730d1b763862c82eea8d0d23b7072d6
Ryan Cocks <ryan@ryancocks.net>
2019-05-06 15:34:57 +0300

    Move algo code to own class. Untested

--- binance.js
+++ binance.js
@@ -3,17 +3,13 @@
 /* eslint func-names: ["warn", "as-needed"] */
 
 require('dotenv').config();
-const async_error_handler = require('./lib/async_error_handler');
-const { ExitNow, ExecutionComplete } = require('./lib/errors');
 
 // TODO: convert all the process.exit calls to be exceptions
 // TODO: add watchdog on trades stream - it can stop responding without realising
 // TODO: - in the original implementations
 
 const Binance = require('binance-api-node').default;
-const BigNumber = require('bignumber.js');
 const send_message = require('./telegram.js');
-const StateMachine = require('javascript-state-machine');
 
 const { argv } = require('yargs')
 	.usage('Usage: $0')
@@ -39,12 +35,6 @@ const { argv } = require('yargs')
 	.alias('b', 'e')
 	.alias('b', 'entry')
 	.describe('b', 'Set buy price (0 for market buy)')
-	// '-B <buyLimitPrice>'
-	.string('B')
-	.alias('B', 'buy-limit')
-	.alias('B', 'E')
-	.alias('B', 'entry-limit')
-	.describe('B', 'Set buy stop-limit order limit price (if different from buy price)')
 	// '-s <stopPrice>'
 	.string('s')
 	.alias('s', 'stop')
@@ -63,34 +53,7 @@ const { argv } = require('yargs')
 	.describe('F', 'Calculate stop/target sell amounts assuming not paying fees using BNB')
 	.default('F', false);
 
-let {
-	p: pair,
-	a: amount,
-	q: quoteAmount,
-	b: buyPrice,
-	B: buyLimitPrice,
-	s: stopPrice,
-	l: limitPrice,
-	t: targetPrice
-} = argv;
-
-if (buyPrice === '') {
-	buyPrice = '0';
-}
-
-if (quoteAmount && buyPrice && buyPrice != 0) {
-	amount = BigNumber(quoteAmount).dividedBy(buyPrice);
-	console.log(`Calculated buy amount ${amount.toFixed()}`);
-}
-
-if (!amount) {
-	console.error('You must specify amount with -a or via -q');
-	process.exit(1);
-}
-
-const { F: nonBnbFees } = argv;
-
-pair = pair.toUpperCase();
+let { p: pair, a: amount, q: quoteAmount, b: buyPrice, s: stopPrice, l: limitPrice, t: targetPrice } = argv;
 
 // TODO: Note that for all authenticated endpoints, you can pass an extra parameter useServerTime
 // TODO: set to true in order to fetch the server time before making the request.
@@ -101,364 +64,25 @@ const binance_client = Binance({
 	// getTime: xxx // time generator function, optional, defaults to () => Date.now()
 });
 
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+// 	return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
 var closeUserWebsocket, closeTradesWebSocket;
 
-async function main() {
-	closeUserWebsocket = await binance_client.ws.user((data) => {
-		const { i: orderId } = data;
-
-		if (orderId === buyOrderId) {
-			checkOrderFilled(data, () => {
-				const { N: commissionAsset } = data;
-				buyOrderId = 0;
-				fsm.buyOrderFilled();
-			});
-		} else if (orderId === stopOrderId) {
-			checkOrderFilled(data, () => {
-				throw new ExecutionComplete(`Stop hit`);
-			});
-		} else if (orderId === targetOrderId) {
-			checkOrderFilled(data, () => {
-				throw new ExecutionComplete(`Target hit`);
-			});
-		}
-	});
-
-	var exchangeInfoData;
-	try {
-		exchangeInfoData = await binance_client.exchangeInfo();
-	} catch (e) {
-		console.error('Error could not pull exchange info');
-		console.error(e);
-		throw new Error('Error could not pull exchange info');
-	}
-
-	const symbolData = exchangeInfoData.symbols.find((ei) => ei.symbol === pair);
-	if (!symbolData) {
-		throw new Error(`Could not pull exchange info for ${pair}`);
-	}
-
-	const { filters } = symbolData;
-	const { stepSize, minQty } = filters.find((eis) => eis.filterType === 'LOT_SIZE');
-	const { tickSize, minPrice } = filters.find((eis) => eis.filterType === 'PRICE_FILTER');
-	const { minNotional } = filters.find((eis) => eis.filterType === 'MIN_NOTIONAL');
-
-	function munge_and_check_quantity(name, volume) {
-		volume = BigNumber(utils.roundStep(BigNumber(volume), stepSize));
-		if (volume.isLessThan(minQty)) {
-			throw new Error(`${name} ${volume} does not meet minimum order amount ${minQty}.`);
-		}
-		return volume;
-	}
-
-	function munge_and_check_price(name, price) {
-		price = BigNumber(price);
-		if (price.isZero()) return price; // don't munge zero, special case for market buys
-		price = BigNumber(utils.roundTicks(price, tickSize));
-		if (price.isLessThan(minPrice)) {
-			throw new Error(`${name} ${price} does not meet minimum order price ${minPrice}.`);
-		}
-		return price;
-	}
-
-	function check_notional(name, price, volume) {
-		if (price.isZero()) return; // don't check zero, special case for market buys
-		let quote_volume = price.times(volume);
-		if (quote_volume.isLessThan(minNotional)) {
-			throw new Error(
-				`${name} does not meet minimum order value ${minNotional} (Buy of ${volume} at ${price} = ${quote_volume}).`
-			);
-		}
-	}
-
-	amount = munge_and_check_quantity('Amount', amount);
-
-	if (buyPrice && buyPrice !== 0) {
-		buyPrice = munge_and_check_price('Buy price', buyPrice);
-		check_notional('Buy order', buyPrice, amount);
-
-		if (buyLimitPrice) {
-			buyLimitPrice = munge_and_check_price('Buy limit price', buyLimitPrice);
-		}
-	}
-
-	if (stopPrice) {
-		stopPrice = munge_and_check_price('Stop price', stopPrice);
-
-		if (limitPrice) {
-			limitPrice = munge_and_check_price('Limit price', limitPrice);
-			check_notional('Stop order', limitPrice, amount);
-		} else {
-			check_notional('Stop order', stopPrice, amount);
-		}
-	}
-
-	if (targetPrice) {
-		targetPrice = munge_and_check_price('Target price', targetPrice);
-		check_notional('Target order', targetPrice, amount);
-	}
-
-	const NON_BNB_TRADING_FEE = BigNumber('0.001');
-
-	const calculateSellAmount = function(commissionAsset, sellAmount) {
-		// Adjust sell amount if BNB not used for trading fee
-		return commissionAsset === 'BNB' && !nonBnbFees
-			? sellAmount
-			: sellAmount.times(BigNumber(1).minus(NON_BNB_TRADING_FEE));
-	};
-
-	let stopOrderId = 0;
-	let targetOrderId = 0;
-
-	const sellComplete = function(error, response) {
-		if (error) {
-			throw new Error('Sell error', error.body);
-		}
-
-		console.log('Sell response', response);
-		console.log(`order id: ${response.orderId}`);
-
-		if (!(stopPrice && targetPrice)) {
-			throw new ExecutionComplete();
-		}
-
-		if (response.type === 'STOP_LOSS_LIMIT') {
-			send_message(`${pair} stopped out`);
-			stopOrderId = response.orderId;
-		} else if (response.type === 'LIMIT') {
-			send_message(`${pair} hit target price`);
-			targetOrderId = response.orderId;
-		}
-	};
-
-	async function placeStopOrder() {
-		try {
-			let args = {
-				side: 'SELL',
-				symbol: pair,
-				type: 'STOP_LOSS_LIMIT',
-				quantity: amount.toFixed(),
-				price: (limitPrice || stopPrice).toFixed(), // TODO: what's this limitPrice bit?
-				stopPrice: stopPrice.toFixed()
-				// TODO: more args here, server time and use FULL response body
-			};
-			console.log(`Creating STOP_LOSS_LIMIT SELL ORDER`);
-			console.log(args);
-			let response = await binance_client.order(args);
-			console.log('Buy response', response);
-			console.log(`order id: ${response.orderId}`);
-			return response.orderId;
-		} catch (error) {
-			async_error_handler(console, `error placing order: ${error.body}`, error);
-		}
-	}
-
-	async function placeTargetOrder() {
-		try {
-			let args = {
-				side: 'SELL',
-				symbol: pair,
-				type: 'LIMIT',
-				quantity: amount.toFixed(),
-				price: targetPrice.toFixed()
-				// TODO: more args here, server time and use FULL response body
-			};
-			console.log(`Creating LIMIT SELL ORDER`);
-			console.log(args);
-			let response = await binance_client.order(args);
-			console.log('Buy response', response);
-			console.log(`order id: ${response.orderId}`);
-			return response.orderId;
-		} catch (error) {
-			async_error_handler(console, `error placing order: ${error.body}`, error);
-		}
-	}
-
-	const placeSellOrder = function() {
-		if (stopPrice) {
-			placeStopOrder();
-		} else if (targetPrice) {
-			placeTargetOrder();
-		} else {
-			throw new ExecutionComplete();
-		}
-	};
-
-	async function create_market_buy_order() {
-		try {
-			let args = {
-				side: 'BUY',
-				symbol: pair,
-				type: 'MARKET',
-				quantity: amount.toFixed()
-				// TODO: more args here, server time and use FULL response body
-			};
-			console.log(`Creating MARKET BUY ORDER`);
-			// console.log(args);
-			let response = await binance_client.order(args);
-			fsm.buyOrderCreated();
-			console.log('Buy response', response);
-			console.log(`order id: ${response.orderId}`);
-			return response.orderId;
-		} catch (error) {
-			async_error_handler(console, `Buy error: ${error.body}`, error);
-		}
-	}
-
-	async function create_limit_buy_order() {
-		try {
-			let args = {
-				side: 'BUY',
-				symbol: pair,
-				type: 'LIMIT',
-				quantity: amount.toFixed(),
-				price: buyPrice.toFixed()
-				// TODO: more args here, server time and use FULL response body
-			};
-			console.log(`Creating LIMIT BUY ORDER`);
-			console.log(args);
-			let response = await binance_client.order(args);
-			fsm.buyOrderCreated();
-			console.log('Buy response', response);
-			console.log(`order id: ${response.orderId}`);
-			return response.orderId;
-		} catch (error) {
-			async_error_handler(console, `Buy error: ${error.body}`, error);
-		}
-	}
-
-	var buyOrderId = 0;
-	let isLimitEntry = false;
-	let isStopEntry = false;
-
-	var fsm = new StateMachine({
-		init: 'initialising',
-		transitions: [
-			{ name: 'buy_order_created', from: 'initialising', to: 'buy_order_open' }, // A
-			// { name: 'wait_for_entry_price', from: 'initialising', to: 'waiting_for_entry_price' }, // B
-			{ name: 'buy_order_filled', from: 'buy_order_open', to: 'waiting_for_exit_price' } // C
-			// { name: 'buy_order_created', from: 'waiting_for_entry_price', to: 'buy_order_open' }
-		],
-		methods: {
-			// TODO: async?
-			onWaitingForExitPrice: function() {
-				console.log('Entering: waiting_for_exit_price');
-				placeSellOrder();
-			}
-		}
-	});
-
-	if (typeof buyPrice !== 'undefined') {
-		if (buyPrice.isZero()) {
-			buyOrderId = await create_market_buy_order();
-		} else {
-			buyOrderId = await create_limit_buy_order();
-		}
-	}
-
-	// TODO: I guess it would be good to check how much the balance is on the exchange
-	// against 'amount' if there is no buy stage
-
-	if (fsm.is('initialising')) {
-		throw new Error(`Unable to determine intial state`);
-	}
-
-	// console.log(`BuyPrice: ${buyPrice}, isZero(): ${buyPrice.isZero()}`);
-	// if (typeof buyPrice !== 'undefined') {
-	// 	if (buyPrice.isZero()) {
-	// 		buyOrderId = await create_market_buy_order();
-	// TODO: move this code
-	// 	} else if (buyPrice.isGreaterThan(0)) {
-	// 		old_binance.prices(pair, (error, ticker) => {
-	// 			const currentPrice = ticker[pair];
-	// 			console.log(`${pair} price: ${currentPrice}`);
-
-	// 			if (buyPrice.isGreaterThan(currentPrice)) {
-	// 				isStopEntry = true;
-	// 				old_binance.buy(
-	// 					pair,
-	// 					amount.toFixed(),
-	// 					(buyLimitPrice || buyPrice).toFixed(),
-	// 					{ stopPrice: buyPrice.toFixed(), type: 'STOP_LOSS_LIMIT', newOrderRespType: 'FULL' },
-	// 					buyComplete
-	// 				);
-	// 			} else {
-	// 				isLimitEntry = true;
-	// 				console.error('needs implementing');
-	// 				throw new Error('backtrace me');
-	// 			}
-	// 		});
-	// 	}
-	// } else {
-	// 	placeSellOrder();
-	// }
-
-	let isCancelling = false;
-
-	// TODO: we don't always need this - only if we have cancel/stop/target orders the need monitoring
-	closeTradesWebSocket = await binance_client.ws.aggTrades([ pair ], async function(trade) {
-		var { s: symbol, p: price } = trade;
-		price = BigNumber(price);
-
-		if (buyOrderId) {
-			// console.log(`${symbol} trade update. price: ${price} buy: ${buyPrice}`);
-		} else if (stopOrderId || targetOrderId) {
-			// console.log(`${symbol} trade update. price: ${price} stop: ${stopPrice} target: ${targetPrice}`);
-			if (stopOrderId && !targetOrderId && price.isGreaterThanOrEqualTo(targetPrice) && !isCancelling) {
-				console.log(`Event: price >= targetPrice: cancelling stop and placeTargetOrder()`);
-				isCancelling = true;
-				try {
-					await binance_client.cancelOrder({ symbol, orderId: stopOrderId });
-					isCancelling = false;
-				} catch (error) {
-					console.error(`${symbol} cancel error:`, error.body);
-					return;
-				}
-				stopOrderId = 0;
-				console.log(`${symbol} cancel response:`, response);
-				placeTargetOrder();
-			} else if (targetOrderId && !stopOrderId && price.isLessThanOrEqualTo(stopPrice) && !isCancelling) {
-				isCancelling = true;
-				try {
-					await binance_client.cancelOrder({ symbol, orderId: targetOrderId });
-					isCancelling = false;
-				} catch (error) {
-					console.error(`${symbol} cancel error:`, error.body);
-					return;
-				}
-				targetOrderId = 0;
-				console.log(`${symbol} cancel response:`, response);
-				placeStopOrder();
-			}
-		}
-	});
-
-	await sleep(2000);
-	console.log('hey - trades active');
-
-	const checkOrderFilled = function(data, orderFilled) {
-		const { s: symbol, p: price, q: quantity, S: side, o: orderType, i: orderId, X: orderStatus } = data;
-
-		console.log(`${symbol} ${side} ${orderType} ORDER #${orderId} (${orderStatus})`);
-		console.log(`..price: ${price}, quantity: ${quantity}`);
-
-		if (orderStatus === 'NEW' || orderStatus === 'PARTIALLY_FILLED') {
-			return;
-		}
-
-		if (orderStatus !== 'FILLED') {
-			throw new Error(`Order ${orderStatus}. Reason: ${data.r}`);
-		}
-
-		orderFilled(data);
-	};
-}
-
-main()
+const algo = new Algo({
+	ee: binance_client,
+	send_message,
+	pair,
+	amount,
+	quoteAmount,
+	buyPrice,
+	stopPrice,
+	limitPrice,
+	targetPrice
+});
+algo
+	.main()
 	.then(() => {
 		console.log('main loop complete');
 	})
@@ -483,16 +107,11 @@ function dump_keepalive() {
 // Note this method returns!
 // Shuts down everything that's keeping us alive so we exit
 function soft_exit(exit_code) {
-	shutdown_streams();
+	algo.shutdown_streams();
 	if (exit_code) process.exitCode = exit_code;
 	// setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
 }
 
-function shutdown_streams() {
-	if (closeUserWebsocket) closeUserWebsocket();
-	if (closeTradesWebSocket) closeTradesWebSocket();
-}
-
 process.on('exit', () => {
-	shutdown_streams();
+	algo.shutdown_streams();
 });
---
+++ service_lib/algo.js
@@ -0,0 +1,394 @@
+const async_error_handler = require('./lib/async_error_handler');
+const { ExitNow, ExecutionComplete } = require('./lib/errors');
+const StateMachine = require('javascript-state-machine');
+const BigNumber = require('bignumber.js');
+
+class Algo {
+	// All numbers are expected to be passed in as strings
+	constructor(
+		{
+			ee, // binance-api-node API
+			send_message,
+			pair,
+			amount,
+			quoteAmount,
+			buyPrice,
+			stopPrice,
+			limitPrice,
+			targetPrice
+		} = {}
+	) {
+		this.ee = ee;
+		this.send_message = send_message;
+		this.pair = pair;
+		this.amount = amount;
+		this.quoteAmount = quoteAmount;
+		this.buyPrice = buyPrice;
+		this.stopPrice = stopPrice;
+		this.limitPrice = limitPrice;
+		this.targetPrice = targetPrice;
+
+		if (this.buyPrice === '') {
+			this.buyPrice = '0';
+		}
+
+		if (this.quoteAmount && this.buyPrice && this.buyPrice != 0) {
+			this.amount = BigNumber(this.quoteAmount).dividedBy(this.buyPrice);
+			console.log(`Calculated buy amount ${this.amount.toFixed()}`);
+		}
+
+		if (!this.amount) {
+			console.error('You must specify amount with -a or via -q');
+			process.exit(1);
+		}
+
+		const { F: nonBnbFees } = argv;
+
+		this.pair = this.pair.toUpperCase();
+	}
+
+	shutdown_streams() {
+		if (this.closeUserWebsocket) this.closeUserWebsocket();
+		if (this.closeTradesWebSocket) this.closeTradesWebSocket();
+	}
+
+	async main() {
+		this.closeUserWebsocket = await binance_client.ws.user((data) => {
+			const { i: orderId } = data;
+
+			if (orderId === buyOrderId) {
+				checkOrderFilled(data, () => {
+					const { N: commissionAsset } = data;
+					buyOrderId = 0;
+					fsm.buyOrderFilled();
+				});
+			} else if (orderId === stopOrderId) {
+				checkOrderFilled(data, () => {
+					throw new ExecutionComplete(`Stop hit`);
+				});
+			} else if (orderId === targetOrderId) {
+				checkOrderFilled(data, () => {
+					throw new ExecutionComplete(`Target hit`);
+				});
+			}
+		});
+
+		var exchangeInfoData;
+		try {
+			exchangeInfoData = await binance_client.exchangeInfo();
+		} catch (e) {
+			console.error('Error could not pull exchange info');
+			console.error(e);
+			throw new Error('Error could not pull exchange info');
+		}
+
+		const symbolData = exchangeInfoData.symbols.find((ei) => ei.symbol === this.pair);
+		if (!symbolData) {
+			throw new Error(`Could not pull exchange info for ${this.pair}`);
+		}
+
+		const { filters } = symbolData;
+		const { stepSize, minQty } = filters.find((eis) => eis.filterType === 'LOT_SIZE');
+		const { tickSize, minPrice } = filters.find((eis) => eis.filterType === 'PRICE_FILTER');
+		const { minNotional } = filters.find((eis) => eis.filterType === 'MIN_NOTIONAL');
+
+		function munge_and_check_quantity(name, volume) {
+			volume = BigNumber(utils.roundStep(BigNumber(volume), stepSize));
+			if (volume.isLessThan(minQty)) {
+				throw new Error(`${name} ${volume} does not meet minimum order amount ${minQty}.`);
+			}
+			return volume;
+		}
+
+		function munge_and_check_price(name, price) {
+			price = BigNumber(price);
+			if (price.isZero()) return price; // don't munge zero, special case for market buys
+			price = BigNumber(utils.roundTicks(price, tickSize));
+			if (price.isLessThan(minPrice)) {
+				throw new Error(`${name} ${price} does not meet minimum order price ${minPrice}.`);
+			}
+			return price;
+		}
+
+		function check_notional(name, price, volume) {
+			if (price.isZero()) return; // don't check zero, special case for market buys
+			let quote_volume = price.times(volume);
+			if (quote_volume.isLessThan(minNotional)) {
+				throw new Error(
+					`${name} does not meet minimum order value ${minNotional} (Buy of ${volume} at ${price} = ${quote_volume}).`
+				);
+			}
+		}
+
+		this.amount = munge_and_check_quantity('Amount', this.amount);
+
+		if (this.buyPrice && this.buyPrice !== 0) {
+			this.buyPrice = munge_and_check_price('Buy price', this.buyPrice);
+			check_notional('Buy order', this.buyPrice, this.amount);
+		}
+
+		if (this.stopPrice) {
+			this.stopPrice = munge_and_check_price('Stop price', this.stopPrice);
+
+			if (this.limitPrice) {
+				this.limitPrice = munge_and_check_price('Limit price', this.limitPrice);
+				check_notional('Stop order', this.limitPrice, this.amount);
+			} else {
+				check_notional('Stop order', this.stopPrice, this.amount);
+			}
+		}
+
+		if (this.targetPrice) {
+			this.targetPrice = munge_and_check_price('Target price', this.targetPrice);
+			check_notional('Target order', this.targetPrice, this.amount);
+		}
+
+		const NON_BNB_TRADING_FEE = BigNumber('0.001');
+
+		const calculateSellAmount = function(commissionAsset, sellAmount) {
+			// Adjust sell amount if BNB not used for trading fee
+			return commissionAsset === 'BNB' && !nonBnbFees
+				? sellAmount
+				: sellAmount.times(BigNumber(1).minus(NON_BNB_TRADING_FEE));
+		};
+
+		let stopOrderId = 0;
+		let targetOrderId = 0;
+
+		const sellComplete = function(error, response) {
+			if (error) {
+				throw new Error('Sell error', error.body);
+			}
+
+			console.log('Sell response', response);
+			console.log(`order id: ${response.orderId}`);
+
+			if (!(this.stopPrice && this.targetPrice)) {
+				throw new ExecutionComplete();
+			}
+
+			if (response.type === 'STOP_LOSS_LIMIT') {
+				this.send_message(`${this.pair} stopped out`);
+				stopOrderId = response.orderId;
+			} else if (response.type === 'LIMIT') {
+				this.send_message(`${this.pair} hit target price`);
+				targetOrderId = response.orderId;
+			}
+		};
+
+		async function placeStopOrder() {
+			try {
+				let args = {
+					side: 'SELL',
+					symbol: this.pair,
+					type: 'STOP_LOSS_LIMIT',
+					quantity: this.amount.toFixed(),
+					price: (this.limitPrice || this.stopPrice).toFixed(),
+					stopPrice: this.stopPrice.toFixed()
+					// TODO: more args here, server time and use FULL response body
+				};
+				console.log(`Creating STOP_LOSS_LIMIT SELL ORDER`);
+				console.log(args);
+				let response = await binance_client.order(args);
+				console.log('Buy response', response);
+				console.log(`order id: ${response.orderId}`);
+				return response.orderId;
+			} catch (error) {
+				async_error_handler(console, `error placing order: ${error.body}`, error);
+			}
+		}
+
+		async function placeTargetOrder() {
+			try {
+				let args = {
+					side: 'SELL',
+					symbol: this.pair,
+					type: 'LIMIT',
+					quantity: this.amount.toFixed(),
+					price: this.targetPrice.toFixed()
+					// TODO: more args here, server time and use FULL response body
+				};
+				console.log(`Creating LIMIT SELL ORDER`);
+				console.log(args);
+				let response = await binance_client.order(args);
+				console.log('Buy response', response);
+				console.log(`order id: ${response.orderId}`);
+				return response.orderId;
+			} catch (error) {
+				async_error_handler(console, `error placing order: ${error.body}`, error);
+			}
+		}
+
+		const placeSellOrder = function() {
+			if (this.stopPrice) {
+				placeStopOrder();
+			} else if (this.targetPrice) {
+				placeTargetOrder();
+			} else {
+				throw new ExecutionComplete();
+			}
+		};
+
+		async function create_market_buy_order() {
+			try {
+				let args = {
+					side: 'BUY',
+					symbol: this.pair,
+					type: 'MARKET',
+					quantity: this.amount.toFixed()
+					// TODO: more args here, server time and use FULL response body
+				};
+				console.log(`Creating MARKET BUY ORDER`);
+				// console.log(args);
+				let response = await binance_client.order(args);
+				fsm.buyOrderCreated();
+				console.log('Buy response', response);
+				console.log(`order id: ${response.orderId}`);
+				return response.orderId;
+			} catch (error) {
+				async_error_handler(console, `Buy error: ${error.body}`, error);
+			}
+		}
+
+		async function create_limit_buy_order() {
+			try {
+				let args = {
+					side: 'BUY',
+					symbol: this.pair,
+					type: 'LIMIT',
+					quantity: this.amount.toFixed(),
+					price: this.buyPrice.toFixed()
+					// TODO: more args here, server time and use FULL response body
+				};
+				console.log(`Creating LIMIT BUY ORDER`);
+				console.log(args);
+				let response = await binance_client.order(args);
+				fsm.buyOrderCreated();
+				console.log('Buy response', response);
+				console.log(`order id: ${response.orderId}`);
+				return response.orderId;
+			} catch (error) {
+				async_error_handler(console, `Buy error: ${error.body}`, error);
+			}
+		}
+
+		var buyOrderId = 0;
+		let isLimitEntry = false;
+		let isStopEntry = false;
+
+		var fsm = new StateMachine({
+			init: 'initialising',
+			transitions: [
+				{ name: 'buy_order_created', from: 'initialising', to: 'buy_order_open' }, // A
+				// { name: 'wait_for_entry_price', from: 'initialising', to: 'waiting_for_entry_price' }, // B
+				{ name: 'buy_order_filled', from: 'buy_order_open', to: 'waiting_for_exit_price' } // C
+				// { name: 'buy_order_created', from: 'waiting_for_entry_price', to: 'buy_order_open' }
+			],
+			methods: {
+				// TODO: async?
+				onWaitingForExitPrice: function() {
+					console.log('Entering: waiting_for_exit_price');
+					placeSellOrder();
+				}
+			}
+		});
+
+		if (typeof this.buyPrice !== 'undefined') {
+			if (this.buyPrice.isZero()) {
+				buyOrderId = await create_market_buy_order();
+			} else {
+				buyOrderId = await create_limit_buy_order();
+			}
+		}
+
+		// TODO: I guess it would be good to check how much the balance is on the exchange
+		// against 'amount' if there is no buy stage
+
+		if (fsm.is('initialising')) {
+			throw new Error(`Unable to determine intial state`);
+		}
+
+		// console.log(`BuyPrice: ${this.buyPrice}, isZero(): ${this.buyPrice.isZero()}`);
+		// if (typeof this.buyPrice !== 'undefined') {
+		// 	if (this.buyPrice.isZero()) {
+		// 		buyOrderId = await create_market_buy_order();
+		// TODO: move this code
+		// 	} else if (this.buyPrice.isGreaterThan(0)) {
+		// 		old_binance.prices(this.pair, (error, ticker) => {
+		// 			const currentPrice = ticker[this.pair];
+		// 			console.log(`${this.pair} price: ${currentPrice}`);
+
+		// 				isLimitEntry = true;
+		// 				console.error('needs implementing');
+		// 				throw new Error('backtrace me');
+		// 		});
+		// 	}
+		// } else {
+		// 	placeSellOrder();
+		// }
+
+		let isCancelling = false;
+
+		// TODO: we don't always need this - only if we have cancel/stop/target orders the need monitoring
+		this.closeTradesWebSocket = await binance_client.ws.aggTrades([ this.pair ], async function(trade) {
+			var { s: symbol, p: price } = trade;
+			price = BigNumber(price);
+
+			if (buyOrderId) {
+				// console.log(`${symbol} trade update. price: ${price} buy: ${this.buyPrice}`);
+			} else if (stopOrderId || targetOrderId) {
+				// console.log(`${symbol} trade update. price: ${price} stop: ${this.stopPrice} target: ${this.targetPrice}`);
+				if (stopOrderId && !targetOrderId && price.isGreaterThanOrEqualTo(this.targetPrice) && !isCancelling) {
+					console.log(`Event: price >= targetPrice: cancelling stop and placeTargetOrder()`);
+					isCancelling = true;
+					try {
+						await binance_client.cancelOrder({ symbol, orderId: stopOrderId });
+						isCancelling = false;
+					} catch (error) {
+						console.error(`${symbol} cancel error:`, error.body);
+						return;
+					}
+					stopOrderId = 0;
+					console.log(`${symbol} cancel response:`, response);
+					placeTargetOrder();
+				} else if (
+					targetOrderId &&
+					!stopOrderId &&
+					price.isLessThanOrEqualTo(this.stopPrice) &&
+					!isCancelling
+				) {
+					isCancelling = true;
+					try {
+						await binance_client.cancelOrder({ symbol, orderId: targetOrderId });
+						isCancelling = false;
+					} catch (error) {
+						console.error(`${symbol} cancel error:`, error.body);
+						return;
+					}
+					targetOrderId = 0;
+					console.log(`${symbol} cancel response:`, response);
+					placeStopOrder();
+				}
+			}
+		});
+
+		const checkOrderFilled = function(data, orderFilled) {
+			const { s: symbol, p: price, q: quantity, S: side, o: orderType, i: orderId, X: orderStatus } = data;
+
+			console.log(`${symbol} ${side} ${orderType} ORDER #${orderId} (${orderStatus})`);
+			console.log(`..price: ${price}, quantity: ${quantity}`);
+
+			if (orderStatus === 'NEW' || orderStatus === 'PARTIALLY_FILLED') {
+				return;
+			}
+
+			if (orderStatus !== 'FILLED') {
+				throw new Error(`Order ${orderStatus}. Reason: ${data.r}`);
+			}
+
+			orderFilled(data);
+		};
+	}
+}
+
+module.exports = Algo;

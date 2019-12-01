13532724b39b03718ad7f06217c3f49f46dc1dbf
Ryan Cocks <ryan@ryancocks.net>
2019-12-01 14:48:50 +0200

    Add telegram details using commandline

--- run-in-k8.sh
+++ run-in-k8.sh
@@ -6,4 +6,10 @@ set -xe
 
 kubectl config set-context ${KUBECTL_CONTEXT}
 
-kubectl run --generator=run-pod/v1 test-job-binance-tool --rm -i --tty --image ${DOCKER_REGISTRY}
+kubectl run \
+    --generator=run-pod/v1 \
+    test-job-binance-tool \
+    --rm -i --tty --image ${DOCKER_REGISTRY} \
+    --env="TELEGRAM_KEY=$TELEGRAM_KEY" \
+    --env="TELEGRAM_CHAT_ID=$TELEGRAM_CHAT_ID" \
+    # end

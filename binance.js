eef56252a48b8953da2ea6e4c6e36cba0734c07d
Ryan Cocks <ryan@ryancocks.net>
2019-12-02 17:51:59 +0200

    Whitespace

--- binance.js
+++ binance.js
@@ -2,159 +2,180 @@
 /* eslint-disable no-console */
 /* eslint func-names: ["warn", "as-needed"] */
 
-require('dotenv').config();
+require("dotenv").config();
 
 // TODO: convert all the process.exit calls to be exceptions
 // TODO: add watchdog on trades stream - it can stop responding without realising
 // TODO: - in the original implementations
 
-const Binance = require('binance-api-node').default;
-const send_message = require('./lib/telegram.js');
-const Algo = require('./service_lib/algo');
-const Logger = require('./lib/faux_logger');
-const BigNumber = require('bignumber.js');
-const TradingRules = require('./lib/trading_rules');
+const Binance = require("binance-api-node").default;
+const send_message = require("./lib/telegram.js");
+const Algo = require("./service_lib/algo");
+const Logger = require("./lib/faux_logger");
+const BigNumber = require("bignumber.js");
+const TradingRules = require("./lib/trading_rules");
 
 const logger = new Logger({ silent: false });
 
 const trading_rules = new TradingRules({
-	max_allowed_portfolio_loss_percentage_per_trade: BigNumber('1'),
-	allowed_to_trade_without_stop: true
+  max_allowed_portfolio_loss_percentage_per_trade: BigNumber("1"),
+  allowed_to_trade_without_stop: true
 });
 
-const { argv } = require('yargs')
-	.usage('Usage: $0')
-	.example(
-		'$0 -p BNBBTC -a 1 -b 0.002 -s 0.001 -t 0.003',
-		'Place a buy order for 1 BNB @ 0.002 BTC. Once filled, place a stop-limit sell @ 0.001 BTC. If a price of 0.003 BTC is reached, cancel stop-limit order and place a limit sell @ 0.003 BTC.'
-	)
-	// '-p <tradingPair>'
-	.demand('pair')
-	.alias('p', 'pair')
-	.describe('p', 'Set trading pair eg. BNBBTC')
-	// '-a <base_amount>'
-	.string('a')
-	.alias('a', 'base_amount')
-	.describe('a', 'Set base_amount to buy/sell')
-	// '-q <quote_amount>'
-	.string('q')
-	.alias('q', 'amountquote')
-	.describe('q', 'Set max to buy in quote coin (alternative to -a)')
-	// '-b <buy_price>'
-	.string('b')
-	.alias('b', 'buy')
-	.alias('b', 'e')
-	.alias('b', 'entry')
-	.describe('b', 'Set buy price (omit price for market buy)')
-	// '-s <stop_price>'
-	.string('s')
-	.alias('s', 'stop')
-	.describe('s', 'Set stop-limit order stop price')
-	// '-l <limit_price>'
-	.string('l')
-	.alias('l', 'limit')
-	.describe('l', 'Set sell stop-limit order limit price (if different from stop price)')
-	// '-t <target_price>'
-	.string('t')
-	.alias('t', 'target')
-	.describe('t', 'Set target limit order sell price')
-	// '--soft-entry'
-	.boolean('soft-entry')
-	.describe('soft-entry', 'Wait until the buy price is hit before creating the limit buy order')
-	.default('soft-entry', false)
-	// '--auto-size'
-	.boolean('auto-size')
-	.describe('auto-size', 'Automatically size the trade based on stopLoss % and available funds')
-	.default('auto-size', false)
-	// '--percentages'
-	.boolean('percentages')
-	.describe('percentages', 'Print trade stats and exit')
-	.default('percentages', false)
-	// '--non-bnb-fees'
-	.boolean('F')
-	.alias('F', 'non-bnb-fees')
-	.describe('F', 'Calculate stop/target sell amounts assuming not paying fees using BNB')
-	.default('F', false);
+const { argv } = require("yargs")
+  .usage("Usage: $0")
+  .example(
+    "$0 -p BNBBTC -a 1 -b 0.002 -s 0.001 -t 0.003",
+    "Place a buy order for 1 BNB @ 0.002 BTC. Once filled, place a stop-limit sell @ 0.001 BTC. If a price of 0.003 BTC is reached, cancel stop-limit order and place a limit sell @ 0.003 BTC."
+  )
+  // '-p <tradingPair>'
+  .demand("pair")
+  .alias("p", "pair")
+  .describe("p", "Set trading pair eg. BNBBTC")
+  // '-a <base_amount>'
+  .string("a")
+  .alias("a", "base_amount")
+  .describe("a", "Set base_amount to buy/sell")
+  // '-q <quote_amount>'
+  .string("q")
+  .alias("q", "amountquote")
+  .describe("q", "Set max to buy in quote coin (alternative to -a)")
+  // '-b <buy_price>'
+  .string("b")
+  .alias("b", "buy")
+  .alias("b", "e")
+  .alias("b", "entry")
+  .describe("b", "Set buy price (omit price for market buy)")
+  // '-s <stop_price>'
+  .string("s")
+  .alias("s", "stop")
+  .describe("s", "Set stop-limit order stop price")
+  // '-l <limit_price>'
+  .string("l")
+  .alias("l", "limit")
+  .describe(
+    "l",
+    "Set sell stop-limit order limit price (if different from stop price)"
+  )
+  // '-t <target_price>'
+  .string("t")
+  .alias("t", "target")
+  .describe("t", "Set target limit order sell price")
+  // '--soft-entry'
+  .boolean("soft-entry")
+  .describe(
+    "soft-entry",
+    "Wait until the buy price is hit before creating the limit buy order"
+  )
+  .default("soft-entry", false)
+  // '--auto-size'
+  .boolean("auto-size")
+  .describe(
+    "auto-size",
+    "Automatically size the trade based on stopLoss % and available funds"
+  )
+  .default("auto-size", false)
+  // '--percentages'
+  .boolean("percentages")
+  .describe("percentages", "Print trade stats and exit")
+  .default("percentages", false)
+  // '--non-bnb-fees'
+  .boolean("F")
+  .alias("F", "non-bnb-fees")
+  .describe(
+    "F",
+    "Calculate stop/target sell amounts assuming not paying fees using BNB"
+  )
+  .default("F", false);
 
 let {
-	p: pair,
-	a: base_amount,
-	q: max_quote_amount_to_buy,
-	b: buy_price,
-	s: stop_price,
-	l: limit_price,
-	t: target_price,
-	F: nonBnbFees,
-	'soft-entry': soft_entry,
-	'auto-size': auto_size,
-	percentages: percentages
+  p: pair,
+  a: base_amount,
+  q: max_quote_amount_to_buy,
+  b: buy_price,
+  s: stop_price,
+  l: limit_price,
+  t: target_price,
+  F: nonBnbFees,
+  "soft-entry": soft_entry,
+  "auto-size": auto_size,
+  percentages: percentages
 } = argv;
 
-if (buy_price === '') {
-	buy_price = '0';
+if (buy_price === "") {
+  buy_price = "0";
 }
 
 var ee;
 const live = false;
-if(live ){
-	ee = Binance({
-		apiKey: process.env.APIKEY,
-		apiSecret: process.env.APISECRET
-		// getTime: xxx // time generator function, optional, defaults to () => Date.now()
-	});
+if (live) {
+  logger.info("Live trading mode");
+  ee = Binance({
+    apiKey: process.env.APIKEY,
+    apiSecret: process.env.APISECRET
+    // getTime: xxx // time generator function, optional, defaults to () => Date.now()
+  });
 } else {
-	const fs = require("fs")
-	const exchange_info = JSON.parse(fs.readFileSync('./test/exchange_info.json', 'utf8'));
-	let ee_config = {
-		starting_balances: {
-			'USDT': BigNumber('50')
-		},
-		logger,
-		exchange_info
-	}
-	const ExchangeEmulator = require('./lib/exchange_emulator');
-	ee = new ExchangeEmulator(ee_config);
+  logger.info("Emulated trading mode");
+  const fs = require("fs");
+  const exchange_info = JSON.parse(
+    fs.readFileSync("./test/exchange_info.json", "utf8")
+  );
+  let ee_config = {
+    starting_balances: {
+      USDT: BigNumber("50")
+    },
+    logger,
+    exchange_info
+  };
+  const ExchangeEmulator = require("./lib/exchange_emulator");
+  ee = new ExchangeEmulator(ee_config);
 }
 
 const algo = new Algo({
-	ee,
-	send_message,
-	logger,
-	pair,
-	base_amount,
-	max_quote_amount_to_buy,
-	buy_price,
-	stop_price,
-	limit_price,
-	target_price,
-	nonBnbFees,
-	soft_entry,
-	trading_rules,
-	auto_size,
-	percentages
+  ee,
+  send_message,
+  logger,
+  pair,
+  base_amount,
+  max_quote_amount_to_buy,
+  buy_price,
+  stop_price,
+  limit_price,
+  target_price,
+  nonBnbFees,
+  soft_entry,
+  trading_rules,
+  auto_size,
+  percentages
 });
 
-const execSync = require('child_process').execSync;
-code = execSync('date -u >&2');
+const execSync = require("child_process").execSync;
+code = execSync("date -u >&2");
 
-algo.main().catch((error) => {
-	if (error.name && error.name === 'FetchError') {
-		logger.error(`${error.name}: Likely unable to connect to Binance and/or Telegram: ${error}`);
-	} else if (error.message && error.message.includes('exception in setup code')) {
-		logger.error(`Error setting up trade, exiting.`);
-	} else {
-		logger.error(`Error in main loop: ${error}`);
-		logger.error(error);
-		logger.error(`Error in main loop: ${error.stack}`);
-		send_message(`${pair}: Error in main loop: ${error}`);
-	}
-	soft_exit();
+algo.main().catch(error => {
+  if (error.name && error.name === "FetchError") {
+    logger.error(
+      `${error.name}: Likely unable to connect to Binance and/or Telegram: ${error}`
+    );
+  } else if (
+    error.message &&
+    error.message.includes("exception in setup code")
+  ) {
+    logger.error(`Error setting up trade, exiting.`);
+  } else {
+    logger.error(`Error in main loop: ${error}`);
+    logger.error(error);
+    logger.error(`Error in main loop: ${error.stack}`);
+    send_message(`${pair}: Error in main loop: ${error}`);
+  }
+  soft_exit();
 });
 
 // Note this method returns!
 // Shuts down everything that's keeping us alive so we exit
 function soft_exit(exit_code) {
-	algo.shutdown_streams();
-	if (exit_code) process.exitCode = exit_code;
-	// setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
+  algo.shutdown_streams();
+  if (exit_code) process.exitCode = exit_code;
+  // setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
 }

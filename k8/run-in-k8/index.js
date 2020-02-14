cabd11164b0a91ef2d3a66fc032e04bd0611a64e
Ryan Cocks <ryan@ryancocks.net>
2020-02-14 05:57:07 +0000

    Take trade id as param

--- k8/run-in-k8/index.js
+++ k8/run-in-k8/index.js
@@ -1,52 +1,57 @@
-const assert = require('assert');
+const assert = require("assert");
 
 // Configuration
-const dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' });
+const dotenv = require("dotenv");
+dotenv.config({ path: "../../.env" });
 // End Config
 
 function fall_over(string) {
-	console.log(`ERROR: ` + string);
-	process.exit(1);
+  console.log(`ERROR: ` + string);
+  process.exit(1);
 }
 
-const fs = require('fs');
-const Handlebars = require('handlebars');
+const fs = require("fs");
+const Handlebars = require("handlebars");
 
-const template_string = fs.readFileSync('template.handlebars', 'utf8');
+const template_string = fs.readFileSync("template.handlebars", "utf8");
 const template = Handlebars.compile(template_string);
 
-const YAML = require('yaml');
+const YAML = require("yaml");
 // const vars = YAML.parse(fs.readFileSync(process.env.VARS_INPUT_FILENAME, 'utf8'));
 // const vault = YAML.parse(fs.readFileSync(process.env.VAULT_INPUT_FILENAME, 'utf8'));
 
 function indent_cert(string) {
-	return string.trim().split('\n').map((line) => `  ${line}`).join('\n');
+  return string
+    .trim()
+    .split("\n")
+    .map(line => `  ${line}`)
+    .join("\n");
 }
 
-const namespace = process.env.KUBECTL_NAMESPACE
-const container_name = "binance-tool"
-const job_name = "binance-tool-dev-job"
-const docker_image = process.env.DOCKER_REGISTRY
-const trade_id = "1"
+const namespace = process.env.KUBECTL_NAMESPACE;
+const container_name = "binance-tool";
+const job_name = "binance-tool-dev-job";
+const docker_image = process.env.DOCKER_REGISTRY;
+const trade_id = process.env.TRADE_ID;
+
+assert(trade_id);
 
 const contents = template({
-    docker_image,
-    container_name,
-    trade_id
+  docker_image,
+  container_name,
+  trade_id
 });
 
 const output_dir = `deployed-trades`;
-const execSync = require('child_process').execSync;
+const execSync = require("child_process").execSync;
 code = execSync(`mkdir -p ${output_dir}`);
 const output_filename = `${output_dir}/${job_name}.yaml`;
 
-fs.writeFileSync(output_filename, contents, (err) => {
-    if (err) {
-        return console.error(`Autsch! Failed to store template: ${err.message}.`);
-    }
-    console.log(`Saved ${output_filename}`);
+fs.writeFileSync(output_filename, contents, err => {
+  if (err) {
+    return console.error(`Autsch! Failed to store template: ${err.message}.`);
+  }
+  console.log(`Saved ${output_filename}`);
 });
 
 code = execSync(`kubectl apply --namespace ${namespace} -f ${output_filename}`);
-
--- run-in-k8.sh
+++ run-in-k8.sh
@@ -6,5 +6,15 @@ set -xe
 
 kubectl config set-context ${KUBECTL_CONTEXT}
 
+export TRADE_ID=$1
+
+if [[ -z "$TRADE_ID" ]] 
+then
+    echo "Error: Supply trade ID"
+    exit 1
+else 
+    echo "Trade ID $TRADE_ID"
+fi
+
 cd k8/run-in-k8
 node index.js

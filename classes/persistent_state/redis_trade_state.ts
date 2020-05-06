cd978549724fce33626ed9f7764bb0dff81e6e2a
Ryan Cocks <ryan@ryancocks.net>
2020-05-06 18:24:10 +0100

    More asserts setting keys

--- classes/persistent_state/redis_trade_state.ts
+++ classes/persistent_state/redis_trade_state.ts
@@ -100,6 +100,8 @@ export class TradeState {
     if (value === 'undefined') {
       throw new Error(`Redis error: attempt to set key ${key} to the string 'undefined'`)
     }
+    // assert key is in our namespace:
+    assert(key.startsWith(`trades:${this.trade_id}:`), `Attempt to set key outside namespace: ${key}`)
     const ret = await this._set_redis_key(key, value)
     if (ret !== 'OK') {
       throw new Error(`Redis error: failed to set key ${key}: ${ret}`)

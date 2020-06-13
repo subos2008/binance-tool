ddaecdea5ff1e21f832f2a6bdc038f5f567b1258
Ryan Cocks <ryan@ryancocks.net>
2020-06-13 14:31:08 +0100

    Move Sentry to typescript

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,8 +1,10 @@
-export default Sentry = require("@sentry/node");
+const Sentry = require("@sentry/node");
 
 Sentry.init({
   dsn: "https://5f5398dfd6b0475ea6061cf39bc4ed03@sentry.io/5178400"
 });
+
+export default Sentry
 
 // Sentry.configureScope(function(scope) {
 //   scope.setTag("service", "my value");

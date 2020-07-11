0ad3bc8819f0aead0e3d0097877f954c48cfb427
Ryan Cocks <ryan@ryancocks.net>
2020-07-11 17:31:36 +0200

    redis watchdog per subsystem improvements

--- classes/persistent_state/redis_watchdog.ts
+++ classes/persistent_state/redis_watchdog.ts
@@ -10,7 +10,7 @@ export class RedisWatchdog {
   watchdog_name: string;
   timeout_seconds: number
   set_redis_key: any;
-  timeout_obj: any
+  timeout_objs: any
 
   constructor({ logger, redis, watchdog_name, timeout_seconds }: { logger: Logger, redis: RedisClient, watchdog_name: string, timeout_seconds: number }) {
     assert(logger);
@@ -21,11 +21,15 @@ export class RedisWatchdog {
     this.watchdog_name = watchdog_name;
     assert(timeout_seconds);
     this.timeout_seconds = timeout_seconds;
+    this.timeout_objs = {}
 
     this.set_redis_key = promisify(this.redis.set).bind(this.redis);
 
-    console.log(`Created redis watchdog for ${this.watchdog_name} expiring at ${this.timeout_seconds}, redis key '${this.get_key()}'`)
-    this._reset_timer()
+    console.log(`Created redis watchdog for ${this.watchdog_name} expiring each ${this.timeout_seconds} seconds, redis key '${this.get_key()}'`)
+    // set a timeout that will make an entry in the logs but don't create the key in redis
+    // this is beacuse we don't want a process that continually restarts without a ping to look
+    // healthy in redis
+    this._reset_timer("default")
   }
 
   get_key() {
@@ -37,6 +41,7 @@ export class RedisWatchdog {
     if (result !== "OK") {
       this.logger.warn(`Setting redis watchdog failed: ${result}`)
     }
+    this._reset_timer("default")
   }
 
   async reset_subsystem(subsystem_name: string): Promise<void> {
@@ -44,16 +49,18 @@ export class RedisWatchdog {
     if (result !== "OK") {
       this.logger.warn(`Setting redis watchdog failed: ${result}`)
     }
+    this._reset_timer(subsystem_name)
+
   }
 
-  _reset_timer() {
-    if (this.timeout_obj) {
-      clearTimeout(this.timeout_obj);
-      this.timeout_obj = null
+  _reset_timer(subsystem: string) {
+    if (this.timeout_objs[subsystem]) {
+      clearTimeout(this.timeout_objs[subsystem]);
+      this.timeout_objs[subsystem] = null
     }
-    this.timeout_obj = setTimeout(() => {
-      console.error(`Watchdog timer ${this.watchdog_name} expired!`);
+    this.timeout_objs[subsystem] = setTimeout(() => {
+      console.error(`Watchdog timer ${this.watchdog_name}:${subsystem} expired!`);
     }, this.timeout_seconds * 1000);
-    this.timeout_obj.unref() // stop the watchdog from keeping the process alive
+    this.timeout_objs[subsystem].unref() // stop the watchdog from keeping the process alive
   }
 }

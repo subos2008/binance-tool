4003f0e49dd782e7652d3f82fa8e493bf3c332ae
Ryan Cocks <ryan@ryancocks.net>
2021-02-01 22:07:02 +0200

    aqmp not https error to become warn

--- lib/amqp/connect_options.ts
+++ lib/amqp/connect_options.ts
@@ -8,6 +8,10 @@ const amqp_user = process.env.AMQP_USER // || "binance-tool";
 const amqp_password = process.env.AMQP_PASSWORD;
 const protocol = process.env.AMQP_PROTOCOL // || "amqps";
 
+const Logger = require("../../lib/faux_logger");
+// Initial logger, we re-create it below once we have the trade_id
+var logger = new Logger({ silent: false });
+
 assert(protocol)
 assert(amqp_password)
 assert(amqp_user)
@@ -15,7 +19,7 @@ assert(vhost)
 assert(amqp_server)
 
 if (protocol !== 'amqps') {
-  console.error(`Connection to AMQP Server ${amqp_server} is not using https: ${protocol}`)
+  logger.warn(`Connection to AMQP Server ${amqp_server} is not using https: ${protocol}`)
 }
 
 const port = 5672
@@ -32,6 +36,6 @@ const connection_options = {
   // ca: splitca("./ca-certificates.crt")
 }
 
-console.log(`AMQP hostname: ${amqp_server} port: ${port} protocol: ${protocol}`)
+logger.info(`AMQP hostname: ${amqp_server} port: ${port} protocol: ${protocol}`)
 
 export default connection_options;

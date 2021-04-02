e587a83ddc086f70c3333e03fb38f917f2ddb8e4
Ryan Cocks <ryan@ryancocks.net>
2021-04-02 10:54:25 +0300

    Make order_created optional

--- service_lib/order_execution_tracker.ts
+++ service_lib/order_execution_tracker.ts
@@ -112,7 +112,7 @@ export class OrderExecutionTracker {
       // Originally orders were all first added here but as we re-architect they will become
       // more likely to pre-exist
       if (this.order_state) await this.order_state.add_new_order(orderId, { symbol, side, orderType })
-      if (this.order_callbacks) await this.order_callbacks.order_created(orderId, data)
+      if (this.order_callbacks && this.order_callbacks.order_created) await this.order_callbacks.order_created(orderId, data)
       return;
     }
 

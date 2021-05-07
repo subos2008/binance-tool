28da7a013005b2d77b8d55623c9f3439d020ec58
Ryan Cocks <ryan@ryancocks.net>
2021-05-07 23:02:14 +0300

    add initial->final close price message in position-tracker

--- classes/position.ts
+++ classes/position.ts
@@ -1,10 +1,10 @@
-import { strict as assert } from "assert";
-import { Logger } from "../interfaces/logger";
+import { strict as assert } from "assert"
+import { Logger } from "../interfaces/logger"
 
-import * as Sentry from "@sentry/node";
+import * as Sentry from "@sentry/node"
 
-import { RedisPositionsState } from "../classes/persistent_state/redis_positions_state";
-import { PositionIdentifier } from "../events/shared/position-identifier";
+import { RedisPositionsState } from "../classes/persistent_state/redis_positions_state"
+import { PositionIdentifier } from "../events/shared/position-identifier"
 
 /*
 { exchange: 'binance', account: 'default', symbol: 'XTZBNB' }
@@ -15,68 +15,84 @@ import { PositionIdentifier } from "../events/shared/position-identifier";
 }
 */
 
-import { BigNumber } from "bignumber.js";
-BigNumber.DEBUG = true; // Prevent NaN
+import { BigNumber } from "bignumber.js"
+BigNumber.DEBUG = true // Prevent NaN
 // Prevent type coercion
 BigNumber.prototype.valueOf = function () {
-  throw Error("BigNumber .valueOf called!");
-};
+  throw Error("BigNumber .valueOf called!")
+}
 
 export class Position {
-  logger: Logger;
-  ee: any;
-  redis_positions: RedisPositionsState;
-  position_identifier: PositionIdentifier;
-  object: any | undefined;
-  prices: { [key: string]: string } = {};
+  logger: Logger
+  ee: any
+  redis_positions: RedisPositionsState
+  position_identifier: PositionIdentifier
+  object: any | undefined
+  prices: { [key: string]: string } = {}
 
   constructor({
     logger,
     redis_positions,
     position_identifier,
   }: {
-    logger: Logger;
-    redis_positions: RedisPositionsState;
-    position_identifier: PositionIdentifier;
+    logger: Logger
+    redis_positions: RedisPositionsState
+    position_identifier: PositionIdentifier
   }) {
-    this.logger = logger;
-    this.redis_positions = redis_positions;
-    this.position_identifier = position_identifier;
+    this.logger = logger
+    this.redis_positions = redis_positions
+    this.position_identifier = position_identifier
+  }
+
+  get tuple() {
+    return {
+      symbol: this.symbol,
+      exchange: this.position_identifier.exchange_identifier.exchange,
+      account: this.position_identifier.exchange_identifier.account,
+    }
   }
 
   get symbol(): string {
-    return this.position_identifier.symbol;
+    return this.position_identifier.symbol
   }
 
   get initial_entry_price(): BigNumber | null {
-    return new BigNumber(this.object?.initial_entry_price);
+    return new BigNumber(this.object?.initial_entry_price)
   }
 
+  // TODO: avg entry price derived from netQuote change and position size?
+  // then make sure netQuote change is tracked properly
+
   get current_price(): BigNumber | undefined {
-    return new BigNumber(this.prices[this.symbol]);
+    return this.prices[this.symbol] ? new BigNumber(this.prices[this.symbol]) : undefined
+  }
+
+  get position_size(): BigNumber | undefined {
+    return this.object?.position_size ? new BigNumber(this.object.position_size) : undefined
   }
 
   get percentage_price_change_since_initial_entry(): BigNumber | undefined {
-    if(!this.initial_entry_price) throw new Error(`initial_entry_price unknown`)
-    if(!this.current_price) throw new Error(`current_price unknown`)
-    return this.current_price.minus(this.initial_entry_price).dividedBy(this.initial_entry_price).times(100)
+    if (!this.initial_entry_price) throw new Error(`initial_entry_price unknown`)
+    if (!this.current_price) throw new Error(`current_price unknown`)
+    return this.current_price
+      .minus(this.initial_entry_price)
+      .dividedBy(this.initial_entry_price)
+      .times(100)
   }
 
   async load_and_init({ prices }: { prices: { [key: string]: string } }) {
-    this.prices = prices;
-    this.object = await this.describe_position();
+    this.prices = prices
+    this.object = await this.describe_position()
   }
 
   async describe_position(): Promise<{
-    position_size: BigNumber | undefined;
-    initial_entry_price: BigNumber | undefined;
-    netQuoteBalanceChange: BigNumber | undefined;
-    current_price?: string;
+    position_size: BigNumber | undefined
+    initial_entry_price: BigNumber | undefined
+    netQuoteBalanceChange: BigNumber | undefined
+    current_price?: string
   }> {
-    const object: any = this.redis_positions.describe_position(
-      this.position_identifier
-    );
-    if (this.prices) object.current_price = this.current_price;
-    return object;
+    const object: any = this.redis_positions.describe_position(this.position_identifier)
+    if (this.prices) object.current_price = this.current_price
+    return object
   }
 }
--- services/position-tracker/position-tracker.ts
+++ services/position-tracker/position-tracker.ts
@@ -1,48 +1,79 @@
-import { strict as assert } from 'assert';
+import { strict as assert } from "assert"
 
-import { RedisClient } from 'redis';
+import { RedisClient } from "redis"
 
-import { Logger } from '../../interfaces/logger'
-import { GenericOrderData } from '../../types/exchange_neutral/generic_order_data'
-import { RedisPositionsState } from '../../classes/persistent_state/redis_positions_state'
-import { PositionPublisher } from '../../classes/amqp/positions-publisher'
+import { Logger } from "../../interfaces/logger"
+import { GenericOrderData } from "../../types/exchange_neutral/generic_order_data"
+import { RedisPositionsState } from "../../classes/persistent_state/redis_positions_state"
+import { PositionPublisher } from "../../classes/amqp/positions-publisher"
 
-import BigNumber from "bignumber.js";
-import { timeStamp } from 'console';
-BigNumber.DEBUG = true; // Prevent NaN
+import BigNumber from "bignumber.js"
+import { timeStamp } from "console"
+BigNumber.DEBUG = true // Prevent NaN
 // Prevent type coercion
 BigNumber.prototype.valueOf = function () {
-  throw Error("BigNumber .valueOf called!");
-};
+  throw Error("BigNumber .valueOf called!")
+}
 
-import * as Sentry from '@sentry/node';
+import * as Sentry from "@sentry/node"
+import { PositionIdentifier } from "../../events/shared/position-identifier"
+import { Position } from "../../classes/position"
 
-type check_func = ({ volume, symbol: string }: { price: BigNumber, volume: BigNumber, symbol: string }) => boolean
+type check_func = ({
+  volume,
+  symbol: string,
+}: {
+  price: BigNumber
+  volume: BigNumber
+  symbol: string
+}) => boolean
 export class PositionTracker {
-  send_message: Function;
-  logger: Logger;
-  positions_state: RedisPositionsState;
+  send_message: Function
+  logger: Logger
+  positions_state: RedisPositionsState
   position_publisher: PositionPublisher
-  close_position_check_func: check_func;
+  close_position_check_func: check_func
 
   constructor({
     send_message,
-    logger, redis, close_position_check_func
-  }: { send_message: (msg: string) => void, logger: Logger, redis: RedisClient, close_position_check_func: check_func }) {
-    assert(logger);
-    this.logger = logger;
-    assert(send_message);
-    this.send_message = send_message;
+    logger,
+    redis,
+    close_position_check_func,
+  }: {
+    send_message: (msg: string) => void
+    logger: Logger
+    redis: RedisClient
+    close_position_check_func: check_func
+  }) {
+    assert(logger)
+    this.logger = logger
+    assert(send_message)
+    this.send_message = send_message
     this.positions_state = new RedisPositionsState({ logger, redis })
-    this.position_publisher = new PositionPublisher({ logger, send_message, broker_name: 'binance' })
+    this.position_publisher = new PositionPublisher({
+      logger,
+      send_message,
+      broker_name: "binance",
+    })
     assert(close_position_check_func)
-    this.close_position_check_func = close_position_check_func;
+    this.close_position_check_func = close_position_check_func
   }
 
   async buy_order_filled({ generic_order_data }: { generic_order_data: GenericOrderData }) {
-    let { symbol, exchange, account, averageExecutionPrice, totalBaseTradeQuantity, totalQuoteTradeQuantity } = generic_order_data
-    if (!account) account = 'default'
-    let position_size: BigNumber = await this.positions_state.get_position_size({ exchange, account, symbol })
+    let {
+      symbol,
+      exchange,
+      account,
+      averageExecutionPrice,
+      totalBaseTradeQuantity,
+      totalQuoteTradeQuantity,
+    } = generic_order_data
+    if (!account) account = "default"
+    let position_size: BigNumber = await this.positions_state.get_position_size({
+      exchange,
+      account,
+      symbol,
+    })
 
     // 1. Is this a new position?
     if (position_size.isZero()) {
@@ -51,79 +82,151 @@ export class PositionTracker {
       } catch (error) {
         console.error(error)
         Sentry.withScope(function (scope) {
-          scope.setTag("symbol", symbol);
-          scope.setTag("exchange", exchange);
-          if (account) scope.setTag("account", account);
-          Sentry.captureException(error);
-        });
+          scope.setTag("symbol", symbol)
+          scope.setTag("exchange", exchange)
+          if (account) scope.setTag("account", account)
+          Sentry.captureException(error)
+        })
       }
 
       // 1.1 create a new position and record the entry price and timestamp
       let initial_entry_price: BigNumber | undefined
       try {
-        initial_entry_price = averageExecutionPrice ? new BigNumber(averageExecutionPrice) : undefined
-        this.positions_state.create_new_position({ symbol, exchange, account }, {
-          position_size: new BigNumber(totalBaseTradeQuantity),
-          initial_entry_price,
-          quote_invested: new BigNumber(0).minus(totalQuoteTradeQuantity)
-        })
+        initial_entry_price = averageExecutionPrice
+          ? new BigNumber(averageExecutionPrice)
+          : undefined
+        this.positions_state.create_new_position(
+          { symbol, exchange, account },
+          {
+            position_size: new BigNumber(totalBaseTradeQuantity),
+            initial_entry_price,
+            quote_invested: new BigNumber(0).minus(totalQuoteTradeQuantity),
+          }
+        )
       } catch (error) {
         console.error(error)
         Sentry.withScope(function (scope) {
-          scope.setTag("symbol", symbol);
-          scope.setTag("exchange", exchange);
-          if (account) scope.setTag("account", account);
-          Sentry.captureException(error);
-        });
+          scope.setTag("symbol", symbol)
+          scope.setTag("exchange", exchange)
+          if (account) scope.setTag("account", account)
+          Sentry.captureException(error)
+        })
       }
 
       // Publish an event declaring the new position
       try {
-        this.position_publisher.publish_new_position_event({ event_type: 'NewPositionEvent', exchange_identifier: { exchange, account }, symbol, position_base_size: totalBaseTradeQuantity, position_initial_quote_spent: totalQuoteTradeQuantity, position_initial_entry_price: initial_entry_price?.toFixed() })
+        this.position_publisher.publish_new_position_event({
+          event_type: "NewPositionEvent",
+          exchange_identifier: { exchange, account },
+          symbol,
+          position_base_size: totalBaseTradeQuantity,
+          position_initial_quote_spent: totalQuoteTradeQuantity,
+          position_initial_entry_price: initial_entry_price?.toFixed(),
+        })
       } catch (error) {
         console.error(error)
         Sentry.withScope(function (scope) {
-          scope.setTag("symbol", symbol);
-          scope.setTag("exchange", exchange);
-          if (account) scope.setTag("account", account);
-          Sentry.captureException(error);
-        });
+          scope.setTag("symbol", symbol)
+          scope.setTag("exchange", exchange)
+          if (account) scope.setTag("account", account)
+          Sentry.captureException(error)
+        })
       }
     } else {
       // 1.2 if existing position just increase the position size
       // not sure what do do about entry price adjustments yet
       this.send_message(`Existing position found for ${symbol}, size ${position_size}`)
-      this.positions_state.increase_position_size_by({ symbol, exchange, account }, new BigNumber(totalBaseTradeQuantity))
+      this.positions_state.increase_position_size_by(
+        { symbol, exchange, account },
+        new BigNumber(totalBaseTradeQuantity)
+      )
     }
 
     // 3. Fire a position changed event or call a callback so we can add auto-exit 10@10 orders
     // A new service called trading-rules-auto-position-exits
   }
 
+  private async load_position_for_order(generic_order_data: GenericOrderData): Promise<Position> {
+    let { symbol, exchange, account, averageExecutionPrice } = generic_order_data
+
+    if (!account) account = "default" // TODO
+    let position_identifier: PositionIdentifier = {
+      exchange_identifier: { exchange, account },
+      symbol,
+    }
+    let position = new Position({
+      logger: this.logger,
+      redis_positions: this.positions_state,
+      position_identifier,
+    })
+    let prices: { [key: string]: string } = {}
+    if (averageExecutionPrice) prices[symbol] = averageExecutionPrice
+    await position.load_and_init({ prices })
+    return position
+  }
+
   async sell_order_filled({ generic_order_data }: { generic_order_data: GenericOrderData }) {
+    let {
+      symbol,
+      exchange,
+      account,
+      averageExecutionPrice,
+      totalBaseTradeQuantity,
+      totalQuoteTradeQuantity, // TODO: use this
+    } = generic_order_data
+
+    if (!account) account = "default" // TODO
+    let position_size: BigNumber = await this.positions_state.get_position_size({
+      exchange,
+      account,
+      symbol,
+    })
+
     // 1. Is this an existing position?
-    let { symbol, exchange, account } = generic_order_data
-    if (!account) account = 'default'
-    let position_size: BigNumber = await this.positions_state.get_position_size({ exchange, account, symbol })
-    if ((position_size).isZero()) {
+    if (position_size.isZero()) {
       this.send_message(`Sell executed on unknown position for ${symbol}`)
-    } else {
-      // 1.2 if existing position just decrease the position size
-      // not sure what do do about partial exits atm
-      let msg = `reducing the position size for ${symbol}`
+      return // this is our NOP
+    }
+
+    // 1.2 if existing position decrease the position size or close the position
+    // TODO: also maintain net quote
+    let msg = `reduced the position size for ${symbol}`
+    this.send_message(msg)
+
+    position_size = new BigNumber(
+      await this.positions_state.decrease_position_size_by(
+        { symbol, exchange, account },
+        new BigNumber(totalBaseTradeQuantity)
+      )
+    )
+
+    if (!averageExecutionPrice) {
+      // TODO: set sentry context after unpacking the order (withScope)
+      let msg = `averageExecutionPrice not supplied, unable to determine if ${symbol} position should be closed.`
+      Sentry.captureMessage(msg)
       this.send_message(msg)
-      let { averageExecutionPrice, totalBaseTradeQuantity, totalQuoteTradeQuantity } = generic_order_data
-      position_size = new BigNumber(await this.positions_state.decrease_position_size_by({ symbol, exchange, account }, new BigNumber(totalBaseTradeQuantity)))
-      if (averageExecutionPrice) {
-        if (this.close_position_check_func({ symbol, volume: position_size, price: new BigNumber(averageExecutionPrice) })) {
-          this.positions_state.close_position({ symbol, exchange, account }).then(() => {
-            this.send_message(`closed position: ${symbol}`)
-          })
-        }
-      } else {
-        Sentry.captureMessage(`averageExecutionPrice not supplied, unable to determine if position should be closed.`)
-      }
+      return
+    }
+
+    // 1.3
+    if (
+      this.close_position_check_func({
+        symbol,
+        volume: position_size,
+        price: new BigNumber(averageExecutionPrice),
+      })
+    ) {
+      this._close_position(await this.load_position_for_order(generic_order_data))
     }
   }
-}
 
+  private async _close_position(position: Position) {
+    let msg = `${position.symbol} traded from ${position.initial_entry_price} to ${position.current_price}: ${position.percentage_price_change_since_initial_entry}% change.`
+    Sentry.captureMessage(msg)
+    this.send_message(msg)
+
+    this.positions_state.close_position(position.tuple).then(() => {
+      this.send_message(`closed position: ${position.symbol}`)
+    })
+  }
+}

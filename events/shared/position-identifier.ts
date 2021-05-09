264242a97e54fa731ba8c25ede1bc1e64b12097f
Ryan Cocks <ryan@ryancocks.net>
2021-05-09 13:20:55 +0300

    create_position_identifier_from_tuple

--- events/shared/position-identifier.ts
+++ events/shared/position-identifier.ts
@@ -1,6 +1,18 @@
-import { ExchangeIdentifier } from './exchange-identifier'
+import { ExchangeIdentifier } from "./exchange-identifier"
 
 export interface PositionIdentifier {
-  exchange_identifier: ExchangeIdentifier;
-  symbol: string;
+  exchange_identifier: ExchangeIdentifier
+  symbol: string
+}
+
+export function create_position_identifier_from_tuple({
+  symbol,
+  account,
+  exchange,
+}: {
+  symbol: string
+  account: string
+  exchange: string
+}): PositionIdentifier {
+  return { symbol, exchange_identifier: { exchange, account } }
 }

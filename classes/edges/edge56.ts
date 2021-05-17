e1e51f2777d5447dffa64b7b006470e41e549796
Ryan Cocks <ryan@ryancocks.net>
2021-05-17 12:32:40 +0300

    Store high_candle instead of just high price

--- classes/edges/edge56.ts
+++ classes/edges/edge56.ts
@@ -12,14 +12,13 @@ import { Logger } from "../../interfaces/logger"
 import { CandleUtils } from "../../classes/utils/candle_utils"
 import { CGMarketData } from "../../classes/utils/coin_gecko"
 
-const humanNumber = require('human-number')
+const humanNumber = require("human-number")
 
 type SendMessageFunc = (msg: string) => void
 export class Edge56 {
   key: "high" | "close"
   current_high: BigNumber
-  current_high_candle: CandleChartResult
-  latest_price: BigNumber
+  current_high_candle: CandleChartResult | Candle
   market_data: CGMarketData
 
   in_position: boolean = false
@@ -37,14 +36,14 @@ export class Edge56 {
     symbol,
     send_message,
     key,
-    market_data
+    market_data,
   }: {
     ee: any
     logger: Logger
     initial_candles: CandleChartResult[]
     symbol: string
     send_message: SendMessageFunc
-    key: "high" | "close",
+    key: "high" | "close"
     market_data: CGMarketData
   }) {
     this.key = key
@@ -57,7 +56,11 @@ export class Edge56 {
     this.set_high(candle)
   }
 
-  private set_high(candle: CandleChartResult) {
+  private is_new_high_candle(candle: CandleChartResult | Candle) {
+    return new BigNumber(candle[this.key]).isGreaterThanOrEqualTo(this.current_high_candle[this.key])
+  }
+
+  private set_high(candle: CandleChartResult | Candle) {
     this.current_high = new BigNumber(candle[this.key])
     this.current_high_candle = candle
     console.log(`${this.symbol} setting high to ${candle[this.key]} from ${new Date(candle.closeTime).toString()}`)
@@ -67,7 +70,11 @@ export class Edge56 {
     let price = new BigNumber(candle.close)
     if (this.in_position) throw new Error(`Already in position`)
     this.in_position = true
-    this.send_message(`${this.symbol} Position entry triggered at price: ${price.toFixed()}  MCAP ${humanNumber(new BigNumber(this.market_data.market_cap).toPrecision(2))} RANK: ${this.market_data.market_cap_rank}`)
+    this.send_message(
+      `${this.symbol} Position entry triggered at price: ${price.toFixed()}  MCAP ${humanNumber(
+        new BigNumber(this.market_data.market_cap).toPrecision(2)
+      )} RANK: ${this.market_data.market_cap_rank}`
+    )
     this.lowest_price_seen_since_entry = price
     this.entry_price = price
   }
@@ -75,6 +82,7 @@ export class Edge56 {
   percentage_change_since_entry(price: BigNumber) {
     return price.minus(this.entry_price).dividedBy(this.entry_price).times(100).dp(1)
   }
+
   async ingest_intercandle_close_update_candle({
     timeframe,
     candle,
@@ -84,12 +92,17 @@ export class Edge56 {
     symbol: string
     candle: Candle
   }) {
-    if (this.potential_new_high_detected) return
+    if (this.potential_new_high_detected) return // don't keep spamming with this alert
     if (new BigNumber(candle.high).isGreaterThan(this.current_high)) {
-      this.send_message(`${this.symbol} Potential new high. MCAP ${humanNumber(new BigNumber(this.market_data.market_cap).toPrecision(2))} RANK: ${this.market_data.market_cap_rank}`)
+      this.send_message(
+        `${this.symbol} Potential new high. MCAP ${humanNumber(
+          new BigNumber(this.market_data.market_cap).toPrecision(2)
+        )} RANK: ${this.market_data.market_cap_rank}`
+      )
       this.potential_new_high_detected = true // just do this once per candle
     }
   }
+
   async ingest_new_candle({
     timeframe,
     candle,
@@ -103,18 +116,22 @@ export class Edge56 {
       console.log(`Short timeframe candle on ${this.symbol} closed at ${candle.close}`)
     }
     this.potential_new_high_detected = false // reset
-    // TODO: shit we need to adjust the high too - if we get a wick but not a close?
     // TODO: add to position if we are >~30% up since entry price (daily close)
     // TODO: stop out at 25% drawdown - presumably that's with just the initial position?
     // TODO: we probably want some kind of limit so we don't buy the top of a 2x spike? Or given small position size maybe we are up for that.
     // TODO: exit strat - we can do exit in a separate service if we tag positions with the edge in redis
     // TODO: don't enter if we already have a position in this symbol
-    this.latest_price = new BigNumber(candle.close)
+    if (this.is_new_high_candle(candle)) this.set_high(candle)
     if (this.in_position) {
       let low = new BigNumber(candle.low)
       if (low.isLessThan(this.lowest_price_seen_since_entry)) {
         this.lowest_price_seen_since_entry = low
-        console.warn(`Drawdown is now: ${this.percentage_change_since_entry(low)}`)
+        let drawdown :BigNumber= this.percentage_change_since_entry(low)
+        console.warn(`${this.symbol} drawdown is now: ${drawdown}`)
+        let limit = 25
+        if(drawdown.isGreaterThanOrEqualTo(limit)){
+          this.send_message(`${this.symbol} hit limit drawdown trigger of ${limit}%`)
+        }
       }
     } else if (new BigNumber(candle.high).isGreaterThan(this.current_high)) {
       console.log(`Entry!! at ${candle.close}, ${new Date(candle.closeTime)}`)

b121458a762273021291dd131565644ef5f73ae7
Ryan Cocks <ryan@ryancocks.net>
2021-05-30 23:44:23 +0300

    fix

--- classes/exchanges/ftx/JSONBigNumber.js
+++ classes/exchanges/ftx/JSONBigNumber.js
@@ -144,8 +144,7 @@
     test, toJSON, toString, valueOf
 */
 
-import BigNumber from "bignumber.js";
-
+const BigNumber = require("bignumber.js");
 
 var rx_escapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 

8ab42450c523287f47f63f11073e088ab9d7e9d8
Ryan Cocks <ryan@ryancocks.net>
2021-06-07 00:34:58 +0300

    Closing and buy orders processed by Position class

--- classes/position.ts
+++ classes/position.ts
@@ -1,15 +1,10 @@
+/* Realtime Presumably Redis backed access to Position data, loading and mutation thereof
+ * Direct access to the Redis state exists in other classes atm and we are trying to move it all here
+ */
+
 import { Logger } from "../interfaces/logger"
 import { RedisPositionsState } from "../classes/persistent_state/redis_positions_state"
 import { PositionIdentifier } from "../events/shared/position-identifier"
-
-/*
-{ exchange: 'binance', account: 'default', symbol: 'XTZBNB' }
-{
-  position_size: 783.8,
-  initial_entry_price: 0.00988,
-  netQuoteBalanceChange: -6.121648
-}
-*/
 
 import { BigNumber } from "bignumber.js"
 import { GenericOrderData } from "../types/exchange_neutral/generic_order_data"
@@ -40,7 +35,7 @@ export class Position {
     this.position_identifier = position_identifier
   }
 
-   get tuple() {
+  get tuple() {
     return {
       baseAsset: this.position_identifier.baseAsset,
       exchange: this.position_identifier.exchange_identifier.exchange,
@@ -48,7 +43,7 @@ export class Position {
     }
   }
 
-  baseAsset(): string {
+  get baseAsset(): string {
     return this.position_identifier.baseAsset
   }
 
@@ -80,6 +75,7 @@ export class Position {
   async add_order_to_position({ generic_order_data }: { generic_order_data: GenericOrderData }) {
     let {
       baseAsset,
+      side,
       // quoteAsset,
       exchange,
       account,
@@ -88,11 +84,26 @@ export class Position {
       // totalQuoteTradeQuantity, // TODO: use this
     } = generic_order_data
     if (!account) account = "default" // TODO
+    if (baseAsset !== this.baseAsset) {
+      throw new Error(`Unexpected base_asset in call to Position.add_order_to_position`)
+    }
+    let base_change =
+      side === "BUY" ? new BigNumber(totalBaseTradeQuantity) : new BigNumber(totalBaseTradeQuantity).negated()
     await this.redis_positions.adjust_position_size_by(
       { baseAsset, exchange, account },
       {
-        base_change: new BigNumber(totalBaseTradeQuantity).negated(),
+        base_change,
       }
     )
+    // TODO: Fire a position changed event
+  }
+
+  async close() {
+    // TODO: maybe do USD equiv?
+    // let msg = `${position.baseAsset} traded from ${position.initial_entry_price} to ${
+    //   position.current_price
+    // }: ${position.percentage_price_change_since_initial_entry?.dp(1)}% change.`
+    // this.send_message(msg)
+    this.redis_positions.close_position(this.tuple)
   }
 }
--- services/position-tracker/position-tracker.ts
+++ services/position-tracker/position-tracker.ts
@@ -17,7 +17,6 @@ BigNumber.prototype.valueOf = function () {
 import * as Sentry from "@sentry/node"
 import { PositionIdentifier } from "../../events/shared/position-identifier"
 import { Position } from "../../classes/position"
-import { ExchangeIdentifier } from "../../events/shared/exchange-identifier"
 
 type check_func = ({
   volume,
@@ -72,15 +71,11 @@ export class PositionTracker {
       totalQuoteTradeQuantity,
       orderTime,
     } = generic_order_data
-    if (!account) account = "default"
-    let position_size: BigNumber | null = await this.positions_state.get_position_size({
-      exchange,
-      account,
-      baseAsset,
-    })
+
+    let position = await this.load_position_for_order(generic_order_data)
 
     // 1. Is this a new position?
-    if (position_size.isZero()) {
+    if ((await position.position_size()).isZero()) {
       try {
         this.send_message(`New position for ${baseAsset}`)
       } catch (error) {
@@ -98,7 +93,7 @@ export class PositionTracker {
       try {
         initial_entry_price = averageExecutionPrice ? new BigNumber(averageExecutionPrice) : undefined
         this.positions_state.create_new_position(
-          { baseAsset, exchange, account },
+          position.tuple,
           {
             position_size: new BigNumber(totalBaseTradeQuantity),
             initial_entry_price,
@@ -119,7 +114,7 @@ export class PositionTracker {
       try {
         this.position_publisher.publish_new_position_event({
           event_type: "NewPositionEvent",
-          exchange_identifier: { exchange, account },
+          exchange_identifier: position.tuple,
           baseAsset,
           position_base_size: totalBaseTradeQuantity,
           position_initial_quote_spent: totalQuoteTradeQuantity,
@@ -137,26 +132,10 @@ export class PositionTracker {
         })
       }
     } else {
-      // 1.2 if existing position just increase the position size
-      // not sure what do do about entry price adjustments yet
-      this.send_message(`Existing position found for ${baseAsset}, size ${position_size}`)
-      // TODO: Fuck, what is the quote currency is different on the buy/sell?
-      // TODO: positions aren't in pairs (symbols) they are in base currencies.
-      this.logger.error(`Adjustment of position size is not implemented`)
-      // TODO: Adjustment of position size
-      // this.positions_state.adjust_position_size_by(
-      //   { baseAsset, exchange, account },
-      //   {
-      //     base_change: new BigNumber(totalBaseTradeQuantity),
-      //     quote_change: new BigNumber(totalQuoteTradeQuantity).negated(),
-      //     quoteAsset,
-      //   }
-      // )
-      position_size = null // invalidated as needs re-loading from state
+      // 1.2 existing position
+      this.send_message(`Existing position found for ${baseAsset}`)
+      await position.add_order_to_position({ generic_order_data })
     }
-
-    // 3. Fire a position changed event or call a callback so we can add auto-exit 10@10 orders
-    // A new service called trading-rules-auto-position-exits
   }
 
   private async load_position_for_order(generic_order_data: GenericOrderData): Promise<Position> {
@@ -182,22 +161,10 @@ export class PositionTracker {
       baseAsset,
       quoteAsset,
       market_symbol,
-      exchange,
-      account,
       averageExecutionPrice,
-      // totalBaseTradeQuantity,
-      // totalQuoteTradeQuantity, // TODO: use this
     } = generic_order_data
 
-    if (!account) account = "default" // TODO
-
-    let exchange_identifier: ExchangeIdentifier = { exchange, account }
-    let position_identifier: PositionIdentifier = { exchange_identifier, baseAsset }
-    let position = new Position({
-      logger: this.logger,
-      redis_positions: this.positions_state,
-      position_identifier,
-    })
+    let position = await this.load_position_for_order(generic_order_data)
 
     // 1. Is this an existing position?
     if ((await position.position_size()).isZero()) {
@@ -209,7 +176,7 @@ export class PositionTracker {
 
     // TODO: the code in autoexits that calls MarketUtils could call onto a position. Would be good on a position
     // to have one call to move the stops on all orders up at once. Position.move_all_stops_to(stop_price)
-    await position.add_order_to_position({generic_order_data})
+    await position.add_order_to_position({ generic_order_data })
 
     let msg = `reduced the position size for ${baseAsset}`
     this.send_message(msg)
@@ -230,19 +197,8 @@ export class PositionTracker {
         price: new BigNumber(averageExecutionPrice),
       })
     ) {
-      this._close_position(await this.load_position_for_order(generic_order_data), quoteAsset)
-    }
-  }
-
-  private async _close_position(position: Position, quoteAsset: string) {
-    // TODO: maybe do USD equiv?
-    // let msg = `${position.baseAsset} traded from ${position.initial_entry_price} to ${
-    //   position.current_price
-    // }: ${position.percentage_price_change_since_initial_entry?.dp(1)}% change.`
-    // this.send_message(msg)
-
-    this.positions_state.close_position(position.tuple).then(() => {
+      await position.close()
       this.send_message(`closed position: ${position.baseAsset} to ${quoteAsset}`)
-    })
+    }
   }
 }

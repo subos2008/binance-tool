1273b52d7d145e0d78a4498fc281b52b2141782c
Ryan Cocks <ryan@ryancocks.net>
2021-06-10 12:33:09 +0300

    comment out FTX code

--- classes/exchanges/ftx/order_execution_tracker.ts
+++ classes/exchanges/ftx/order_execution_tracker.ts
@@ -1,120 +1,120 @@
-// Converts a stream of exchange order info into a defined interface of callbacks
+// // Converts a stream of exchange order info into a defined interface of callbacks
 
-import { strict as assert } from "assert"
+// import { strict as assert } from "assert"
 
-import { BigNumber } from "bignumber.js"
-BigNumber.DEBUG = true // Prevent NaN
-// Prevent type coercion
-BigNumber.prototype.valueOf = function () {
-  throw Error("BigNumber .valueOf called!")
-}
+// import { BigNumber } from "bignumber.js"
+// BigNumber.DEBUG = true // Prevent NaN
+// // Prevent type coercion
+// BigNumber.prototype.valueOf = function () {
+//   throw Error("BigNumber .valueOf called!")
+// }
 
-import { Logger } from "../../../interfaces/logger"
-import { FtxOrderCallbacks, FtxOrderWsEvent, FtxWsOrderData } from "../../../interfaces/exchange/ftx/orders"
-import { FtxWebsocketClient } from "../../../classes/exchanges/ftx/websocket-client"
+// import { Logger } from "../../../interfaces/logger"
+// import { FtxOrderCallbacks, FtxOrderWsEvent, FtxWsOrderData } from "../../../interfaces/exchange/ftx/orders"
+// import { FtxWebsocketClient } from "../../../classes/exchanges/ftx/websocket-client"
 
-import * as Sentry from "@sentry/node"
+// import * as Sentry from "@sentry/node"
 
-export class FtxOrderExecutionTracker {
-  send_message: Function
-  logger: Logger
-  ws: FtxWebsocketClient
-  closeUserWebsocket: Function
-  order_callbacks: FtxOrderCallbacks | undefined
-  print_all_trades: boolean = true
+// export class FtxOrderExecutionTracker {
+//   send_message: Function
+//   logger: Logger
+//   ws: FtxWebsocketClient
+//   closeUserWebsocket: Function
+//   order_callbacks: FtxOrderCallbacks | undefined
+//   print_all_trades: boolean = true
 
-  // All numbers are expected to be passed in as strings
-  constructor({
-    ws, // FTX WebSocket
-    send_message,
-    logger,
-    order_callbacks,
-    print_all_trades,
-  }: {
-    ws: FtxWebsocketClient
-    send_message: (msg: string) => void
-    logger: Logger
-    order_callbacks?: FtxOrderCallbacks
-    print_all_trades?: boolean
-  }) {
-    assert(logger)
-    this.logger = logger
-    assert(send_message)
-    this.send_message = send_message
-    this.order_callbacks = order_callbacks
-    assert(ws)
-    this.ws = ws
-    if (print_all_trades) this.print_all_trades = true
+//   // All numbers are expected to be passed in as strings
+//   constructor({
+//     ws, // FTX WebSocket
+//     send_message,
+//     logger,
+//     order_callbacks,
+//     print_all_trades,
+//   }: {
+//     ws: FtxWebsocketClient
+//     send_message: (msg: string) => void
+//     logger: Logger
+//     order_callbacks?: FtxOrderCallbacks
+//     print_all_trades?: boolean
+//   }) {
+//     assert(logger)
+//     this.logger = logger
+//     assert(send_message)
+//     this.send_message = send_message
+//     this.order_callbacks = order_callbacks
+//     assert(ws)
+//     this.ws = ws
+//     if (print_all_trades) this.print_all_trades = true
 
-    process.on("exit", () => {
-      this.shutdown_streams()
-    })
-  }
+//     process.on("exit", () => {
+//       this.shutdown_streams()
+//     })
+//   }
 
-  async main() {
-    try {
-      await this.monitor_order_stream()
-    } catch (error) {
-      Sentry.captureException(error)
-      this.logger.error(error)
-      throw error
-    }
-  }
+//   async main() {
+//     try {
+//       await this.monitor_order_stream()
+//     } catch (error) {
+//       Sentry.captureException(error)
+//       this.logger.error(error)
+//       throw error
+//     }
+//   }
 
-  shutdown_streams() {
-    this.logger.error(`shutdown_streams not implemented for FtxOrderExecutionTracker`)
-  }
+//   shutdown_streams() {
+//     this.logger.error(`shutdown_streams not implemented for FtxOrderExecutionTracker`)
+//   }
 
-  async monitor_order_stream() {
-    this.ws.subscribe("orders")
-    ;async (event: FtxOrderWsEvent) => {
-      let data: FtxWsOrderData = event.data
-      try {
-        if (event.type !== "update") {
-          return
-        }
-        await this.processExecutionReport(data)
-      } catch (error) {
-        Sentry.captureException(error)
-        let msg = `SHIT: error tracking FTX orders for pair ${data.market}`
-        this.logger.error(msg)
-        this.logger.error(error)
-        this.send_message(msg)
-      }
-    }
-  }
+//   async monitor_order_stream() {
+//     this.ws.subscribe("orders")
+//     ;async (event: FtxOrderWsEvent) => {
+//       let data: FtxWsOrderData = event.data
+//       try {
+//         if (event.type !== "update") {
+//           return
+//         }
+//         await this.processExecutionReport(data)
+//       } catch (error) {
+//         Sentry.captureException(error)
+//         let msg = `SHIT: error tracking FTX orders for pair ${data.market}`
+//         this.logger.error(msg)
+//         this.logger.error(error)
+//         this.send_message(msg)
+//       }
+//     }
+//   }
 
-  async processExecutionReport(data: FtxWsOrderData) {
-    const { id, market, avgFillPrice, price, side, status: orderStatus, type, size } = data
-    let orderId = id.toString()
-    let orderType = type.toUpperCase()
+//   async processExecutionReport(data: FtxWsOrderData) {
+//     const { id, market, avgFillPrice, price, side, status: orderStatus, type, size } = data
+//     let orderId = id.toString()
+//     let orderType = type.toUpperCase()
 
-    try {
-    } catch (error) {
-      this.logger.error(error)
-      Sentry.withScope(function (scope) {
-        scope.setTag("operation", "processExecutionReport")
-        scope.setTag("market_symbol", market)
-        if (orderId) scope.setTag("orderId", orderId)
-        Sentry.captureException(error)
-      })
-    }
+//     try {
+//     } catch (error) {
+//       this.logger.error(error)
+//       Sentry.withScope(function (scope) {
+//         scope.setTag("operation", "processExecutionReport")
+//         scope.setTag("market_symbol", market)
+//         if (orderId) scope.setTag("orderId", orderId)
+//         Sentry.captureException(error)
+//       })
+//     }
 
-    if (this.print_all_trades) {
-      this.logger.info(`${market} ${side} ${orderType} ORDER #${orderId} (${orderStatus})`)
-      this.logger.info(
-        `..price: ${price.toFixed()}, quantity: ${size.toFixed()}, averageExecutionPrice: ${avgFillPrice.toFixed()}`
-      )
-    }
+//     if (this.print_all_trades) {
+//       this.logger.info(`${market} ${side} ${orderType} ORDER #${orderId} (${orderStatus})`)
+//       this.logger.info(
+//         `..price: ${price.toFixed()}, quantity: ${size.toFixed()}, averageExecutionPrice: ${avgFillPrice.toFixed()}`
+//       )
+//     }
 
-    if (orderStatus !== "closed") {
-      this.logger.warn(`Unknown order status for FtxOrderTracker: ${orderStatus}`)
-    }
+//     if (orderStatus !== "closed") {
+//       this.logger.warn(`Unknown order status for FtxOrderTracker: ${orderStatus}`)
+//     }
 
-    // if (this.order_callbacks) await this.order_callbacks.order_filled_or_partially_filled(orderId, data)
+//     // if (this.order_callbacks) await this.order_callbacks.order_filled_or_partially_filled(orderId, data)
 
-    if (orderStatus == "closed") {
-      if (this.order_callbacks) await this.order_callbacks.order_filled(orderId, data)
-    }
-  }
-}
+//     if (orderStatus == "closed") {
+//       if (this.order_callbacks) await this.order_callbacks.order_filled(orderId, data)
+//     }
+//   }
+// }
--- interfaces/exchange/ftx/orders.ts
+++ interfaces/exchange/ftx/orders.ts
@@ -1,64 +1,64 @@
-// from: https://docs.ftx.com/#orders-2
+// // from: https://docs.ftx.com/#orders-2
 
-import { GenericOrderData } from "../../../types/exchange_neutral/generic_order_data"
-import { ExchangeUtils } from "../../../interfaces/exchange/generic/exchange-utils"
-import BigNumber from "bignumber.js"
+// import { GenericOrderData } from "../../../types/exchange_neutral/generic_order_data"
+// import { ExchangeUtils } from "../../../interfaces/exchange/generic/exchange-utils"
+// import BigNumber from "bignumber.js"
 
-export type FtxWsOrderData = {
-  "id": number
-  "clientId": null | string
-  "market": string
-  "type": "limit" | "market"
-  "side": "buy" | "sell"
-  "size": BigNumber
-  "price": BigNumber
-  "reduceOnly": boolean
-  "ioc": boolean
-  "postOnly": boolean
-  "status": "closed" | string
-  "filledSize": BigNumber
-  "remainingSize": BigNumber
-  "avgFillPrice": BigNumber
-}
+// export type FtxWsOrderData = {
+//   "id": number
+//   "clientId": null | string
+//   "market": string
+//   "type": "limit" | "market"
+//   "side": "buy" | "sell"
+//   "size": BigNumber
+//   "price": BigNumber
+//   "reduceOnly": boolean
+//   "ioc": boolean
+//   "postOnly": boolean
+//   "status": "closed" | string
+//   "filledSize": BigNumber
+//   "remainingSize": BigNumber
+//   "avgFillPrice": BigNumber
+// }
 
-export type FtxOrderWsEvent = {
-  "channel": "orders"
-  "data": FtxWsOrderData
-  "type": "update"
-}
+// export type FtxOrderWsEvent = {
+//   "channel": "orders"
+//   "data": FtxWsOrderData
+//   "type": "update"
+// }
 
-export interface FtxOrderCallbacks {
-  order_cancelled(order_id: string, data: FtxWsOrderData): Promise<void>
-  order_filled(order_id: string, data: FtxWsOrderData): Promise<void>
-  order_filled_or_partially_filled(order_id: string, data: FtxWsOrderData): Promise<void>
-  order_created?(order_id: string, data: FtxWsOrderData): Promise<void>
-}
+// export interface FtxOrderCallbacks {
+//   order_cancelled(order_id: string, data: FtxWsOrderData): Promise<void>
+//   order_filled(order_id: string, data: FtxWsOrderData): Promise<void>
+//   order_filled_or_partially_filled(order_id: string, data: FtxWsOrderData): Promise<void>
+//   order_created?(order_id: string, data: FtxWsOrderData): Promise<void>
+// }
 
-export function fromCompletedFtxOrderData(o: FtxWsOrderData, eu: ExchangeUtils): GenericOrderData {
-  if (o.status !== "closed") {
-    console.warn(`FTX order status ${o.status}: unknown mapping for GenericOrderData.orderStatus`)
-  }
+// export function fromCompletedFtxOrderData(o: FtxWsOrderData, eu: ExchangeUtils): GenericOrderData {
+//   if (o.status !== "closed") {
+//     console.warn(`FTX order status ${o.status}: unknown mapping for GenericOrderData.orderStatus`)
+//   }
 
-  if (o.status !== "closed") {
-    throw new Error(`FTX order status ${o.status}: order is not closed not safe for mapping`)
-  }
+//   if (o.status !== "closed") {
+//     throw new Error(`FTX order status ${o.status}: order is not closed not safe for mapping`)
+//   }
 
-  return {
-    exchange: "ftx", // name / slug of the exchange
-    // account?: string // account identifier on the exchange
-    orderId: o.id.toString(),
-    // as provided by the exchange
-    market_symbol: o.market, // pair etc - included but should be likely ignored as differs
-    baseAsset: eu.base_asset_for_market(o.market),
-    quoteAsset: eu.quote_asset_for_market(o.market),
-    side: o.side.toUpperCase() as "BUY" | "SELL",
-    orderType: o.type.toUpperCase() as "MARKET" | "LIMIT",
+//   return {
+//     exchange: "ftx", // name / slug of the exchange
+//     // account?: string // account identifier on the exchange
+//     orderId: o.id.toString(),
+//     // as provided by the exchange
+//     market_symbol: o.market, // pair etc - included but should be likely ignored as differs
+//     baseAsset: eu.base_asset_for_market(o.market),
+//     quoteAsset: eu.quote_asset_for_market(o.market),
+//     side: o.side.toUpperCase() as "BUY" | "SELL",
+//     orderType: o.type.toUpperCase() as "MARKET" | "LIMIT",
 
-    orderStatus: o.status === "closed" ? "COMPLETED" : undefined, // COMPLETED | PART... // tricky semantics here
-    // orderTime: number // timestamp, presume ms
+//     orderStatus: o.status === "closed" ? "COMPLETED" : undefined, // COMPLETED | PART... // tricky semantics here
+//     // orderTime: number // timestamp, presume ms
 
-    totalBaseTradeQuantity: o.filledSize.toFixed(),
-    totalQuoteTradeQuantity: o.filledSize.times(o.avgFillPrice).toFixed(), // NB: this will not be perfect :-/
-    averageExecutionPrice: o.avgFillPrice.toFixed(), // Added by us
-  }
-}
+//     totalBaseTradeQuantity: o.filledSize.toFixed(),
+//     totalQuoteTradeQuantity: o.filledSize.times(o.avgFillPrice).toFixed(), // NB: this will not be perfect :-/
+//     averageExecutionPrice: o.avgFillPrice.toFixed(), // Added by us
+//   }
+// }

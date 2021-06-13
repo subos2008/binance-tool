416a90da1622657f6a4213ab5e906b5eabe6ec8a
Ryan Cocks <ryan@ryancocks.net>
2021-06-13 18:54:23 +0300

    refactor set-stop

--- cli/set-stop.ts
+++ cli/set-stop.ts
@@ -30,6 +30,9 @@ import { ExchangeIdentifier } from "../events/shared/exchange-identifier"
 import { CurrentPortfolioGetter } from "../interfaces/exchange/generic/portfolio-getter"
 import { Balance } from "../interfaces/portfolio"
 import { BinancePortfolioGetter } from "../interfaces/exchange/binance/binance-portfolio-getter"
+import { BinancePriceGetter } from "../interfaces/exchange/binance/binance-price-getter"
+import { CurrentPriceGetter } from "../interfaces/exchange/generic/price-getter"
+import { nextTick } from "process"
 
 require("dotenv").config()
 
@@ -60,6 +63,35 @@ async function main() {
 }
 main().then(() => {})
 
+let price_getters: { [exchange: string]: CurrentPriceGetter } = {}
+function mint_price_getter({
+  exchange_identifier,
+}: {
+  exchange_identifier: ExchangeIdentifier
+}): CurrentPriceGetter {
+  if (exchange_identifier.exchange === "binance") {
+    const Binance = require("binance-api-node").default
+    const ee = Binance({
+      apiKey: process.env.APIKEY,
+      apiSecret: process.env.APISECRET,
+    })
+    return new BinancePriceGetter({ ee })
+  } else {
+    throw new Error(`Exchange ${exchange_identifier.exchange} not implemented`)
+  }
+}
+async function get_current_price({
+  exchange_identifier,
+  market_symbol,
+}: {
+  exchange_identifier: ExchangeIdentifier
+  market_symbol: string
+}): Promise<BigNumber> {
+  if (!(exchange_identifier.exchange in price_getters)) {
+    price_getters[exchange_identifier.exchange] = mint_price_getter({ exchange_identifier })
+  }
+  return price_getters[exchange_identifier.exchange].get_current_price({ market_symbol })
+}
 function mint_portfolio_getter({
   exchange_identifier,
 }: {
@@ -76,23 +108,68 @@ function mint_portfolio_getter({
     throw new Error(`Exchange ${exchange_identifier.exchange} not implemented`)
   }
 }
+
+async function convert_to_usd({
+  exchange_identifier,
+  base_asset,
+  quantity,
+}: {
+  base_asset: string
+  exchange_identifier: ExchangeIdentifier
+  quantity: BigNumber
+}): Promise<BigNumber> {
+  return (
+    await get_current_price({ exchange_identifier, market_symbol: `${base_asset.toUpperCase()}USDT` })
+  ).times(quantity)
+}
+export interface DecoratedBalance {
+  asset: string
+  total: BigNumber
+  usd_equivalent?: BigNumber
+}
+function decorate_balances(balances: Balance[]) {
+  return balances.map((b) => ({ asset: b.asset, total: new BigNumber(b.free).plus(b.locked) }))
+}
 async function get_current_portfolio({
   exchange_identifier,
 }: {
   exchange_identifier: ExchangeIdentifier
 }): Promise<Balance[]> {
-  return mint_portfolio_getter({ exchange_identifier }).get_balances()
+  return await mint_portfolio_getter({ exchange_identifier }).get_balances()
 }
+async function get_decorated_balances({
+  exchange_identifier,
+}: {
+  exchange_identifier: ExchangeIdentifier
+}): Promise<DecoratedBalance[]> {
+  let balances: Balance[] = await get_current_portfolio({ exchange_identifier })
+  let decorated_balances: DecoratedBalance[] = decorate_balances(balances)
+  for (const balance of decorated_balances) {
+    try {
+      balance.usd_equivalent = (
+        await convert_to_usd({
+          base_asset: balance.asset,
+          quantity: balance.total,
+          exchange_identifier,
+        })
+      ).dp(0)
+    } catch (err) {
+      // console.error(`Error processing info for ${balance}: ${err}`)
+    }
+  }
+  return decorated_balances.filter((b) => b.usd_equivalent?.isGreaterThanOrEqualTo(20))
+}
+
 async function list_portfolio(argv: any) {
   let exchange_identifier = { exchange: argv.exchange, account: argv.account }
-  let balances: Balance[] = await get_current_portfolio({ exchange_identifier })
-  if (balances.length === 0) {
+  let decorated_balances: DecoratedBalance[] = await get_decorated_balances({ exchange_identifier })
+  if (decorated_balances.length === 0) {
     console.log(`No open positions`)
     return
   }
-  for (const balance of balances) {
+  for (const balance of decorated_balances) {
     try {
-      console.log(balance)
+      console.log(`${balance.asset}: \$${balance.usd_equivalent?.toFixed()}`)
     } catch (err) {
       console.error(`Error processing info for ${balance}: ${err}`)
     }

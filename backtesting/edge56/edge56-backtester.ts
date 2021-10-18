d3730167a5ef3716b312aa4b96471b6afc2aff98
Ryan Cocks <ryan@ryancocks.net>
2021-10-18 02:10:06 +0100

    playing with backtester

--- backtesting/edge56/edge56-backtester.ts
+++ backtesting/edge56/edge56-backtester.ts
@@ -1,40 +1,49 @@
 // #!./node_modules/.bin/ts-node
 
+// TODO:
+// Load candles - from disk or via abstraction class
+// Calculate ADX
+// Manually check agains chart - maybe check for the colour change at various points
+// Change the parameters and check it matches TradingView plots
+
 // const Logger = require("../../lib/faux_logger")
 // // Initial logger, we re-create it below once we have the trade_id
 // var logger = new Logger({ silent: false })
 // require("dotenv").config()
+this.candles_collector = new CandlesCollector({ ee })
 
-// import BigNumber from "bignumber.js"
-// BigNumber.DEBUG = true // Prevent NaN
-// // Prevent type coercion
-// BigNumber.prototype.valueOf = function () {
-//   throw Error("BigNumber .valueOf called!")
-// }
+let initial_candles: CandleChartResult[]
+let price_history_candles: LimitedLengthCandlesHistory = new LimitedLengthCandlesHistory({
+  length: 20,
+  initial_candles,
+  key: this.historical_candle_key,
+})
 
-// import * as Sentry from "@sentry/node"
-// Sentry.init({
-//   dsn: "https://5f5398dfd6b0475ea6061cf39bc4ed03@sentry.io/5178400",
-// })
-// Sentry.configureScope(function (scope: any) {
-//   scope.setTag("service", "binance-tool")
-// })
+import BigNumber from "bignumber.js"
+BigNumber.DEBUG = true // Prevent NaN
+// Prevent type coercion
+BigNumber.prototype.valueOf = function () {
+  throw Error("BigNumber .valueOf called!")
+}
 
-// // import { get_redis_client, set_redis_logger } from "../../lib/redis"
-// // set_redis_logger(logger)
-// // const redis = get_redis_client()
+import * as Sentry from "@sentry/node"
+Sentry.init({
+  dsn: "https://5f5398dfd6b0475ea6061cf39bc4ed03@sentry.io/5178400",
+})
+Sentry.configureScope(function (scope: any) {
+  scope.setTag("service", "binance-tool")
+})
 
-// // const { promisify } = require("util")
-// // const hgetallAsync = promisify(redis.hgetall).bind(redis)
-// import binance, { CancelOrderResult } from "binance-api-node"
-// import { Binance, CandleChartInterval, CandleChartResult } from "binance-api-node"
-// import { threadId } from "worker_threads"
-// import { assert } from "console"
+import binance, { CancelOrderResult } from "binance-api-node"
+import { Binance, CandleChartInterval, CandleChartResult } from "binance-api-node"
+import { CandlesCollector, LimitedLengthCandlesHistory, CandleUtils } from "../../classes/utils/candle_utils"
+
+import { assert } from "console"
 
 // import { Edge56 } from "../../classes/edges/edge56"
 
-// // var talib = require('talib');
-// // console.log("TALib Version: " + talib.version);
+var talib = require("talib")
+console.log("TALib Version: " + talib.version)
 // // Display all available indicator function names
 // // var functions = talib.functions;
 // // for (let i in functions) {
@@ -44,104 +53,95 @@
 // var { argv } = require("yargs").string("symbol").demand("symbol")
 // let { "symbol": symbol } = argv
 
-// logger = new Logger({ silent: false, template: { symbol } })
+const logger = new Logger({ silent: false, template: { symbol } })
 
-// process.on("unhandledRejection", (error) => {
-//   logger.error(error)
-// })
+process.on("unhandledRejection", (error) => {
+  logger.error(error)
+})
+class Backtester {
+  candles_collector: CandlesCollector
+  ee: any
+  start_date: Date
+  end_date: Date
 
-// import { CandlesCollector, CandleUtils } from "../../classes/utils/candle_utils"
+  constructor({
+    ee,
+    symbol,
+    start_date,
+    end_date,
+  }: {
+    ee: any
+    symbol: string
+    start_date: Date
+    end_date: Date
+  }) {
+    this.ee = ee
+    this.candles_collector = new CandlesCollector({ ee })
+    this.start_date = start_date
+    this.end_date = end_date
+  }
 
-// class Edge56Backtester {
-//   edge: Edge56
-//   start_of_bullmarket_date: Date
-//   start_of_algo_date: Date
-//   end_of_algo_date: Date
-//   candles_collector: CandlesCollector
-//   ee: any
+  //   async run() {
+  //     let all_candles = await this.candles_collector.get_daily_candles_between(
+  //       this.start_date,
+  //       this.end_date)
+  //     )
+  //     console.log(`${all_candles.length} total candles`)
+  //     let initial_candles = all_candles.filter((candle) => candle.closeTime < this.start_of_algo_date.getTime())
+  //     console.log(`${initial_candles.length} initial candles`)
 
-//   constructor({
-//     ee,
-//     symbol,
-//     start_of_bullmarket_date,
-//     start_of_algo_date,
-//     end_of_algo_date,
-//   }: {
-//     ee: any
-//     symbol: string
-//     start_of_bullmarket_date: Date
-//     start_of_algo_date: Date
-//     end_of_algo_date: Date
-//   }) {
-//     this.start_of_bullmarket_date = start_of_bullmarket_date
-//     this.start_of_algo_date = start_of_algo_date
-//     this.end_of_algo_date = end_of_algo_date
-//     this.candles_collector = new CandlesCollector({ ee, symbol, start_date: start_of_bullmarket_date })
-//     this.ee = ee
-//   }
+  //     let candles = all_candles.filter((candle) => candle.closeTime >= this.start_of_algo_date.getTime())
+  //     console.log(`${candles.length} candles to ingest`)
+  //     for (let i = 0; i < candles.length; i++) {
+  //       this.edge.ingest_new_candle(candles[i])
+  //     }
+  //     this.edge.surmise_position()
+  //   }
+}
 
-//   async run() {
-//     let all_candles = await this.candles_collector.get_daily_candles_between(
-//       this.start_of_bullmarket_date,
-//       new Date()
-//     )
-//     console.log(`${all_candles.length} total candles`)
-//     let initial_candles = all_candles.filter((candle) => candle.closeTime < this.start_of_algo_date.getTime())
-//     console.log(`${initial_candles.length} initial candles`)
+async function main(symbol: string) {
+  var ee: Binance = binance({
+    apiKey: process.env.APIKEY || "foo",
+    apiSecret: process.env.APISECRET || "foo",
+  })
 
-//     this.edge = new Edge56({
-//       logger,
-//       ee: this.ee,
-//       symbol,
-//       start_of_bullmarket_date: this.start_of_bullmarket_date,
-//       end_of_algo_date: this.end_of_algo_date,
-//       initial_candles,
-//     })
+  try {
+    // const start_date = new Date("2021-01-01")
+    // const start_of_algo_date = new Date("2021-04-01")
 
-//     let candles = all_candles.filter((candle) => candle.closeTime >= this.start_of_algo_date.getTime())
-//     console.log(`${candles.length} candles to ingest`)
-//     for (let i = 0; i < candles.length; i++) {
-//       this.edge.ingest_new_candle(candles[i])
-//     }
-//     this.edge.surmise_position()
-//   }
-// }
+    let edge = new Edge56({
+      logger,
+      ee: this.ee,
+      symbol,
+      start_date: this.start_date,
+      end_date: this.end_date,
+      initial_candles,
+    })
 
-// async function main(symbol: string) {
-//   var ee: Binance = binance({
-//     apiKey: process.env.APIKEY || "foo",
-//     apiSecret: process.env.APISECRET || "foo",
-//   })
+    const edge = new Backtester({
+      ee,
+      symbol,
+      start_date,
+      end_date,
+    })
+    await edge.run()
+    soft_exit(0)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-//   try {
-//     const start_of_bullmarket_date = new Date("2021-01-01")
-//     const start_of_algo_date = new Date("2021-04-01")
+// TODO: exceptions
+main(symbol).catch((error) => {
+  console.error(`Error in main loop: ${error}`)
+  console.error(error)
+  console.error(`Error in main loop: ${error.stack}`)
+})
 
-//     const edge56 = new Edge56Backtester({
-//       ee,
-//       start_of_bullmarket_date,
-//       start_of_algo_date,
-//       end_of_algo_date: new Date(),
-//       symbol,
-//     })
-//     await edge56.run()
-//     soft_exit(0)
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-
-// // TODO: exceptions
-// main(symbol).catch((error) => {
-//   console.error(`Error in main loop: ${error}`)
-//   console.error(error)
-//   console.error(`Error in main loop: ${error.stack}`)
-// })
-
-// function soft_exit(exit_code?: number | undefined) {
-//   console.warn(`soft_exit called, exit_code: ${exit_code}`)
-//   if (exit_code) console.warn(`soft_exit called with non-zero exit_code: ${exit_code}`)
-//   if (exit_code) process.exitCode = exit_code
-//   // redis.quit()
-//   // setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
-// }
+function soft_exit(exit_code?: number | undefined) {
+  console.warn(`soft_exit called, exit_code: ${exit_code}`)
+  if (exit_code) console.warn(`soft_exit called with non-zero exit_code: ${exit_code}`)
+  if (exit_code) process.exitCode = exit_code
+  // redis.quit()
+  // setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
+}

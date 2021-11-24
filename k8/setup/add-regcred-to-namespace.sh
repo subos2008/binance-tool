ddfebd64c9682e1c0c7aac63a1780abe592d3273
Ryan Cocks <ryan@ryancocks.net>
2021-11-24 23:20:57 +0200

    ghcr.io

--- k8/setup/add-regcred-to-namespace.sh
+++ k8/setup/add-regcred-to-namespace.sh
@@ -2,4 +2,4 @@
 
 source ../../.env
 
-kubectl create secret docker-registry regcred --docker-server=docker.pkg.github.com --docker-username=${DOCKER_REGISTRY_USER} --docker-password=${DOCKER_REGISTRY_PASSWORD} --namespace $1 -- $*
+kubectl create secret docker-registry regcred --docker-server=ghcr.io --docker-username=${DOCKER_REGISTRY_USER} --docker-password=${DOCKER_REGISTRY_PASSWORD} --namespace $1 -- $*

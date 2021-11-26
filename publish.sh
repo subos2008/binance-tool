90c9e792b725ef6c926381ec83024b7fcd45db73
Ryan Cocks <ryan@ryancocks.net>
2021-11-26 15:18:04 +0200

    amend

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ source .env
 set -xe 
 
 # $(aws ecr get-login --no-include-email --region eu-central-1)
-docker login docker.pkg.github.com -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD
+docker login ghcr.io -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD
 
 docker build -t binance-tool:latest .
 docker tag binance-tool:latest ${DOCKER_REGISTRY}:latest

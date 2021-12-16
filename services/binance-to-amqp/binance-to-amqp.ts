4f102a6174efbd71aa6d3f8b49b5715ca2fed368
Ryan Cocks <ryan@ryancocks.net>
2021-12-17 01:51:16 +0200

    binance-to-amqp refactor

--- services/binance-to-amqp/binance-to-amqp.ts
+++ services/binance-to-amqp/binance-to-amqp.ts
@@ -15,14 +15,11 @@
 //  1. Could also check redis-trades matches position sizes
 //  1. Doesn't currently re-publish on deposits/withdrawals
 
-import { strict as assert } from "assert"
 
 // Config
-import { ExchangeIdentifier } from "../../events/shared/exchange-identifier"
-const exchange_identifier: ExchangeIdentifier = { exchange: "binance", account: "default" }
 const service_name = "binance-to-amqp"
 
-import { MasterPortfolioClass, PortfolioBitchClass } from "./interfaces"
+import { HealthAndReadiness } from "../../classes/health_and_readiness"
 
 require("dotenv").config()
 
@@ -32,13 +29,13 @@ Sentry.configureScope(function (scope: any) {
   scope.setTag("service", service_name)
 })
 
-var service_is_healthy: boolean = true
-
 const send_message = require("../../lib/telegram.js")(`${service_name}: `)
 
 import { Logger } from "../../interfaces/logger"
 const LoggerClass = require("../../lib/faux_logger")
 const _logger: Logger = new LoggerClass({ silent: false })
+
+
 
 import { BigNumber } from "bignumber.js"
 BigNumber.DEBUG = true // Prevent NaN
@@ -53,159 +50,34 @@ process.on("unhandledRejection", (error) => {
   send_message(`UnhandledPromiseRejection: ${error}`)
 })
 
-import { PortfolioPublisher } from "../../classes/amqp/portfolio-publisher"
-import { PortfolioUtils } from "../../classes/utils/portfolio-utils"
-import { Portfolio, Balance } from "../../interfaces/portfolio"
-import { BinancePortfolioTracker } from "./portfolio"
-
-const publisher = new PortfolioPublisher({ logger: _logger, send_message, exchange_identifier, event_name: 'SpotBinancePortfolio' })
-
-class PortfolioTracker implements MasterPortfolioClass {
-  send_message: Function
-  logger: Logger
-  ee: any
-  portfolios: { [exchange: string]: Portfolio } = {}
-  exchanges: { [exchange: string]: PortfolioBitchClass } = {}
-
-  constructor({ send_message, logger }: { send_message: (msg: string) => void; logger: Logger }) {
-    assert(logger)
-    this.logger = logger
-    assert(send_message)
-    this.send_message = send_message
-  }
-
-  async set_portfolio_for_exchange({
-    exchange_identifier,
-    portfolio,
-  }: {
-    exchange_identifier: ExchangeIdentifier
-    portfolio: Portfolio
-  }) {
-    // TODO: account not used in ExchangeIdentifier: default (default added so this appears in greps)
-    this.portfolios[exchange_identifier.exchange] = portfolio
-    this.report_current_portfolio() // this line is going to be a problem when we have multiple exchanges
-  }
-
-  async update_and_report_portfolio() {
-    for await (const exchange of Object.values(this.exchanges)) {
-      await exchange.update_portfolio_from_exchange()
-    }
-    await this.report_current_portfolio()
-  }
-
-  // this is called periodically or on orders and reports on the current portfolio
-  async report_current_portfolio() {
-    try {
-      let portfolio = await this.collapse_and_decorate_exchange_balances()
-      if (!portfolio) {
-        this.logger.info(`no portfolio, skipping`)
-        return
-      }
-
-      // This is just for the logfiles, we don't use this to send the event
-      try {
-        let msg = `B: ${portfolio.btc_value}, U: ${portfolio.usd_value}`
-        try {
-          msg += " as " + portfolio_utils.balances_to_string(portfolio, "BTC")
-        } catch (err) {
-          Sentry.captureException(err)
-          this.logger.error(err)
-        }
-        if (portfolio.prices) {
-          try {
-            msg += ` BTCUSDT: ${new BigNumber(portfolio.prices["BTCUSDT"]).dp(0).toFixed()}`
-          } catch (e) {
-            /* just ignore */
-          }
-        }
-        this.logger.info(msg)
-      } catch (err) {
-        // Not fatal, we just used it for logging anyway
-        Sentry.captureException(err)
-        this.logger.error(err)
-      }
-
-      await publisher.publish(portfolio)
-    } catch (err) {
-      Sentry.captureException(err)
-      this.logger.error(err)
-    }
-  }
-
-  async collapse_and_decorate_exchange_balances() {
-    if (!this.portfolios) {
-      this.logger.warn(`No portfolios present in portfilio-tracker`)
-      return
-    }
-    let exchanges: string[] = Object.keys(this.portfolios)
-    if (exchanges.length > 1) throw new Error(`Multiple exchanges not implemented yet`)
-    return this.decorate_portfolio(this.portfolios[exchanges[0]])
-  }
-
-  async decorate_portfolio(portfolio: Portfolio): Promise<Portfolio> {
-    portfolio = portfolio_utils.add_quote_value_to_portfolio_balances({
-      // TODO: convert to list
-      portfolio,
-      quote_currency: "BTC",
-    }).portfolio
-    portfolio = portfolio_utils.add_quote_value_to_portfolio_balances({
-      portfolio,
-      quote_currency: "USDT",
-    }).portfolio
-    portfolio.btc_value = portfolio_utils
-      .calculate_portfolio_value_in_quote_currency({ quote_currency: "BTC", portfolio })
-      .total // .dp(3)
-      .toFixed()
-    if (!portfolio.prices) throw new Error(`No prices`)
-    portfolio.usd_value = portfolio_utils
-      .convert_base_to_quote_currency({
-        base_quantity: new BigNumber(portfolio.btc_value),
-        base_currency: "BTC",
-        quote_currency: "USDT",
-        prices: portfolio.prices,
-      })
-      // .dp(0)
-      .toFixed()
-    return portfolio
-  }
-}
+import { BinancePortfolioToAMQP } from "./portfolio"
 
 let logger = _logger
 
-const portfolio_utils: PortfolioUtils = new PortfolioUtils({ logger, sentry: Sentry })
+const health = new HealthAndReadiness({ logger, send_message })
+const service_is_healthy = health.addSubsystem({ name: "global", ready: true, healthy: true })
 
 async function main() {
   const execSync = require("child_process").execSync
   execSync("date -u")
 
-  let portfolio_tracker = new PortfolioTracker({ logger, send_message })
-  let binance = new BinancePortfolioTracker({ send_message, logger, master: portfolio_tracker })
-
-  // To function this service needs both of these, so go unhealthy if either fails
+  
   try {
-    await binance.start()
+    let health_and_readiness = health.addSubsystem({
+      name: 'binance-portfolio-to-amqp',
+      ready: false,
+      healthy: false,
+    })
+    let portfolio_to_amqp = new BinancePortfolioToAMQP({ send_message, logger, health_and_readiness })
+    await portfolio_to_amqp.start()
   } catch (error) {
     Sentry.captureException(error)
     logger.error(`Error connecting to exchange: ${error}`)
     logger.error(error)
     logger.error(`Error connecting to exchange: ${error.stack}`)
-    soft_exit(1, `Error connecting to exchange: ${error}`)
+    service_is_healthy.healthy(false) // it seems service isn't exiting on soft exit, but add this to make sure
     return
   }
-
-  try {
-    await publisher.connect()
-  } catch (error) {
-    Sentry.captureException(error)
-    logger.error(`Error connecting to AMQP: ${error}`)
-    logger.error(error)
-    logger.error(`Error connecting to AMQP: ${error.stack}`)
-    soft_exit(1, `Error connecting to AMQP: ${error}`)
-    return
-  }
-
-  setInterval(portfolio_tracker.update_and_report_portfolio.bind(portfolio_tracker), 1000 * 60 * 60 * 6)
-  await binance.update_portfolio_from_exchange() // automatically triggers report_current_portfolio
 }
 
 main().catch((error) => {
@@ -213,25 +85,12 @@ main().catch((error) => {
   logger.error(`Error in main loop: ${error}`)
   logger.error(error)
   logger.error(`Error in main loop: ${error.stack}`)
-  soft_exit(1, `Error in main loop: ${error}`)
 })
-
-// Note this method returns!
-// Shuts down everything that's keeping us alive so we exit
-function soft_exit(exit_code: number | null = null, reason: string) {
-  service_is_healthy = false // it seems service isn't exiting on soft exit, but add this to make sure
-  logger.warn(`soft_exit called, exit_code: ${exit_code}`)
-  if (exit_code) logger.warn(`soft_exit called with non-zero exit_code: ${exit_code}, reason: ${reason}`)
-  if (exit_code) process.exitCode = exit_code
-  if (publisher) publisher.shutdown_streams()
-  Sentry.close(500)
-  // setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
-}
 
 import * as express from "express"
 var app = express()
 app.get("/health", function (req, res) {
-  if (service_is_healthy) res.send({ status: "OK" })
+  if (health.healthy()) res.send({ status: "OK" })
   else res.status(500).json({ status: "UNHEALTHY" })
 })
 const port = "80"
--- services/binance-to-amqp/portfolio.ts
+++ services/binance-to-amqp/portfolio.ts
@@ -3,19 +3,21 @@
 /* eslint func-names: ["warn", "as-needed"] */
 
 /**
- * BinancePortfolioTracker implements PortfolioBitchClass
- * 
+ * BinancePortfolioToAMQP implements PortfolioBitchClass
+ *
  * callbacks on OrderExecutionTracker.
- * 
+ *
  * order_filled callback calls update_portfolio_from_exchange that updates price and portfolio data
  * in the master. I think the master might then callback into the publisher to fire the event.
- * 
+ *
  * TODO: MasterPortfolioClass was I think an attempt at having one shared master portfolio
  * updated by multiple exchange connected classes. Perhaps we could mimic that and
  * have exchange specific events be collated by a master portfolio tracker that then
  * sends out a master portfolio updated event, merging all exchanges positions.
- * 
+ *
  */
+
+// TODO: health_and_readiness isn't great here. Healthy() can be called from multiple places in the code, one true could overwrite another (false)
 
 // (OLD) TODO:
 // 1. Take initial portfolio code from the position sizer
@@ -23,6 +25,7 @@
 
 import { strict as assert } from "assert"
 const service_name = "binance-to-amqp"
+const event_expiration_seconds = "60"
 
 import { MasterPortfolioClass, PortfolioBitchClass } from "./interfaces"
 import { Binance as BinanceType } from "binance-api-node"
@@ -43,33 +46,242 @@ BigNumber.prototype.valueOf = function () {
   throw Error("BigNumber .valueOf called!")
 }
 
+import { connect, Connection } from "amqplib"
+import { GenericTopicPublisher } from "../../classes/amqp/generic-publishers"
+import { MyEventNameType } from "../../classes/amqp/message-routing"
+
 import { OrderExecutionTracker } from "../../classes/exchanges/binance/order_execution_tracker"
 import { BinanceOrderData } from "../../interfaces/order_callbacks"
 import { ExchangeIdentifier } from "../../events/shared/exchange-identifier"
 import { Balance, Portfolio } from "../../interfaces/portfolio"
 
-export class BinancePortfolioTracker implements PortfolioBitchClass {
+import { PortfolioUtils } from "../../classes/utils/portfolio-utils"
+import { HealthAndReadinessSubsystem } from "../../classes/health_and_readiness"
+
+// Let's keep this code, could become part of ensuring same format events accross exchanges
+export class PortfolioPublisher {
+  logger: Logger
+  closeTradesWebSocket: (() => void) | null
+  connection: Connection
+  channel: any
+  pub: GenericTopicPublisher
+  event_name: MyEventNameType
+  health_and_readiness: HealthAndReadinessSubsystem
+
+  constructor({
+    logger,
+    event_name,
+    health_and_readiness,
+  }: {
+    logger: Logger
+    event_name: MyEventNameType
+    health_and_readiness: HealthAndReadinessSubsystem
+  }) {
+    this.logger = logger
+    this.health_and_readiness = health_and_readiness
+    this.event_name = event_name
+    this.pub = new GenericTopicPublisher({ logger, event_name })
+  }
+
+  async connect(): Promise<void> {
+    await this.pub.connect()
+    this.health_and_readiness.ready(true)
+  }
+
+  async publish(event: Portfolio): Promise<void> {
+    // Extract only those fields we want to publish
+    let trimmed_event: Portfolio = {
+      usd_value: event.usd_value,
+      btc_value: event.btc_value,
+      balances: event.balances,
+      prices: event.prices,
+    }
+    const options = {
+      expiration: event_expiration_seconds,
+      persistent: false,
+      timestamp: Date.now(),
+    }
+    try {
+      await this.pub.publish(JSON.stringify(trimmed_event), options)
+    } catch (e) {
+      this.health_and_readiness.healthy(false)
+    }
+  }
+
+  async shutdown_streams() {
+    if (this.pub) this.pub.shutdown_streams()
+  }
+}
+
+// This class is a bit strange because it was originally intended to conglomerate multiple
+// portfolio accounts into one view for publishing.
+class PortfolioTracker implements MasterPortfolioClass {
   send_message: Function
   logger: Logger
-  ee: BinanceType
-  master: MasterPortfolioClass
-  order_execution_tracker: OrderExecutionTracker
-  exchange_identifier: ExchangeIdentifier
-  portfolio: Portfolio = { balances: [] }
+  ee: any
+  portfolios: { [exchange: string]: Portfolio } = {}
+  exchanges: { [exchange: string]: PortfolioBitchClass } = {}
+  publisher: PortfolioPublisher
+  portfolio_utils: PortfolioUtils
+  health_and_readiness: HealthAndReadinessSubsystem
 
   constructor({
     send_message,
     logger,
-    master,
+    publisher,
+    health_and_readiness,
   }: {
     send_message: (msg: string) => void
     logger: Logger
-    master: MasterPortfolioClass
+    publisher: PortfolioPublisher
+    health_and_readiness: HealthAndReadinessSubsystem
   }) {
     assert(logger)
     this.logger = logger
     assert(send_message)
-    this.master = master
+    this.send_message = send_message
+    this.publisher = publisher
+    this.portfolio_utils = new PortfolioUtils({ logger, sentry: Sentry })
+    this.health_and_readiness = health_and_readiness
+  }
+
+  async set_portfolio_for_exchange({
+    exchange_identifier,
+    portfolio,
+  }: {
+    exchange_identifier: ExchangeIdentifier
+    portfolio: Portfolio
+  }) {
+    // TODO: account not used in ExchangeIdentifier: default (default added so this appears in greps)
+    this.portfolios[exchange_identifier.exchange] = portfolio
+    this.report_current_portfolio() // this line is going to be a problem when we have multiple exchanges
+  }
+
+  async update_and_report_portfolio() {
+    for await (const exchange of Object.values(this.exchanges)) {
+      await exchange.update_portfolio_from_exchange()
+    }
+    await this.report_current_portfolio()
+  }
+
+  // this is called periodically or on orders and reports on the current portfolio
+  async report_current_portfolio() {
+    try {
+      let portfolio = await this.collapse_and_decorate_exchange_balances()
+      if (!portfolio) {
+        this.logger.info(`no portfolio, skipping`)
+        return
+      }
+
+      // This is just for the logfiles, we don't use this to send the event
+      try {
+        let msg = `B: ${portfolio.btc_value}, U: ${portfolio.usd_value}`
+        try {
+          msg += " as " + this.portfolio_utils.balances_to_string(portfolio, "BTC")
+        } catch (err) {
+          Sentry.captureException(err)
+          this.logger.error(err)
+        }
+        if (portfolio.prices) {
+          try {
+            msg += ` BTCUSDT: ${new BigNumber(portfolio.prices["BTCUSDT"]).dp(0).toFixed()}`
+          } catch (e) {
+            /* just ignore */
+          }
+        }
+        this.logger.info(msg)
+      } catch (err) {
+        // Not fatal, we just used it for logging anyway
+        Sentry.captureException(err)
+        this.logger.error(err)
+      }
+
+      await this.publisher.publish(portfolio)
+    } catch (err) {
+      Sentry.captureException(err)
+      this.logger.error(err)
+    }
+  }
+
+  async collapse_and_decorate_exchange_balances() {
+    if (!this.portfolios) {
+      this.logger.warn(`No portfolios present in portfilio-tracker`)
+      return
+    }
+    let exchanges: string[] = Object.keys(this.portfolios)
+    if (exchanges.length > 1) throw new Error(`Multiple exchanges not implemented yet`)
+    return this.decorate_portfolio(this.portfolios[exchanges[0]])
+  }
+
+  async decorate_portfolio(portfolio: Portfolio): Promise<Portfolio> {
+    portfolio = this.portfolio_utils.add_quote_value_to_portfolio_balances({
+      // TODO: convert to list
+      portfolio,
+      quote_currency: "BTC",
+    }).portfolio
+    portfolio = this.portfolio_utils.add_quote_value_to_portfolio_balances({
+      portfolio,
+      quote_currency: "USDT",
+    }).portfolio
+    portfolio.btc_value = this.portfolio_utils
+      .calculate_portfolio_value_in_quote_currency({ quote_currency: "BTC", portfolio })
+      .total // .dp(3)
+      .toFixed()
+    if (!portfolio.prices) throw new Error(`No prices`)
+    portfolio.usd_value = this.portfolio_utils
+      .convert_base_to_quote_currency({
+        base_quantity: new BigNumber(portfolio.btc_value),
+        base_currency: "BTC",
+        quote_currency: "USDT",
+        prices: portfolio.prices,
+      })
+      // .dp(0)
+      .toFixed()
+    return portfolio
+  }
+}
+
+export class BinancePortfolioToAMQP implements PortfolioBitchClass {
+  send_message: Function
+  logger: Logger
+  ee: BinanceType
+  master: MasterPortfolioClass // duplicated
+  portfolio_tracker: PortfolioTracker // duplicated
+  order_execution_tracker: OrderExecutionTracker
+  exchange_identifier: ExchangeIdentifier
+  portfolio: Portfolio = { balances: [] }
+  publisher: PortfolioPublisher
+  health_and_readiness: HealthAndReadinessSubsystem
+
+  constructor({
+    send_message,
+    logger,
+    health_and_readiness,
+  }: {
+    send_message: (msg: string) => void
+    logger: Logger
+    health_and_readiness: HealthAndReadinessSubsystem
+  }) {
+    assert(logger)
+    this.logger = logger
+    assert(send_message)
+
+    this.health_and_readiness = health_and_readiness
+
+    this.publisher = new PortfolioPublisher({
+      logger,
+      event_name: "SpotBinancePortfolio",
+      health_and_readiness,
+    })
+
+    this.portfolio_tracker = new PortfolioTracker({
+      logger,
+      send_message,
+      publisher: this.publisher,
+      health_and_readiness,
+    })
+
+    this.master = this.portfolio_tracker
     this.send_message = send_message
     logger.info("Live monitoring mode")
     this.exchange_identifier = { exchange: "binance", account: "default" }
@@ -88,6 +300,24 @@ export class BinancePortfolioTracker implements PortfolioBitchClass {
   }
 
   async start() {
+    try {
+      await this.publisher.connect()
+    } catch (error) {
+      Sentry.captureException(error)
+      this.logger.error(`Error connecting to AMQP: ${error}`)
+      this.logger.error(error)
+      this.logger.error(`Error connecting to AMQP: ${error.stack}`)
+      // TODO: this should be more like a HealthCheck class
+      this.health_and_readiness.healthy(false)
+      return
+    }
+
+    setInterval(
+      this.portfolio_tracker.update_and_report_portfolio.bind(this.portfolio_tracker),
+      1000 * 60 * 60 * 6
+    )
+    await this.update_portfolio_from_exchange() // automatically triggers report_current_portfolio
+
     this.order_execution_tracker.main()
   }
 
@@ -107,7 +337,9 @@ export class BinancePortfolioTracker implements PortfolioBitchClass {
 
   async get_prices_from_exchange() {
     // TODO: refresh prices but maybe cache them? If at daily close we enter lots of positions it would be good not to call this repeatedly
-    this.logger.warn(`Getting prices from exchange, this is not cached and If at daily close we enter lots of positions it would be good not to call this repeatedly.`)
+    this.logger.warn(
+      `Getting prices from exchange, this is not cached and If at daily close we enter lots of positions it would be good not to call this repeatedly.`
+    )
     try {
       return await this.ee.prices()
     } catch (error) {

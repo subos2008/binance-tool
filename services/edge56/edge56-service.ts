e93179b7e95f4955c0eaf60bad27aa6daf7a6f2f
Ryan Cocks <ryan@ryancocks.net>
2021-12-17 01:35:59 +0200

    21 days for edge56

--- services/edge56/edge56-service.ts
+++ services/edge56/edge56-service.ts
@@ -59,7 +59,7 @@ function sleep(ms: number) {
 let publisher: GenericTopicPublisher = new GenericTopicPublisher({ logger, event_name: "Edge56EntrySignal" })
 
 const edge56_parameters: Edge56Parameters = {
-  days_of_price_history: 20,
+  days_of_price_history: 21,
   long_highest_volume_in_days: 7,
 }
 

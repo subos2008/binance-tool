25784291de278160aaed66176e38ed9c64d33977
Ryan Cocks <ryan@ryancocks.net>
2022-01-16 07:35:12 -0300

    TAS: express logging

--- services/spot-trade-abstraction/tas.ts
+++ services/spot-trade-abstraction/tas.ts
@@ -28,21 +28,41 @@ BigNumber.prototype.valueOf = function () {
   throw Error("BigNumber .valueOf called!")
 }
 
-import { SendMessage, SendMessageFunc } from "../../lib/telegram-v2"
-import express, { Request, Response } from "express"
-import { TradeAbstractionService } from "./trade-abstraction-service"
-import { BinanceSpotExecutionEngine } from "./execution-engine"
-import { Positions } from "./positions"
-import { PositionsPersistance } from "./positions-persistance"
-import { RedisPositionsStateAdapter } from "./redis-positions-state-adapter"
-
 process.on("unhandledRejection", (error) => {
   logger.error(error)
   const send_message: SendMessageFunc = new SendMessage({ service_name, logger }).build()
   send_message(`UnhandledPromiseRejection: ${error}`)
 })
 
+import { SendMessage, SendMessageFunc } from "../../lib/telegram-v2"
+import { TradeAbstractionService } from "./trade-abstraction-service"
+import { BinanceSpotExecutionEngine } from "./execution-engine"
+import { Positions } from "./positions"
+import { PositionsPersistance } from "./positions-persistance"
+import { RedisPositionsStateAdapter } from "./redis-positions-state-adapter"
+
+import express, { Request, Response } from "express"
+const winston = require('winston')
+const expressWinston = require('express-winston');
+
+
 var app = express()
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function (req: Request, res: Response) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
 var bodyParser = require("body-parser")
 
 app.use(bodyParser.json()) // for parsing application/json

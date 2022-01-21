ecbe1f95068042a2775eee02163247ce06b9aba8
Ryan Cocks <ryan@ryancocks.net>
2022-01-21 13:07:00 -0300

    edge60: send_message when we initialise direction

--- services/edge60/direction-persistance.ts
+++ services/edge60/direction-persistance.ts
@@ -1,8 +1,8 @@
 import { RedisClient } from "redis"
 import { promisify } from "util"
-import { MarketIdentifier_V3 } from "../../events/shared/market-identifier"
 import { Logger } from "../../interfaces/logger"
 import { get_redis_client, set_redis_logger } from "../../lib/redis"
+import { SendMessageFunc } from "../../lib/telegram-v2"
 
 export type Direction = "short" | "long" // Redis returns null for unset
 
@@ -12,14 +12,24 @@ export class DirectionPersistance {
   private prefix: string
   private setAsync: any
   private getAsync: any
+  private send_message: SendMessageFunc
 
-  constructor({ logger, prefix }: { logger: Logger; prefix: string }) {
+  constructor({
+    logger,
+    prefix,
+    send_message,
+  }: {
+    logger: Logger
+    prefix: string
+    send_message: SendMessageFunc
+  }) {
     this.logger = logger
     set_redis_logger(logger)
     this.redis = get_redis_client()
     this.prefix = prefix
     this.setAsync = promisify(this.redis.set).bind(this.redis)
     this.getAsync = promisify(this.redis.get).bind(this.redis)
+    this.send_message = send_message
   }
 
   private _market_to_key(symbol: string): string {
@@ -28,6 +38,10 @@ export class DirectionPersistance {
 
   async set_direction(symbol: string, direction: Direction) {
     this.logger.info(`Setting direction ${direction} for ${symbol}`)
+    let previous_direction = await this.get_direction(symbol)
+    if (previous_direction === null) {
+      this.send_message(`Initialising edge60 direction for ${symbol} to ${direction}`)
+    }
     this.setAsync(this._market_to_key(symbol), direction)
   }
 

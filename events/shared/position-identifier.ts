802d34a82855edcbb1920ac67830e21871dca31b
Ryan Cocks <ryan@ryancocks.net>
2022-02-04 00:14:30 -0300

    HACK: always map edge60 to undefined

--- events/shared/position-identifier.ts
+++ events/shared/position-identifier.ts
@@ -32,7 +32,11 @@ export function check_edge(edge: string | undefined): AuthorisedEdgeType {
     Sentry.captureException(new Error(`check_edge: undefined value passed in`))
     return "undefined"
   }
-  if (edge === "edge60") return "edge60"
+  // if (edge === "edge60") return "edge60" HACK
+  if (edge === "edge60") {
+    console.warn(`Hack to map edge60 to undefined triggered`)
+    return "undefined" // HACK
+  }
   let msg = `Unauthorised edge: ${edge}`
   console.error(msg)
   Sentry.captureException(new Error(msg))

7cc1cf4a89e84a4b4aedd94345028895ef2d14f4
Ryan Cocks <ryan@ryancocks.net>
2022-02-04 21:03:05 -0300

    rename secret keys

--- k8/setup/add-binance-secret-to-namespace.sh
+++ k8/setup/add-binance-secret-to-namespace.sh
@@ -2,4 +2,4 @@
 
 source ../../.env
 
-kubectl create secret generic binance --from-literal=APIKEY="$APIKEY" --from-literal=APISECRET="$APISECRET" --namespace $1
+kubectl create secret generic binance --from-literal=BINANCE_API_KEY="$APIKEY" --from-literal=BINANCE_API_SECRET="$APISECRET" --namespace $1

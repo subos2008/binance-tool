6fcf07436f14d408d2a4d2ab646d4446c3ecc683
Ryan Cocks <ryan@ryancocks.net>
2022-02-07 05:40:39 -0300

    Revert "revert edge60 -> undefined hack"
    This reverts commit e3a125889f4d04f80636a0e4f9af305d306e3d4c.

--- classes/spot/abstractions/position-identifier.ts
+++ classes/spot/abstractions/position-identifier.ts
@@ -32,8 +32,10 @@ export function check_edge(edge: string | undefined): AuthorisedEdgeType {
     Sentry.captureException(new Error(`check_edge: undefined value passed in`))
     return "undefined"
   }
+  // if (edge === "edge60") return "edge60" HACK
   if (edge === "edge60") {
-    return edge
+    console.warn(`Hack to map edge60 to undefined triggered`)
+    return "undefined" // HACK
   }
   let msg = `Unauthorised edge: ${edge}`
   console.error(msg)

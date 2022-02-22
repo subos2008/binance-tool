6b61dfdf4988a57c566ba79ad18e7c6f3034ee16
Ryan Cocks <ryan@ryancocks.net>
2022-02-21 23:13:17 -0300

    429 handling in v2 send_message

--- lib/telegram-v2.ts
+++ lib/telegram-v2.ts
@@ -4,7 +4,7 @@ import { bind } from "lodash"
 import { URL } from "url"
 import { Logger } from "../interfaces/logger"
 
-const fetch = require("node-fetch")
+import fetch from "node-fetch"
 const Sentry = require("@sentry/node")
 
 export type SendMessageFunc = (msg: string) => Promise<void>
@@ -22,17 +22,29 @@ export class SendMessage {
       this.logger.error("Telegram message delivery not configured.")
       throw new Error(`TELEGRAM_KEY and/or TELEGRAM_CHAT_ID not set`)
     }
-    return bind((message: string) => {
-      this.logger.info(message)
-      try {
-        const url = new URL(`https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`)
-        url.searchParams.append("chat_id", process.env.TELEGRAM_CHAT_ID as string)
-        url.searchParams.append("text", `${this.service_name}: ${message}`)
-        return fetch(url)
-      } catch (error) {
-        Sentry.captureException(error)
-        this.logger.error(error)
+    return bind(this.send_message, this)
+  }
+
+  async send_message(message: string) {
+    this.logger.info(message)
+    try {
+      const url = new URL(`https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`)
+      url.searchParams.append("chat_id", process.env.TELEGRAM_CHAT_ID as string)
+      url.searchParams.append("text", `${this.service_name}: ${message}`)
+      let response = await fetch(url)
+      if (response.status == 429) {
+        // https://core.telegram.org/bots/faq#my-bot-is-hitting-limits-how-do-i-avoid-this
+        Sentry.captureException(new Error(`Hit rate limit on telegram API (429)`))
+        this.logger.warn(`Hit rate limit on telegram API (429)`)
+        setTimeout(bind(this.send_message, this, message), 1000 * 60)
       }
-    }, this)
+      if (response.status != 200) {
+        throw new Error(`Response status code from telegram api: ${response.status} ${response.statusText}`)
+      }
+    } catch (error) {
+      // few things throw
+      Sentry.captureException(error)
+      this.logger.error(error)
+    }
   }
 }

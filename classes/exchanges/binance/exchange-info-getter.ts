7bb105a97f487b2f2172bd972f8385825f2acb19
Ryan Cocks <ryan@ryancocks.net>
2022-03-01 02:41:01 -0300

    Maybe fix exchangeInfo storms

--- classes/exchanges/binance/exchange-info-getter.ts
+++ classes/exchanges/binance/exchange-info-getter.ts
@@ -2,19 +2,26 @@ import { Binance, ExchangeInfo } from "binance-api-node"
 
 export class BinanceExchangeInfoGetter {
   private ee: Binance
-  private exchange_info: ExchangeInfo | null | undefined
+  private exchange_info_promise: Promise<ExchangeInfo> | null | undefined
 
   constructor({ ee }: { ee: Binance }) {
     this.ee = ee
   }
 
   async get_exchange_info(): Promise<ExchangeInfo> {
-    if (!this.exchange_info) {
-      this.exchange_info = await this.ee.exchangeInfo()
-      setTimeout(() => {
-        this.exchange_info = null
-      },10 * 60 * 1000).unref()
+    if (this.exchange_info_promise) {
+      return this.exchange_info_promise
     }
-    return this.exchange_info
+
+    console.warn(`exchange_info not cached, reloading`)
+
+    if (!this.exchange_info_promise) {
+      setTimeout(() => {
+        this.exchange_info_promise = null
+      }, 10 * 60 * 1000).unref()
+    }
+
+    this.exchange_info_promise = this.ee.exchangeInfo()
+    return this.exchange_info_promise
   }
 }

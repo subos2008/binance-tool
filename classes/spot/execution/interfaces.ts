d1c40fc80ba8042365b9601efafc0f96081ae289
Ryan Cocks <ryan@ryancocks.net>
2022-03-08 23:59:51 -0300

    Refactor edge60

---
+++ classes/spot/execution/interfaces.ts
@@ -0,0 +1,27 @@
+import { BigNumber } from "bignumber.js"
+BigNumber.DEBUG = true // Prevent NaN
+// Prevent type coercion
+BigNumber.prototype.valueOf = function () {
+  throw Error("BigNumber .valueOf called!")
+}
+
+import { AuthorisedEdgeType } from "../abstractions/position-identifier";
+
+export interface PositionEntryArgs {
+    quote_asset: string
+    base_asset: string
+    direction: string
+    edge: AuthorisedEdgeType
+}
+
+export interface PositionEntryReturn {
+  executed_quote_quantity: string
+  stop_order_id: string | number | undefined
+  executed_price: BigNumber
+  stop_price: BigNumber
+}
+
+export interface PositionEntry {
+ open_position(args: PositionEntryArgs): Promise<PositionEntryReturn>
+
+}

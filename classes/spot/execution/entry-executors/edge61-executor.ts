ba1b0e1b90da8f4306671f8d5aaff26934dee628
Ryan Cocks <ryan@ryancocks.net>
2022-03-11 16:52:58 -0300

    e61 open_position Sentry capture

--- classes/spot/execution/entry-executors/edge61-executor.ts
+++ classes/spot/execution/entry-executors/edge61-executor.ts
@@ -89,113 +89,118 @@ export class Edge61SpotPositionsExecution {
     stop_price: BigNumber
     take_profit_price: BigNumber
   }> {
-    args.edge = check_edge(args.edge)
-    assert.equal(args.edge, "edge61")
-    let { trigger_price, edge } = args
-
-    let edge_percentage_stop = new BigNumber(5)
-    let edge_percentage_stop_limit = new BigNumber(15)
-    let edge_percentage_take_profit = new BigNumber(5)
-    let edge_percentage_buy_limit = new BigNumber(0.5)
-
-    this.send_message(`Opening Spot position ${edge}:${args.base_asset} using ${args.quote_asset}`)
-
-    /**
-     * TODO: Make this trading rules instead
-     */
-
-    /**
-     * Get the position size, -- this can be hardcoded, just needs price or to specify quote amount to spend
-     * Try and execute a buy on that position size
-     * Create sell order at the stop price for any amount that was executed for the buy
-     */
-
-    let quote_amount = await this.position_sizer.position_size_in_quote_asset(args)
-    let order_context: OrderContext_V1 = { edge, object_type: "OrderContext", version: 1 }
-    let limit_price_factor = new BigNumber(100).plus(edge_percentage_buy_limit).div(100)
-    let limit_price = trigger_price.times(limit_price_factor)
-    this.logger.info(
-      `Calculated buy_limit price of ${limit_price.toFixed()} given trigger_price of ${trigger_price.toFixed()} (${edge_percentage_buy_limit.toFixed()}%)`
-    )
-    let base_amount = quote_amount.dividedBy(limit_price)
-    let cmd: SpotLimitBuyCommand = {
-      order_context,
-      market_identifier: this.get_market_identifier_for(args),
-      base_amount,
-      limit_price,
-      order_type: "ioc",
-    }
-    let buy_result = await this.ee.limit_buy(cmd)
-    let { executed_quote_quantity, executed_price, executed_base_quantity } = buy_result
-
-    if (executed_base_quantity.isZero()) {
-      let msg = `${edge}:${args.base_asset} IOC limit buy executed zero`
-      this.logger.info(msg)
-      throw new Error(msg)
-    }
-
-    let stop_price_factor = new BigNumber(100).minus(edge_percentage_stop).div(100)
-    let stop_price = trigger_price.times(stop_price_factor)
-    let stop_limit_price_factor = new BigNumber(100).minus(edge_percentage_stop_limit).div(100)
-    let stop_limit_price = trigger_price.times(stop_limit_price_factor)
-    this.logger.info(
-      `Calculated stop price of ${stop_price.toFixed()} given trigger price of ${trigger_price.toFixed()}`
-    )
-
-    let take_profit_price_factor = new BigNumber(100).plus(edge_percentage_take_profit).div(100)
-    let take_profit_price = trigger_price.times(take_profit_price_factor)
-    this.logger.info(
-      `Calculated take profit price of ${take_profit_price.toFixed()} given trigger price of ${trigger_price.toFixed()}`
-    )
-
-    let { clientOrderId: stop_ClientOrderId } = await this.ee.store_order_context_and_generate_clientOrderId(
-      cmd.order_context
-    )
-    let { clientOrderId: take_profit_ClientOrderId } =
-      await this.ee.store_order_context_and_generate_clientOrderId(cmd.order_context)
-    let { clientOrderId: oco_list_ClientOrderId } = await this.ee.store_order_context_and_generate_clientOrderId(
-      cmd.order_context
-    )
-    let spot_position_identifier: SpotPositionIdentifier_V3 = {
-      exchange_identifier: this.get_exchange_identifier(),
-      base_asset: args.base_asset,
-      edge,
-    }
-    await this.positions_persistance.set_oco_order(spot_position_identifier, oco_list_ClientOrderId)
-
-    let oco_cmd: SpotOCOSellCommand = {
-      order_context,
-      market_identifier: cmd.market_identifier,
-      base_amount: executed_base_quantity,
-      stop_price,
-      stop_limit_price,
-      take_profit_price,
-      stop_ClientOrderId,
-      take_profit_ClientOrderId,
-      oco_list_ClientOrderId,
-    }
-
     try {
-      let oco_result = await this.ee.oco_sell_order(oco_cmd)
+      args.edge = check_edge(args.edge)
+      assert.equal(args.edge, "edge61")
+      let { trigger_price, edge } = args
+
+      let edge_percentage_stop = new BigNumber(5)
+      let edge_percentage_stop_limit = new BigNumber(15)
+      let edge_percentage_take_profit = new BigNumber(5)
+      let edge_percentage_buy_limit = new BigNumber(0.5)
+
+      this.send_message(`Opening Spot position ${edge}:${args.base_asset} using ${args.quote_asset}`)
+
+      /**
+       * TODO: Make this trading rules instead
+       */
+
+      /**
+       * Get the position size, -- this can be hardcoded, just needs price or to specify quote amount to spend
+       * Try and execute a buy on that position size
+       * Create sell order at the stop price for any amount that was executed for the buy
+       */
+
+      let quote_amount = await this.position_sizer.position_size_in_quote_asset(args)
+      let order_context: OrderContext_V1 = { edge, object_type: "OrderContext", version: 1 }
+      let limit_price_factor = new BigNumber(100).plus(edge_percentage_buy_limit).div(100)
+      let limit_price = trigger_price.times(limit_price_factor)
+      this.logger.info(
+        `Calculated buy_limit price of ${limit_price.toFixed()} given trigger_price of ${trigger_price.toFixed()} (${edge_percentage_buy_limit.toFixed()}%)`
+      )
+      let base_amount = quote_amount.dividedBy(limit_price)
+      let cmd: SpotLimitBuyCommand = {
+        order_context,
+        market_identifier: this.get_market_identifier_for(args),
+        base_amount,
+        limit_price,
+        order_type: "ioc",
+      }
+      let buy_result = await this.ee.limit_buy(cmd)
+      let { executed_quote_quantity, executed_price, executed_base_quantity } = buy_result
+
+      if (executed_base_quantity.isZero()) {
+        let msg = `${edge}:${args.base_asset} IOC limit buy executed zero`
+        this.logger.info(msg)
+        throw new Error(msg)
+      }
+
+      let stop_price_factor = new BigNumber(100).minus(edge_percentage_stop).div(100)
+      let stop_price = trigger_price.times(stop_price_factor)
+      let stop_limit_price_factor = new BigNumber(100).minus(edge_percentage_stop_limit).div(100)
+      let stop_limit_price = trigger_price.times(stop_limit_price_factor)
+      this.logger.info(
+        `Calculated stop price of ${stop_price.toFixed()} given trigger price of ${trigger_price.toFixed()}`
+      )
+
+      let take_profit_price_factor = new BigNumber(100).plus(edge_percentage_take_profit).div(100)
+      let take_profit_price = trigger_price.times(take_profit_price_factor)
+      this.logger.info(
+        `Calculated take profit price of ${take_profit_price.toFixed()} given trigger price of ${trigger_price.toFixed()}`
+      )
+
+      let { clientOrderId: stop_ClientOrderId } = await this.ee.store_order_context_and_generate_clientOrderId(
+        cmd.order_context
+      )
+      let { clientOrderId: take_profit_ClientOrderId } =
+        await this.ee.store_order_context_and_generate_clientOrderId(cmd.order_context)
+      let { clientOrderId: oco_list_ClientOrderId } = await this.ee.store_order_context_and_generate_clientOrderId(
+        cmd.order_context
+      )
+      let spot_position_identifier: SpotPositionIdentifier_V3 = {
+        exchange_identifier: this.get_exchange_identifier(),
+        base_asset: args.base_asset,
+        edge,
+      }
+      await this.positions_persistance.set_oco_order(spot_position_identifier, oco_list_ClientOrderId)
+
+      let oco_cmd: SpotOCOSellCommand = {
+        order_context,
+        market_identifier: cmd.market_identifier,
+        base_amount: executed_base_quantity,
+        stop_price,
+        stop_limit_price,
+        take_profit_price,
+        stop_ClientOrderId,
+        take_profit_ClientOrderId,
+        oco_list_ClientOrderId,
+      }
+
+      try {
+        let oco_result = await this.ee.oco_sell_order(oco_cmd)
+      } catch (error) {
+        Sentry.captureException(error)
+        this.send_message(
+          `Failed to create oco order for ${edge}:${args.base_asset} on ${
+            oco_cmd.market_identifier.symbol
+          } at ${stop_price.toFixed()}`
+        )
+        throw error
+      }
+
+      return {
+        executed_quote_quantity: executed_quote_quantity.toFixed(),
+        executed_base_quantity: executed_base_quantity.toFixed(),
+        oco_order_id: oco_list_ClientOrderId,
+        stop_order_id: stop_ClientOrderId,
+        take_profit_order_id: take_profit_ClientOrderId,
+        executed_price,
+        stop_price,
+        take_profit_price,
+      }
     } catch (error) {
       Sentry.captureException(error)
-      this.send_message(
-        `Failed to create oco order for ${edge}:${args.base_asset} on ${
-          oco_cmd.market_identifier.symbol
-        } at ${stop_price.toFixed()}`
-      )
       throw error
-    }
-
-    return {
-      executed_quote_quantity: executed_quote_quantity.toFixed(),
-      executed_base_quantity: executed_base_quantity.toFixed(),
-      oco_order_id: oco_list_ClientOrderId,
-      stop_order_id: stop_ClientOrderId,
-      take_profit_order_id: take_profit_ClientOrderId,
-      executed_price,
-      stop_price,
-      take_profit_price,
     }
   }
 }

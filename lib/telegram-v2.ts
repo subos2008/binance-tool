20f00e87a805b76eaa825e5cca9b4c9b3390b111
Ryan Cocks <ryan@ryancocks.net>
2022-03-16 23:14:56 -0300

    Add ContextTags to send_message (unused)

--- lib/telegram-v2.ts
+++ lib/telegram-v2.ts
@@ -5,10 +5,14 @@ import { URL } from "url"
 import { Logger } from "../interfaces/logger"
 
 import fetch from "node-fetch"
+import { AuthorisedEdgeType } from "../classes/spot/abstractions/position-identifier"
 const Sentry = require("@sentry/node")
 
-export type SendMessageFunc = (msg: string) => Promise<void>
+export type SendMessageFunc = (msg: string, tags?: ContextTags) => Promise<void>
 
+export interface ContextTags {
+  edge?: string | AuthorisedEdgeType
+}
 export class SendMessage {
   service_name: string
   logger: Logger
@@ -25,18 +29,23 @@ export class SendMessage {
     return bind(this.send_message, this)
   }
 
-  async send_message(message: string) {
+  get_chat_id(tags?: ContextTags): string {
+    if (tags?.edge === "edge61") return "682031175"
+    return process.env.TELEGRAM_CHAT_ID as string
+  }
+
+  async send_message(message: string, tags?: ContextTags) {
     this.logger.info(message)
     try {
       const url = new URL(`https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/sendMessage`)
-      url.searchParams.append("chat_id", process.env.TELEGRAM_CHAT_ID as string)
+      url.searchParams.append("chat_id", this.get_chat_id(tags))
       url.searchParams.append("text", `${this.service_name}: ${message}`)
       let response = await fetch(url)
       if (response.status == 429) {
         // https://core.telegram.org/bots/faq#my-bot-is-hitting-limits-how-do-i-avoid-this
         Sentry.captureException(new Error(`Hit rate limit on telegram API (429)`))
         this.logger.warn(`Hit rate limit on telegram API (429)`)
-        setTimeout(this.send_message.bind(this, message), 1000 * 60)
+        setTimeout(this.send_message.bind(this, message, tags), 1000 * 60)
       }
       if (response.status != 200) {
         throw new Error(`Response status code from telegram api: ${response.status} ${response.statusText}`)

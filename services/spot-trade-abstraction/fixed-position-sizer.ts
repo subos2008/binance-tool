d66756730406004062f18dff9a31f72fe9035444
Ryan Cocks <ryan@ryancocks.net>
2022-03-21 13:43:32 -0300

    Smaller PositionSize for edge61

--- services/spot-trade-abstraction/fixed-position-sizer.ts
+++ services/spot-trade-abstraction/fixed-position-sizer.ts
@@ -1,21 +1,20 @@
 import { Logger } from "../../interfaces/logger"
 import { strict as assert } from "assert"
-import { MarketIdentifier_V3 } from "../../events/shared/market-identifier"
 
 import { BigNumber } from "bignumber.js"
+import { AuthorisedEdgeType } from "../../classes/spot/abstractions/position-identifier"
 BigNumber.DEBUG = true // Prevent NaN
 // Prevent type coercion
 BigNumber.prototype.valueOf = function () {
   throw Error("BigNumber .valueOf called!")
 }
 
-/** Configuration */
-let fixed_position_size = {
-  quote_asset: "BUSD",
-  quote_amount: new BigNumber(150),
-}
 export interface PositionSizer {
-  position_size_in_quote_asset(args: { base_asset: string; quote_asset: string }): Promise<BigNumber>
+  position_size_in_quote_asset(args: {
+    base_asset: string
+    quote_asset: string
+    edge: AuthorisedEdgeType
+  }): Promise<BigNumber>
 }
 export class FixedPositionSizer implements PositionSizer {
   logger: Logger
@@ -28,11 +27,22 @@ export class FixedPositionSizer implements PositionSizer {
   async position_size_in_quote_asset({
     base_asset,
     quote_asset,
+    edge,
   }: {
     base_asset: string
     quote_asset: string
+    edge: string
   }): Promise<BigNumber> {
-    assert(fixed_position_size.quote_asset === quote_asset)
-    return fixed_position_size.quote_amount
+    switch (edge) {
+      case "edge60":
+        assert("BUSD" === quote_asset)
+        return new BigNumber(150)
+
+      case "edge61":
+        assert("BUSD" === quote_asset)
+        return new BigNumber(50)
+      default:
+        throw new Error(`Unknoen edge in positionSizer: ${edge}`)
+    }
   }
 }

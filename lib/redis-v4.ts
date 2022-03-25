2ea9b08ad83bb3026000ce0e977ebf7450fe9f65
Ryan Cocks <ryan@ryancocks.net>
2022-03-24 21:47:21 -0300

    Tweak redis error messages

--- lib/redis-v4.ts
+++ lib/redis-v4.ts
@@ -13,22 +13,22 @@ export async function get_redis_client(
 ): Promise<RedisClientType> {
   let url = `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}`
 
-  let reconnectInvocationCounter = 0;
+  let reconnectInvocationCounter = 0
 
   const redis: RedisClientType = createClient({
     url,
     // retry_strategy: redis_retry_strategy,
     socket: {
       reconnectStrategy: (retries) => {
-        reconnectInvocationCounter++;
+        reconnectInvocationCounter++
 
         if (retries < 5) {
-          return 0;
+          return 0
         }
 
-        return new Error("No more retries remaining, giving up.");
-      }
-    }
+        return new Error("No more retries remaining, giving up.")
+      },
+    },
   })
 
   redis.on("ready", function () {
@@ -39,14 +39,11 @@ export async function get_redis_client(
   })
 
   redis.on("error", function (err: any) {
+    logger.error({ msg: `Redis.on error: ${err.toString()}`, err })
     let obj = { object_type: "RedisConnectionStatus", ready: false, REDIS_HOST: process.env.REDIS_HOST }
-    logger.error(`Redis disconnected: ${err.toString()}`)
     logger.object(obj)
     logger.warn(`Not setting redis-v4 as unhealthy on.error event`)
     // health_and_readiness.healthy(false)
-    logger.error("Redis.on errror handler called")
-    logger.error(err.stack)
-    logger.error(err)
     Sentry.withScope(function (scope: any) {
       scope.setTag("location", "redis-global-error-handler")
       Sentry.captureException(err)

204b410d4499f2bac45c644b2779b9fb12f26e1f
Ryan Cocks <ryan@ryancocks.net>
2022-03-30 00:47:47 -0300

    add redis auth copy in persistent-state namespace

--- k8/setup/add-redis-secret-to-namespace.sh
+++ k8/setup/add-redis-secret-to-namespace.sh
@@ -4,3 +4,4 @@ source ../../.env
 
 kubectl create secret generic redis --from-literal=REDIS_HOST="$REDIS_HOST" --namespace $1
 kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace $1
+kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace persistent-state

cf7040e0b871dc779981d29853b11b4bb1d0571a
Ryan Cocks <ryan@ryancocks.net>
2022-04-04 01:33:10 -0300

    Logger interface

--- interfaces/logger.ts
+++ interfaces/logger.ts
@@ -1,8 +1,10 @@
+// NB: made it mandatory to supply a tags object as the first arg,
+//  err is a special tag for passing exceptions
 export interface Logger {
-  info(...message: any[]): void
-  error(...message: any[]): void
-  warn(...message: any[]): void
-  debug(...message: any[]): void
-  silly(...message: any[]): void
-  object(...message: any[]): void // dump object as single line to console info
+  info(tags: Object, ...message: any[]): void
+  error(tags: Object, ...message: any[]): void
+  warn(tags: Object, ...message: any[]): void
+  debug(tags: Object, ...message: any[]): void
+  silly(tags: Object, ...message: any[]): void
+  object(tags: Object, ...message: any[]): void // dump object as single line to console info
 }

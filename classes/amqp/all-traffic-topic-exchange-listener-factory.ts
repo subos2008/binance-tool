93de32da1dde7cd243a2cf6302a3b9c88051bff9
Ryan Cocks <ryan@ryancocks.net>
2022-05-03 11:47:39 -0300

    Add event-logger-all-events

---
+++ classes/amqp/all-traffic-topic-exchange-listener-factory.ts
@@ -0,0 +1,143 @@
+#!./node_modules/.bin/ts-node
+/* eslint-disable no-console */
+/* eslint func-names: ["warn", "as-needed"] */
+
+// const amqp_exchange_name = "positions"
+
+const connect_options = require("../../lib/amqp/connect_options").default
+
+import * as Sentry from "@sentry/node"
+
+import { Channel, connect, Connection } from "amqplib"
+import { assert } from "console"
+import { Logger } from "../../interfaces/logger"
+import { HealthAndReadinessSubsystem } from "../health_and_readiness"
+import { MessageProcessor } from "./interfaces"
+import { MessageRouting, MyEventNameType } from "./message-routing"
+
+// A Factory / router where you give it an event type and some other shit like an exchange identifier
+// and it gives you the amqp connection / queue binding that calls your callback?
+
+// This class could also have a buddy class that
+// set up and check for all the expected queues and maybe even have an admin access to RabbitMQ?
+
+/**
+ * PublisherFactory
+ * ListenerFactory
+ */
+
+/**
+ * What do we currently have in our routing?
+ * - MyEventNameType
+ * - ExchangeIdentifier
+ * - exchange (AMQP exchange name)
+ * - routing_keys
+ *
+ * */
+
+// Prevents unhandled exceptions from MessageProcessor's
+class MessageProcessorIsolator implements MessageProcessor {
+  message_processor: MessageProcessor
+  logger: Logger
+  health_and_readiness: HealthAndReadinessSubsystem
+
+  constructor({
+    message_processor,
+    logger,
+    health_and_readiness,
+  }: {
+    message_processor: MessageProcessor
+    logger: Logger
+    health_and_readiness: HealthAndReadinessSubsystem
+  }) {
+    assert(message_processor && logger)
+    this.message_processor = message_processor
+    this.logger = logger
+    this.health_and_readiness = health_and_readiness
+  }
+  async process_message(event: any, channel: Channel): Promise<void> {
+    // TODO: sentry scope
+    let Body
+    try {
+      Body = JSON.parse(event.content.toString())
+      return this.message_processor.process_message(event, channel)
+    } catch (err) {
+      // Eat any exceptions to prevent this handler from affecting the process
+      // Designed for having multiple independent listeners in one process
+      Sentry.captureException(err, { extra: Body })
+      this.logger.warn({ err })
+    }
+  }
+}
+
+export class ListenerFactory {
+  logger: Logger
+
+  constructor({ logger }: { logger: Logger }) {
+    this.logger = logger
+  }
+
+  // isolated means it's wrapped in an exception catcher/eater
+  // You might not want to await on this in case it hangs?
+  async build_isolated_listener({
+    message_processor,
+    health_and_readiness,
+  }: {
+    message_processor: MessageProcessor
+    health_and_readiness: HealthAndReadinessSubsystem
+  }) {
+    Sentry.withScope(async (scope) => {
+      try {
+        assert(message_processor)
+        await this.connect({
+          health_and_readiness,
+          message_processor: new MessageProcessorIsolator({
+            message_processor,
+            logger: this.logger,
+            health_and_readiness,
+          }),
+        })
+      } catch (err) {
+        health_and_readiness.healthy(false)
+        this.logger.error({ err }, `Error connecting MessageProcessor to amqp server`)
+        Sentry.captureException(err)
+        // throw err // don't throw when setting up isolated infrastructure
+      }
+    })
+  }
+
+  private async connect({
+    message_processor,
+    health_and_readiness,
+  }: {
+    message_processor: MessageProcessor
+    health_and_readiness: HealthAndReadinessSubsystem
+  }) {
+    let exchange_name = "binance-tool"
+    let exchange_type = "topic"
+    // TODO: durable, exclusive, noAck, ... lots of configurable shit here...
+    let { routing_key, durable } = { durable: false, routing_key: "#" } // '#' gets all messages
+    let connection: Connection = await connect(connect_options)
+    process.once("SIGINT", connection.close.bind(connection))
+    this.logger.info(`PositionsListener: Connection with AMQP server established.`)
+    let channel: Channel = await connection.createChannel() // hangs
+    let logger = this.logger
+    channel.on("close", function () {
+      logger.error(`AMQP Channel closed!`)
+      health_and_readiness.healthy(false)
+    })
+    health_and_readiness.healthy(true)
+    health_and_readiness.ready(true)
+    // TODO: do we not look at the return code here?
+    await channel.assertExchange(exchange_name, exchange_type, { durable })
+    const q = await channel.assertQueue("", { exclusive: true })
+    await channel.bindQueue(q.queue, exchange_name, routing_key)
+    let wrapper_func = function (event: any) {
+      message_processor.process_message(event, channel)
+    }
+    channel.consume(q.queue, wrapper_func, { noAck: false })
+    this.logger.info(
+      `ListenerFactory: waiting for events on AMQP: exchange: ${exchange_type}:${exchange_name}, routing_key: ${routing_key}.`
+    )
+  }
+}
---
+++ services/event-logger-all-events/event-logger-all-events-service.ts
@@ -0,0 +1,131 @@
+#!./node_modules/.bin/ts-node
+/* eslint-disable no-console */
+/* eslint func-names: ["warn", "as-needed"] */
+
+/**
+ * Event/message listener
+ */
+
+import { strict as assert } from "assert"
+const service_name = "event-persistance"
+
+import { ListenerFactory } from "../../classes/amqp/all-traffic-topic-exchange-listener-factory"
+import { HealthAndReadiness, HealthAndReadinessSubsystem } from "../../classes/health_and_readiness"
+
+require("dotenv").config()
+
+import * as Sentry from "@sentry/node"
+Sentry.init({})
+Sentry.configureScope(function (scope: any) {
+  scope.setTag("service", service_name)
+})
+
+import { Logger } from "../../interfaces/logger"
+const LoggerClass = require("../../lib/faux_logger")
+const logger: Logger = new LoggerClass({ silent: false })
+
+import { SendMessage, SendMessageFunc } from "../../lib/telegram-v2"
+const send_message: SendMessageFunc = new SendMessage({ service_name, logger }).build()
+
+process.on("unhandledRejection", (err) => {
+  logger.error({ err })
+  Sentry.captureException(err)
+  send_message(`UnhandledPromiseRejection: ${err}`)
+})
+
+import { MessageProcessor } from "../../classes/amqp/interfaces"
+
+let region = "ap-southeast-1"
+import { PutObjectCommand, PutObjectRequest, S3Client } from "@aws-sdk/client-s3"
+assert(process.env.AWS_ACCESS_KEY_ID)
+assert(process.env.AWS_SECRET_ACCESS_KEY)
+const s3Client = new S3Client({ region })
+let Bucket = "binance-tool-event-storage"
+import { randomUUID } from "crypto"
+
+let listener_factory = new ListenerFactory({ logger })
+class EventLogger implements MessageProcessor {
+  send_message: Function
+  logger: Logger
+
+  constructor({
+    send_message,
+    logger,
+    health_and_readiness,
+  }: {
+    send_message: (msg: string) => void
+    logger: Logger
+    health_and_readiness: HealthAndReadiness
+  }) {
+    assert(logger)
+    this.logger = logger
+    assert(send_message)
+    this.send_message = send_message
+    const amqp_health: HealthAndReadinessSubsystem = health_and_readiness.addSubsystem({
+      name: `amqp-listener`,
+      ready: false,
+      healthy: false,
+    })
+    listener_factory.build_isolated_listener({
+      message_processor: this,
+      health_and_readiness: amqp_health,
+    }) // Add arbitrary data argument
+  }
+
+  async process_message(event: any, channel: Channel) {
+    try {
+      this.logger.info(event)
+      let event_name = event.object_type || "Orphaned"
+      let Body = event.content.toString()
+      let Key = `${event_name}/${+new Date()}-${randomUUID()}.json` // ms timestamp
+      let params: PutObjectRequest = { Bucket, Key, Body }
+      const results = await s3Client.send(new PutObjectCommand(params))
+      console.log("Successfully created " + params.Key + " and uploaded it to " + params.Bucket + "/" + params.Key)
+      channel.ack(event)
+    } catch (err) {
+      Sentry.captureException(err)
+      this.logger.error({ err })
+    }
+  }
+}
+
+const health_and_readiness = new HealthAndReadiness({ logger, send_message })
+const service_is_healthy: HealthAndReadinessSubsystem = health_and_readiness.addSubsystem({
+  name: "global",
+  ready: true,
+  healthy: true,
+})
+
+async function main() {
+  const execSync = require("child_process").execSync
+  execSync("date -u")
+
+  new EventLogger({ health_and_readiness, logger, send_message })
+}
+
+main().catch((err) => {
+  Sentry.captureException(err)
+  logger.error(`Error in main loop: ${err}`)
+  logger.error({ err })
+  logger.error(`Error in main loop: ${err.stack}`)
+  soft_exit(1, `Error in main loop: ${err}`)
+})
+
+// Note this method returns!
+// Shuts down everything that's keeping us alive so we exit
+function soft_exit(exit_code: number | null = null, reason: string) {
+  service_is_healthy.healthy(false) // it seems service isn't exiting on soft exit, but add this to make sure
+  logger.warn(`soft_exit called, exit_code: ${exit_code}`)
+  if (exit_code) logger.warn(`soft_exit called with non-zero exit_code: ${exit_code}, reason: ${reason}`)
+  if (exit_code) process.exitCode = exit_code
+  Sentry.close(500)
+  // setTimeout(dump_keepalive, 10000); // note enabling this debug line will delay exit until it executes
+}
+
+import { Channel } from "amqplib"
+import express from "express"
+var app = express()
+app.get("/health", health_and_readiness.health_handler.bind(health_and_readiness))
+const port = "80"
+app.listen(port)
+logger.info(`Server on port ${port}`)

97fc3ffead511b6aed15b0bf5e586120a990f219
Ryan Cocks <ryan@ryancocks.net>
2022-05-04 23:07:21 -0300

    Add binance futures orders to amqp

---
+++ services/binance/futures/orders-to-amqp/binance-futures-orders-to-amqp-service.ts
@@ -0,0 +1,108 @@
+#!./node_modules/.bin/ts-node
+/* eslint-disable no-console */
+/* eslint func-names: ["warn", "as-needed"] */
+
+// portfolio-publisher service:
+//  Publishes the portfolio to AMQP:
+//    1. on startup
+//    2. monitoring the order streams and re-publishing on any changes
+//    3. Periodically
+//
+// On changes:
+//  1. Publishes to AMQP: portfolio with current price information
+//
+// Thoughts/TODO:
+//  1. Could also check redis-trades matches position sizes
+//  1. Doesn't currently re-publish on deposits/withdrawals
+
+// Config
+const service_name = "binance-futures-orders-to-amqp"
+
+import { HealthAndReadiness } from "../../../../classes/health_and_readiness"
+
+require("dotenv").config()
+
+import * as Sentry from "@sentry/node"
+Sentry.init({})
+Sentry.configureScope(function (scope: any) {
+  scope.setTag("service", service_name)
+})
+
+import { Logger } from "../../../../lib/faux_logger"
+const logger: Logger = new Logger({ silent: false })
+
+import { SendMessage, SendMessageFunc } from "../../../../lib/telegram-v2"
+const send_message: SendMessageFunc = new SendMessage({ service_name, logger }).build()
+
+import { BigNumber } from "bignumber.js"
+BigNumber.DEBUG = true // Prevent NaN
+// Prevent type coercion
+BigNumber.prototype.valueOf = function () {
+  throw Error("BigNumber .valueOf called!")
+}
+
+process.on("unhandledRejection", (err) => {
+  logger.error({ err })
+  Sentry.captureException(err)
+  send_message(`UnhandledPromiseRejection: ${err}`)
+})
+
+const exchange_identifier: ExchangeIdentifier_V3 = {
+  version: "v3",
+  exchange: "binance",
+  account: "default",
+  type: "futures",
+}
+
+import { BinanceFuturesOrdersToAMQP } from "./binance-futures-orders-to-amqp"
+
+import { get_redis_client, set_redis_logger } from "../../../../lib/redis"
+
+const health = new HealthAndReadiness({ logger, send_message })
+const service_is_healthy = health.addSubsystem({ name: "global", ready: true, healthy: true })
+
+async function main() {
+  const execSync = require("child_process").execSync
+  execSync("date -u")
+
+  set_redis_logger(logger)
+  let redis = get_redis_client()
+
+  try {
+    let health_and_readiness = health.addSubsystem({
+      name: "binance-orders-to-amqp",
+      ready: false,
+      healthy: false,
+    })
+    let portfolio_to_amqp = new BinanceFuturesOrdersToAMQP({
+      send_message,
+      logger,
+      health_and_readiness,
+      redis,
+      exchange_identifier,
+    })
+    await portfolio_to_amqp.start()
+  } catch (err: any) {
+    Sentry.captureException(err)
+    logger.error(`Error connecting to exchange: ${err}`)
+    logger.error({ err })
+    logger.error(`Error connecting to exchange: ${err.stack}`)
+    service_is_healthy.healthy(false) // it seems service isn't exiting on soft exit, but add this to make sure
+    return
+  }
+}
+
+main().catch((err) => {
+  Sentry.captureException(err)
+  logger.error(`Error in main loop: ${err}`)
+  logger.error({ err })
+  logger.error(`Error in main loop: ${err.stack}`)
+})
+
+import express from "express"
+import { ExchangeIdentifier_V3 } from "../../../../events/shared/exchange-identifier"
+var app = express()
+app.get("/health", health.health_handler.bind(health))
+const port = "80"
+app.listen(port)
+logger.info(`Server on port ${port}`)
---
+++ services/binance/futures/orders-to-amqp/binance-futures-orders-to-amqp.ts
@@ -0,0 +1,91 @@
+import { strict as assert } from "assert"
+const service_name = "binance-orders-to-amqp"
+
+import { Binance as BinanceType } from "binance-api-node"
+import Binance from "binance-api-node"
+
+import * as Sentry from "@sentry/node"
+Sentry.init({})
+Sentry.configureScope(function (scope: any) {
+  scope.setTag("service", service_name)
+})
+
+import { Logger } from "../../../../interfaces/logger"
+
+import { BigNumber } from "bignumber.js"
+BigNumber.DEBUG = true // Prevent NaN
+// Prevent type coercion
+BigNumber.prototype.valueOf = function () {
+  throw Error("BigNumber .valueOf called!")
+}
+
+import { BinanceOrderData } from "../../../../interfaces/order_callbacks"
+import { ExchangeIdentifier_V3 } from "../../../../events/shared/exchange-identifier"
+import { HealthAndReadinessSubsystem } from "../../../../classes/health_and_readiness"
+import { RedisClient } from "redis"
+import { RedisOrderContextPersistance } from "../../../../classes/spot/persistence/redis-implementation/redis-order-context-persistence"
+import { BinanceOrderPublisher } from "../../lib/binance-order-publisher"
+import { OrderExecutionTracker } from "../../../../classes/exchanges/binance/order_execution_tracker"
+
+export class BinanceFuturesOrdersToAMQP {
+  logger: Logger
+  ee: BinanceType
+  order_execution_tracker: OrderExecutionTracker
+  exchange_identifier: ExchangeIdentifier_V3
+  health_and_readiness: HealthAndReadinessSubsystem
+  publisher: BinanceOrderPublisher
+
+  constructor({
+    send_message,
+    logger,
+    health_and_readiness,
+    redis,
+    exchange_identifier,
+  }: {
+    send_message: (msg: string) => void
+    logger: Logger
+    health_and_readiness: HealthAndReadinessSubsystem
+    redis: RedisClient
+    exchange_identifier: ExchangeIdentifier_V3
+  }) {
+    assert(logger)
+    this.logger = logger
+    assert(send_message)
+    this.health_and_readiness = health_and_readiness
+    this.exchange_identifier = exchange_identifier
+    if (!process.env.BINANCE_API_KEY) throw new Error(`Missing BINANCE_API_KEY in ENV`)
+    if (!process.env.BINANCE_API_SECRET) throw new Error(`Missing BINANCE_API_SECRET in ENV`)
+    this.ee = Binance({
+      apiKey: process.env.BINANCE_API_KEY,
+      apiSecret: process.env.BINANCE_API_SECRET,
+    })
+    let order_context_persistence = new RedisOrderContextPersistance({ logger, redis })
+
+    this.order_execution_tracker = new OrderExecutionTracker({
+      ee: this.ee,
+      send_message,
+      logger,
+      order_callbacks: this,
+      order_context_persistence,
+      exchange_identifier,
+    })
+
+    this.publisher = new BinanceOrderPublisher({
+      logger,
+      health_and_readiness,
+    })
+  }
+
+  async start() {
+    await this.publisher.connect()
+    this.order_execution_tracker.main()
+  }
+
+  // What about partial fills?
+  // I think should should be a more raw interface - not using the callbacks interface but instead
+  // mapping and sending all messages, with an alert in the mapper when it sees anything it doesn't recognise
+  async order_filled(data: BinanceOrderData): Promise<void> {
+    this.logger.info(`Binance: ${data.side} order on ${data.symbol} filled.`)
+    await this.publisher.publish(data)
+  }
+}

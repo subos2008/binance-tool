b8750e437af937f83de0c0fc58405c2619156d9b
Ryan Cocks <ryan@ryancocks.net>
2022-05-08 15:59:04 -0300

    delete old files that got moved in earlier commits

--- interfaces/exchange/generic/orders-getter.ts
+++
@@ -1,8 +0,0 @@
-import { GenericOrder } from "../../../types/exchange_neutral/generic_order_data"
-
-export { GenericOrder }
-
-export interface OrdersGetter {
-  // get_open_orders(): Promise<GenericOrderData[]>
-  get_open_orders_on_specific_market({ market_symbol }: { market_symbol: string }): Promise<GenericOrder[]>
-}
--- interfaces/exchange/generic/portfolio-getter.ts
+++
@@ -1,5 +0,0 @@
-import { Balance } from "../../portfolio"
-
-export interface CurrentPortfolioGetter {
-  get_balances(): Promise<Balance[]>
-}

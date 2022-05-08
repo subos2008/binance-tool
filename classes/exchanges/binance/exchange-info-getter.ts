247f695fb720f2bea9c5dae9f0cbf213232a05cd
Ryan Cocks <ryan@ryancocks.net>
2022-05-08 15:43:51 -0300

    Add BinanceFuturesExchangeInfoGetter

--- classes/exchanges/binance/exchange-info-getter.ts
+++ classes/exchanges/binance/exchange-info-getter.ts
@@ -30,3 +30,31 @@ export class BinanceExchangeInfoGetter {
     return this.exchange_info_promise
   }
 }
+
+export class BinanceFuturesExchangeInfoGetter {
+  private ee: Binance
+  private exchange_info_promise: Promise<ExchangeInfo> | null | undefined
+  private minutes_to_cache_expiry: number = 24 * 60
+
+  constructor({ ee, minutes_to_cache_expiry }: { ee: Binance; minutes_to_cache_expiry?: number }) {
+    this.ee = ee
+    if (minutes_to_cache_expiry) this.minutes_to_cache_expiry = minutes_to_cache_expiry
+  }
+
+  async get_exchange_info(): Promise<ExchangeInfo> {
+    if (this.exchange_info_promise) {
+      return this.exchange_info_promise
+    }
+
+    logger.warn({ object_type: "BinanceFuturesExchangeInfoCacheMiss" }, `exchange_info not cached, reloading`)
+
+    if (!this.exchange_info_promise) {
+      setTimeout(() => {
+        this.exchange_info_promise = null
+      }, this.minutes_to_cache_expiry * 60 * 1000).unref()
+    }
+
+    this.exchange_info_promise = this.ee.futuresExchangeInfo()
+    return this.exchange_info_promise
+  }
+}

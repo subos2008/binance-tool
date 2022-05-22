e312e469a49dd4f6aeaaf863f64c12b1e2dcce19
Ryan Cocks <ryan@ryancocks.net>
2022-05-22 04:51:31 -0300

    fix add-redis-secret-to-namespace.sh

--- k8/setup/add-redis-secret-to-namespace.sh
+++ k8/setup/add-redis-secret-to-namespace.sh
@@ -2,6 +2,7 @@
 
 source ../../.env
 
-kubectl create secret generic redis --from-literal=REDIS_HOST="$REDIS_HOST" --namespace $1
-kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace $1
-kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace persistent-state
+kubectl create secret generic redis --from-literal=REDIS_HOST="$REDIS_HOST" --from-literal=REDIS_DATABASE_NUMBER="$REDIS_DATABASE_NUMBER" --from-literal=REDIS_PASSWORD="$REDIS_PASSWORD" --namespace $1
+# kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace $1
+# kubectl create secret generic redis-auth --from-literal=password="$REDIS_PASSWORD" --namespace persistent-state
+

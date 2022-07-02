b77892169a4b2ac9f34e44e5dfe9c5ea1ab4b421
Ryan Cocks <ryan@ryancocks.net>
2022-07-02 08:20:44 +0400

    BinanceFuturesPriceGetter

--- interfaces/exchanges/binance/binance-price-getter.ts
+++ interfaces/exchanges/binance/binance-price-getter.ts
@@ -33,3 +33,28 @@ export class BinancePriceGetter implements CurrentPriceGetter {
     return new BigNumber(this.prices[market_symbol])
   }
 }
+
+export class BinanceFuturesPriceGetter implements CurrentPriceGetter {
+  ee: Binance
+  prices: { [symbol: string]: string } | null = null
+  cache_timeout_ms: number
+  logger: Logger
+
+  constructor({ logger, ee, cache_timeout_ms }: { logger: Logger; ee: Binance; cache_timeout_ms?: number }) {
+    this.ee = ee
+    this.cache_timeout_ms = cache_timeout_ms || 60 * 1000
+    this.logger = logger
+  }
+
+  async get_current_price({ market_symbol }: { market_symbol: string }): Promise<BigNumber> {
+    if (!this.prices) {
+      this.logger.object({ object_type: "BinanceCurrentPriceGetterCacheMiss" })
+      this.prices = await this.ee.futuresPrices()
+      let timer: NodeJS.Timeout = setTimeout(() => {
+        this.prices = null
+      }, this.cache_timeout_ms)
+      timer.unref()
+    }
+    return new BigNumber(this.prices[market_symbol])
+  }
+}

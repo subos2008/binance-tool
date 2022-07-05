325b6bbe3b11b7afb6155c8975ad17c99eab40f0
Ryan Cocks <ryan@ryancocks.net>
2022-07-05 23:26:09 +0400

    mapper fix

--- edges/edge62/edge62-tas-to-ee-mapper.ts
+++ edges/edge62/edge62-tas-to-ee-mapper.ts
@@ -1,9 +1,10 @@
-import { LimitSellByQuoteQuantityWithTPandSLCommand, TradeAbstractionOpenShortCommand } from "../../services/binance/futures/trade-abstraction/interfaces/short"
-import { OrderContext_V1 } from "../../interfaces/orders/order-context"
+import {
+  TradeAbstractionOpenShortCommand,
+} from "../../services/binance/futures/trade-abstraction/interfaces/short"
 import { OrderContext_V2 } from "../../interfaces/orders/order-context"
 
 import { BigNumber } from "bignumber.js"
-import { BinanceFuturesExecutionEngine } from "../../services/binance/futures/trade-abstraction/execution/execution_engines/binance-futures-execution-engine"
+import { BinanceFuturesExecutionEngine, LimitSellByQuoteQuantityWithTPandSLCommand } from "../../services/binance/futures/trade-abstraction/execution/execution_engines/binance-futures-execution-engine"
 BigNumber.DEBUG = true // Prevent NaN
 // Prevent type coercion
 BigNumber.prototype.valueOf = function () {
@@ -21,6 +22,7 @@ export async function map_tas_to_ee_cmd_short(args: {
   order_context: OrderContext_V2
   trigger_price: BigNumber
   quote_amount: BigNumber
+  quote_asset: string
 }): Promise<LimitSellByQuoteQuantityWithTPandSLCommand> {
   let stop_price_factor = new BigNumber(100).plus(edge_percentage_stop).div(100)
   let stop_price = args.trigger_price.times(stop_price_factor)
@@ -31,13 +33,14 @@ export async function map_tas_to_ee_cmd_short(args: {
   let sell_limit_price_factor = new BigNumber(100).minus(edge_percentage_sell_limit).div(100)
   let sell_limit_price = args.trigger_price.times(sell_limit_price_factor)
 
-  let { quote_asset, base_asset } = args.tas_cmd
+  let { base_asset } = args.tas_cmd
+  let { quote_asset } = args
   let market_identifier = await args.ee.get_market_identifier_for({ quote_asset, base_asset })
 
   let { order_context, quote_amount } = args
   let short_entry_cmd: LimitSellByQuoteQuantityWithTPandSLCommand = {
-    object_type: "LimitSellByQuoteQuantityWithTPandSLCommand",
-    version: 1,
+    // object_type: "LimitSellByQuoteQuantityWithTPandSLCommand",
+    // version: 1,
     order_context,
     market_identifier,
     quote_amount,

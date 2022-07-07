9cbbe2e491e47ea0506aa44526dccd77d6ee288b
Ryan Cocks <ryan@ryancocks.net>
2022-07-07 23:52:31 +0400

    e60 longs $50

--- edges/position-sizer/fixed-position-sizer.ts
+++ edges/position-sizer/fixed-position-sizer.ts
@@ -43,7 +43,7 @@ export class FixedPositionSizer implements PositionSizer {
       if (direction === "long") return new BigNumber(50)
     }
     if (edge === "edge60") {
-      return new BigNumber(20)
+      return new BigNumber(50)
     }
     this.logger.warn(`Using default position size`)
     return new BigNumber(20)

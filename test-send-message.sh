02d7586562bb2cc34f9d8c0e36498e814f5c27f6
Ryan Cocks <ryan@ryancocks.net>
2022-07-19 01:24:19 +0400

    Add test-send-message

--- test-send-message.sh
+++ test-send-message.sh
@@ -3,3 +3,6 @@
 # ./proxy-rabbitmq.sh
 source .env
 /usr/local/opt/rabbitmq/sbin/rabbitmqadmin -u "$AMQP_USER" -p "$AMQP_PASSWORD" -V $AMQP_VHOST publish exchange=binance-tool routing_key=SendMessage payload="{ \"object_type\":\"SendMessage\", \"service_name\": \"ryan_test\", \"msg\": \"Hello World!\", \"tags\": {}}"
+
+
+// { "object_type":"SendMessage", "service_name": "ryan_test", "msg": "Hello World!", "tags": {}}

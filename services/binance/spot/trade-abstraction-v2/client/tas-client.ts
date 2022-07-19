6d16b4ff44576241204590166fbf75598d0fc0d5
Ryan Cocks <ryan@ryancocks.net>
2022-07-20 00:05:24 +0400

    All spot tas-client get's retry

--- services/binance/spot/trade-abstraction-v2/client/tas-client.ts
+++ services/binance/spot/trade-abstraction-v2/client/tas-client.ts
@@ -13,13 +13,11 @@ import {
 } from "../../../../../classes/spot/abstractions/position-identifier"
 import { ExchangeIdentifier_V3 } from "../../../../../events/shared/exchange-identifier"
 import Sentry from "../../../../../lib/sentry"
+import { AxiosRetry } from "./axios-retry"
 Sentry.configureScope(function (scope: any) {
   scope.setTag("class", "TradeAbstractionServiceClient")
 })
 
-function sleep(seconds: number) {
-  return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
-}
 export class TradeAbstractionServiceClient {
   logger: Logger
   TAS_URL: string
@@ -39,24 +37,24 @@ export class TradeAbstractionServiceClient {
   }
 
   async get_exchange_identifier(): Promise<ExchangeIdentifier_V3> {
-    let response = await this._call("GET", new URL("/exchange_identifier", this.TAS_URL).toString())
+    let response = await this.get(new URL("/exchange_identifier", this.TAS_URL).toString())
     this.logger.info(`Returned exchange_identifier:`)
-    this.logger.object(response)
-    return response
+    this.logger.object(response.data)
+    return response.data
   }
 
   async prices(): Promise<BinanceStyleSpotPrices> {
-    let response = await this._call("GET", new URL("/prices", this.TAS_URL).toString())
+    let response = await this.get(new URL("/prices", this.TAS_URL).toString())
     this.logger.info(`Returned prices:`)
-    this.logger.object(response)
-    return response
+    this.logger.object(response.data)
+    return response.data
   }
 
   async positions(): Promise<SpotPositionIdentifier_V3[]> {
-    let response = await this._call("GET", new URL("/positions", this.TAS_URL).toString())
+    let response = await this.get(new URL("/positions", this.TAS_URL).toString())
     this.logger.info(`Returned positions:`)
-    this.logger.object(response)
-    return response
+    this.logger.object(response.data)
+    return response.data
   }
 
   async close(cmd: TradeAbstractionCloseCommand): Promise<TradeAbstractionCloseResult> {
@@ -74,27 +72,17 @@ export class TradeAbstractionServiceClient {
 
   async long(cmd: TradeAbstractionOpenLongCommand): Promise<TradeAbstractionOpenLongResult> {
     let tas_response: TradeAbstractionOpenLongResult | undefined
-    do {
-      let response = await this.get(new URL("/long", this.TAS_URL).toString(), cmd)
-      this.logger.object(response)
-      tas_response = response.data as TradeAbstractionOpenLongResult
-      // TODO: need to return something if we get bullshit back, throw right
-      if (tas_response?.object_type !== "TradeAbstractionOpenLongResult") {
-        let err = new Error(`Unexpected result, expected object_type 'TradeAbstractionOpenLongResult`)
-        Sentry.captureException(err, { contexts: { tas_response: { tas_response } } })
-        throw err
-      }
-      try {
-        if (tas_response.http_status === 429) {
-          let seconds = Number(response.headers["retry-after"] || "0")
-          this.logger.warn(`429 from TAS, retrying in ${seconds} seconds`)
-          await sleep(seconds)
-        }
-      } catch (err) {
-        Sentry.captureException(err)
-        this.logger.error({ err })
-      }
-    } while (tas_response.http_status === 429)
+    let response = await this.get(new URL("/long", this.TAS_URL).toString(), cmd)
+
+    this.logger.object(response)
+    tas_response = response.data as TradeAbstractionOpenLongResult
+    if (tas_response?.object_type !== "TradeAbstractionOpenLongResult") {
+      let err = new Error(`Unexpected result, expected object_type 'TradeAbstractionOpenLongResult`)
+      Sentry.captureException(err, { contexts: { tas_response: { tas_response } } })
+      this.logger.error({ err })
+      throw err
+    }
+
     return tas_response
   }
 
@@ -111,54 +99,9 @@ export class TradeAbstractionServiceClient {
     return tas_response
   }
 
-  /**
-   * @private Make a HTTP request to a specific endpoint. Private endpoints are automatically signed.
-   */
-  private async _call(method: Method, endpoint: string, params?: string | object): Promise<any> {
-    try {
-      const options: AxiosRequestConfig = {
-        url: endpoint,
-        timeout: 10 * 1000, // ms, 1000 = 1 second
-        headers: {},
-        method: method,
-        transformResponse: (res: string) => {
-          // Do your own parsing here if needed ie JSON.parse(res);
-          return JSONBigNumber.parse(res)
-        },
-        // json: false, // avoid parsing json with the built in libs as they use floating point numbers
-        params,
-        validateStatus: (status) => status < 500,
-      }
-
-      let response = await axios(options)
-      return response.data
-    } catch (err) {
-      Sentry.captureException(err)
-      this.logger.error({ err })
-      throw err
-    }
-  }
-
   private async get(endpoint: string, params?: string | object): Promise<AxiosResponse<any, any>> {
-    try {
-      const options: AxiosRequestConfig = {
-        timeout: 10 * 1000, // ms, 1000 = 1 second
-        headers: {},
-        transformResponse: (res: string) => {
-          // Do your own parsing here if needed ie JSON.parse(res);
-          return JSONBigNumber.parse(res)
-        },
-        // json: false, // avoid parsing json with the built in libs as they use floating point numbers
-        params,
-        validateStatus: (status) => status < 500,
-      }
-
-      let response = await axios.get(endpoint, options)
-      return response
-    } catch (err) {
-      Sentry.captureException(err)
-      this.logger.error({ err })
-      throw err
-    }
+    let axios_retry = new AxiosRetry({ logger: this.logger })
+    let response = await axios_retry.get(endpoint, params)
+    return response
   }
 }

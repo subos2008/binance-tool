1823a32c4abebc92a541c210e906d018774b3b2f
Ryan Cocks <ryan@ryancocks.net>
2022-07-21 10:38:22 +0400

    reduce Sentry spam in telegram send_message

--- services/amqp/send-message/telegram/send-message.ts
+++ services/amqp/send-message/telegram/send-message.ts
@@ -33,6 +33,7 @@ export class SendMessage {
         Sentry.captureException(new Error(`Hit rate limit on telegram API (429)`))
         this.logger.warn(`Hit rate limit on telegram API (429)`)
         setTimeout(this.send_message.bind(this, ack_func, message, tags), 1000 * 61)
+        return
       }
       if (response.status != 200) {
         throw new Error(`Response status code from telegram api: ${response.status} ${response.statusText}`)

e303a9fa1a703fda7d7f54ab521c128b543b77af
Ryan Cocks <ryan@ryancocks.net>
2022-08-09 16:21:35 +0400

    fix market direction metrics

--- services/edge70-signals/backtesting/direction-persistance-mock.ts
+++ services/edge70-signals/backtesting/direction-persistance-mock.ts
@@ -6,7 +6,7 @@ export type Direction = "short" | "long" // Redis returns null for unset
 export class DirectionPersistenceMock implements DirectionPersistence {
   private logger: ServiceLogger
   private prefix: string
-  symbols: string[] | undefined
+  base_assets: string[] | undefined
   private keys: { [key: string]: string } = {}
 
   constructor({ logger, prefix }: { logger: ServiceLogger; prefix: string }) {
@@ -14,8 +14,8 @@ export class DirectionPersistenceMock implements DirectionPersistence {
     this.prefix = prefix
   }
 
-  private _market_to_key(symbol: string): string {
-    return `${this.prefix}:signal_direction:${symbol.toUpperCase()}`
+  private _market_to_key(base_asset: string): string {
+    return `${this.prefix}:signal_direction:${base_asset.toUpperCase()}`
   }
 
   private set(key: string, value: any) {
@@ -26,33 +26,33 @@ export class DirectionPersistenceMock implements DirectionPersistence {
     return this.keys[key]
   }
 
-  set_symbols(symbols: string[]) {
-    this.symbols = symbols
+  set_base_assets(base_assets: string[]) {
+    this.base_assets = base_assets
   }
 
-  async set_direction(symbol: string, direction: Direction) {
-    let previous_direction = await this.get_direction(symbol)
+  async set_direction(base_asset: string, direction: Direction) {
+    let previous_direction = await this.get_direction(base_asset)
     if (previous_direction === null) {
-      this.logger.info(`Initialising direction for ${symbol} to ${direction}`)
+      this.logger.info(`Initialising direction for ${base_asset} to ${direction}`)
     } else if (previous_direction !== direction) {
-      this.logger.debug(`Direction change to ${direction} for ${symbol}`)
+      this.logger.debug(`Direction change to ${direction} for ${base_asset}`)
     }
-    this.set(this._market_to_key(symbol), direction)
+    this.set(this._market_to_key(base_asset), direction)
     return previous_direction
   }
 
-  async get_direction(symbol: string): Promise<Direction | null> {
-    let direction = await this.get(this._market_to_key(symbol))
+  async get_direction(base_asset: string): Promise<Direction | null> {
+    let direction = await this.get(this._market_to_key(base_asset))
     return direction as Direction
   }
 
   async get_all_market_stats() {
-    if (!this.symbols) throw new Error(`Symbols not initialised`)
+    if (!this.base_assets) throw new Error(`base_assets not initialised`)
     let long = 0,
       short = 0,
       unknown = 0
-    for (const symbol of this.symbols) {
-      let dir: Direction | null = await this.get_direction(symbol)
+    for (const base_asset of this.base_assets) {
+      let dir: Direction | null = await this.get_direction(base_asset)
       if (!dir) unknown++
       if (dir === "long") long++
       if (dir === "short") short++
--- services/edge70-signals/interfaces/direction-persistance.ts
+++ services/edge70-signals/interfaces/direction-persistance.ts
@@ -1,6 +1,6 @@
 export type Direction = "short" | "long" // Redis returns null for unset
 
 export interface DirectionPersistence {
-  set_direction(symbol: string, direction: Direction): Promise<Direction | null>
-  get_direction(symbol: string): Promise<Direction | null>
+  set_direction(base_asset: string, direction: Direction): Promise<Direction | null>
+  get_direction(base_asset: string): Promise<Direction | null>
 }

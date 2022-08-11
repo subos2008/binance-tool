08d650daff2dab3f50c484084bb051c09f6b0d9c
Ryan Cocks <ryan@ryancocks.net>
2022-08-11 19:56:02 +0400

    ExchangeType

--- interfaces/send-message.ts
+++ interfaces/send-message.ts
@@ -1,3 +1,5 @@
+import { ExchangeType } from "../events/shared/exchange-identifier";
+
 export type SendMessageFunc = (msg: string, tags?: ContextTags) => void
 
 // TODO: idea, make a top level observability/ dir and put global tags styles (facets) in there
@@ -5,6 +7,6 @@ export interface ContextTags {
   edge?: string
   base_asset?: string // depricate in favour of some kind of context object?
   class?: string // name of the class calling send_message
-  exchange_type?: "spot" | "futures"
+  exchange_type?: ExchangeType
   symbol?: string
 }

545bb86674e9a38bb74873026e5c56463f39fbe8
Ryan Cocks <ryan@ryancocks.net>
2022-08-11 15:30:29 +0400

    fix backtest

--- backtesting/edge70/run.sh
+++ backtesting/edge70/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 source .env-backtest
+
+export INFLUXDB_TOKEN
+export INFLUXDB_HOST
+export INFLUXDB_ORG_ID
+export INFLUXDB_BUCKET
+export GRAFANA_HOST
+
 ./node_modules/.bin/ts-node ./backtesting/edge70/edge70-mega-backtester.ts

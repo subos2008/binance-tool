b7285dc75bcdfb17f10f0300139b9c666cd9b819
Ryan Cocks <ryan@ryancocks.net>
2022-09-17 12:11:46 +0400

    Remove edge60-signal-to-tas-bridge

--- services/edge60-signal-to-tas-bridge/README.md
+++
@@ -1,1 +0,0 @@
-It is very important that the bridge is aware of rate limiting and retries. Especially on close events.
--- services/edge60-signal-to-tas-bridge/interfaces.ts
+++
@@ -1,5 +0,0 @@
-import { Edge60PositionEntrySignal } from "../../events/shared/edge60-position-entry"
-
-export interface Edge60EntrySignalProcessor {
-  process_edge60_entry_signal: (signal: Edge60PositionEntrySignal) => Promise<void>
-}

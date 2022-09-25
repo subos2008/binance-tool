5423e6cb6d59e412b7e79c831461ea47d20ebb3f
Ryan Cocks <ryan@ryancocks.net>
2022-09-25 18:13:55 +0400

    Move to helm to deploy rabbitmq-operator

--- terraform/clusters/staging/0200-k8-rabbitmq-cluster-operator/README.md
+++
@@ -1,4 +0,0 @@
-The Operator allows us to define the rabbitmq cluster and also queue topology with k8 objects
-
-Note the operator just went to v2.0 but it requires k8 v1.25 which DO doesn't support yet
-
--- terraform/clusters/staging/0200-k8-rabbitmq-cluster-operator/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "DIGITALOCEAN_ACCESS_TOKEN" {}
-
-variable "DIGITALOCEAN_CLUSTER_NAME" {
-  description = "Used as a key in the path for the tf state - do not change after creation"
-}
-
-variable "rabbitmq_cluster_operator_version" {
-  default = "1.14.0" # v2 requires k8 v1.25
-}

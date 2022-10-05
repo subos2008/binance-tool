054136295b4dc9fee0d1e3b729bdeacc9313ac97
Ryan Cocks <ryan@ryancocks.net>
2022-10-05 13:00:46 +0700

    redis-v3 upgrade node-redis and add socket.setKeepAlive
    Otherwise an idle redis connection closes after 2 hours

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,6 @@
-import Sentry from "./sentry";
-import { Logger } from "../interfaces/logger";
-import { RedisClient } from "redis";
+import Sentry from "./sentry"
+import { Logger } from "../interfaces/logger"
+import { RedisClient } from "redis"
 
 var client_singleton: RedisClient | undefined
 var logger: Logger
@@ -13,50 +13,56 @@ export function set_redis_logger(_logger: Logger) {
 function generate_client(logger: Logger): RedisClient {
   const redis_retry_strategy = function (options: any) {
     Sentry.withScope(function (scope: any) {
-      scope.setTag("location", "redis-global-error-handler");
-      scope.setExtra("options", options);
+      scope.setTag("location", "redis-global-error-handler")
+      scope.setExtra("options", options)
       // Sentry.captureMessage("In redis_retry_strategy.");
-    });
+    })
     // logger.warn('Redis retry strategy called:');
     // logger.warn(util.inspect(options));
 
     if (options.error && options.error.code === "ECONNREFUSED") {
       // End reconnecting on a specific error and flush all commands with
       // a individual error
-      Sentry.captureMessage("In redis_retry_strategy: The server refused the connection");
-      return new Error("The server refused the connection");
+      Sentry.captureMessage("In redis_retry_strategy: The server refused the connection")
+      return new Error("The server refused the connection")
     }
     if (options.total_retry_time > 1000 * 60 * 60) {
-      Sentry.captureMessage("In redis_retry_strategy: Retry time exhausted");
+      Sentry.captureMessage("In redis_retry_strategy: Retry time exhausted")
       // End reconnecting after a specific timeout and flush all commands
       // with a individual error
-      return new Error("Retry time exhausted");
+      return new Error("Retry time exhausted")
     }
     if (options.attempt > 10) {
-      Sentry.captureMessage("In redis_retry_strategy: End reconnecting with built in error (too many attempts)");
+      Sentry.captureMessage("In redis_retry_strategy: End reconnecting with built in error (too many attempts)")
       // End reconnecting with built in error
-      return undefined;
+      return undefined
     }
     // reconnect after
-    if (options.attempt > 1) logger.warn('Redis retry strategy called: reconnect after');
-    return Math.min(options.attempt * 100, 3000);
+    if (options.attempt > 1) logger.warn("Redis retry strategy called: reconnect after")
+    return Math.min(options.attempt * 100, 3000)
   }
 
   const redis = require("redis").createClient({
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD,
     retry_strategy: redis_retry_strategy,
-  });
+  })
 
-  redis.on('error', function (err: any) {
-    logger.warn('Redis.on errror handler called');
-    logger.error(err.stack);
-    logger.error({ err });
+  redis.on("error", function (err: any) {
+    logger.warn("Redis.on errror handler called")
+    logger.error(err.stack)
+    logger.error({ err })
     Sentry.withScope(function (scope: any) {
-      scope.setTag("location", "redis-global-error-handler");
-      Sentry.captureException(err);
-    });
-  });
+      scope.setTag("location", "redis-global-error-handler")
+      Sentry.captureException(err)
+    })
+  })
+
+  // https://github.com/redis/node-redis/pull/1396
+  redis.on("connect", function () {
+    var socket = redis.stream
+    socket.setKeepAlive(true, 30 * 1000)
+  })
 
   return redis
 }
@@ -65,5 +71,5 @@ export function get_redis_client() {
   if (!client_singleton) {
     client_singleton = generate_client(logger)
   }
-  return client_singleton;
+  return client_singleton
 }

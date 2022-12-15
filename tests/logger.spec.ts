190a388d189bba57278aa4467c7d249bc64b5765
Ryan Cocks <ryan@ryancocks.net>
2022-12-15 01:32:06 +0000

    Fix jest logger tests

--- tests/logger.spec.ts
+++ tests/logger.spec.ts
@@ -3,23 +3,29 @@
 
 import { spawn } from "child_process"
 import path from "node:path"
-// import * as LineByLineReader from 'line-by-line'
+
+jest.setTimeout(8000)
 
 var LineByLineReader = require("line-by-line")
+// import * as LineByLineReader from "line-by-line"
 
 describe("logger behaviour", () => {
-  it("logs out multiple params - 2 strings", (done) => {
+  it("logs out multiple params - 2 strings", () => {
     const testAppFilePath = path.join(__dirname, "./logger.ts")
-    const testApp = spawn("../node_modules/.bin/ts-node", [testAppFilePath])
+    const testApp = spawn("./node_modules/.bin/ts-node", [testAppFilePath])
 
     let lr = new LineByLineReader(testApp.stdout)
 
     lr.on("line", (data: string) => {
       const stdoutData = JSON.parse(data)
-      expect(stdoutData.msg).toBe("Hello from .info without tags!")
-      // expect(stdoutData.foo).toBe("bar")
-      testApp.kill("SIGINT")
-      done()
+      expect(stdoutData.msg).toBe("Hello from .info with tags!")
+      expect(stdoutData.object_type).toBe("TEST_EVENT")
+      expect(stdoutData.base_asset).toBe("EVENT")
+      expect(stdoutData.object_class).toBe("event")
+      lr.close() // needed?
+      lr.removeAllListeners() // needed?
+      testApp.disconnect() // needed?
+      testApp.kill("SIGINT") // needed?
     })
   })
 })
